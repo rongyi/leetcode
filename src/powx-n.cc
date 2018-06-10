db66bfc7926696a4c0f07f9242bcf0eb48ba812a
rongyi <sqrongyi@163.com>
2018-06-10 16:05:00 +0800

    Initial commit

---
+++ src/powx-n.cc
@@ -0,0 +1,23 @@
+#include "one.h"
+
+class Solution {
+public:
+  double myPow(double x, int n) {
+    if (n < 0) {
+      return 1 / pow(x, -n);
+    }
+    return pow(x, n);
+  }
+private:
+  double pow(double x, int n) {
+    if (n == 0) {
+      return 1;
+    }
+    double half = pow(x, n / 2);
+    // n is odd
+    if (n & 0x1) {
+      return x * half * half;
+    }
+    return half * half;
+  }
+};
