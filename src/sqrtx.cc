b436df850809ee882d05135dfaf52f96453a9bc6
rongyi <sqrongyi@163.com>
2018-06-10 16:42:22 +0800

    Add valid sqrtx

---
+++ src/sqrtx.cc
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/sqrtx/description/
+#include "one.h"
+
+class Solution {
+public:
+  int mySqrt(int x) {
+    if (x <= 1) {
+      return x;
+    }
+    int left = 0;
+    int right = x;
+    while (left < right) {
+      int mid = left + (right - left) / 2;
+      if (x / mid >= mid) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
+    }
+    return right - 1;
+  }
+};
