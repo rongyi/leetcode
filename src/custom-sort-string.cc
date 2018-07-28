d9dd09c2e77628479f116f3dfd3f871b116542e0
rongyi <sqrongyi@163.com>
2018-07-28 10:57:43 +0800

    Add custom sort

---
+++ src/custom-sort-string.cc
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/custom-sort-string/description/
+#include "one.h"
+
+class Solution {
+public:
+  string customSortString2(string S, string T) {
+    std::string ret = T;
+    std::unordered_map<char, int> order;
+    for (int i = 0; i < S.size(); i++) {
+      order[S[i]] = i + 1;
+    }
+    std::sort(ret.begin(), ret.end(),
+              [&](char a, char b) { return order[a] < order[b]; });
+    return ret;
+  }
+  string customSortString(string S, string T) {
+    std::string ret;
+    std::unordered_map<char, int> count;
+    for (auto c : T) {
+      ++count[c];
+    }
+
+    for (auto c : S) {
+      ret += string(count[c], c);
+      count[c] = 0;
+    }
+
+    for (auto c : count) {
+      ret += string(c.second, c.first);
+    }
+
+    return ret;
+  }
+};
