e9f4af605c877c84efb27c1b0d46b9751e69391b
rongyi <sqrongyi@163.com>
2018-07-28 09:49:00 +0800

    Update numberOfLines

---
+++ src/number-of-lines-to-write-string.cc
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/number-of-lines-to-write-string/description/
+#include "one.h"
+
+class Solution {
+public:
+  vector<int> numberOfLines(vector<int> &widths, string s) {
+    int line = 1;
+    int line_chars = 0;
+    for (int i = 0; i < s.size(); i++) {
+      auto w = widths[s[i] - 'a'];
+      line_chars += w;
+      if (line_chars > 100) {
+        line++;
+        line_chars = 0;
+        i--;
+      }
+    }
+    return vector<int>{line, line_chars};
+  }
+};
