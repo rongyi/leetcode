3199a74451ddd473fcae0ce87cae3c115034be00
rongyi <sqrongyi@163.com>
2018-08-11 08:56:31 +0800

    Add rotatedDigits

---
+++ src/rotated-digits.cc
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/rotated-digits/description/
+#include "one.h"
+
+class Solution {
+public:
+  int rotatedDigits(int N) {
+    int ret = 0;
+    for (int i = 1; i <= N; ++i) {
+      if (valid(i)) {
+        ++ret;
+      }
+    }
+    return ret;
+  }
+private:
+  bool valid(const int n) {
+    bool ret = false;
+    string sn = std::to_string(n);
+    for (auto c : sn) {
+      if (c == '3' || c == '4' || c == '7') {
+        return false;
+      }
+      if (c == '2' || c == '5' || c == '6' || c == '9') {
+        ret = true;
+      }
+    }
+    return ret;
+  }
+};
