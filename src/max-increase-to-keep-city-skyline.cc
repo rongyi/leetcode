561d9b89ba1a0ce535980820311263b756540019
rongyi <sqrongyi@163.com>
2018-08-12 08:46:16 +0800

    Add maxIncreaseKeepingSkyline

---
+++ src/max-increase-to-keep-city-skyline.cc
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/max-increase-to-keep-city-skyline/description/
+
+#include "one.h"
+
+class Solution {
+public:
+  int maxIncreaseKeepingSkyline(vector<vector<int>> &grid) {
+    const int m = grid.size();
+    const int n = grid[0].size();
+
+    vector<int> row(m, 0);
+    vector<int> col(n, 0);
+    int ret = 0;
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        row[i] = max(row[i], grid[i][j]);
+        col[j] = max(col[j], grid[i][j]);
+      }
+    }
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        ret += min(row[i] - grid[i][j], col[j] - grid[i][j]);
+      }
+    }
+
+
+    return ret;
+  }
+};
