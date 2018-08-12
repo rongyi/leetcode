ec1474ebec4387b9abb58cd618ecd08403809716
rongyi <sqrongyi@163.com>
2018-08-12 09:27:17 +0800

    Add comment for minimum swap

--- src/minimum-swaps-to-make-sequences-increasing.cc
+++ src/minimum-swaps-to-make-sequences-increasing.cc
@@ -9,6 +9,9 @@ public:
   // noswap[0] == 0
 
 
+  // 如果当前节点和前一个节点在两个数组中都有序了，所以按道理不需要置换当前节点，所以 noswap[i] = noswap[i - 1]
+  // 但如果非要强制转换，那之前的节点也得跟着换，所以swap[i] = swap[i - 1] + 1
+  // 如果是置换当前节点后有序，则swap[i] = noswap[i - 1] + 1, 如果非要不换此节点（某种程度的相对运动）,那就换前一个节点，noswap[i] = swap[i - 1]
   int minSwap(vector<int> &a, vector<int> &b) {
     const int n = a.size();
     vector<int> swap(n, n);
