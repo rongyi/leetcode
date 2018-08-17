a309d3defd834f49b877af753ac721d1af08df13
rongyi <sqrongyi@163.com>
2018-08-18 07:34:34 +0800

    Add soupServings

---
+++ src/soup-servings.cc
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/soup-servings/description/
+#include "one.h"
+class Solution {
+public:
+  double soupServings(int N) {
+    return N >= 4800 ? 1.0 : f(N, N);
+  }
+
+private:
+  double f (int a, int b) {
+    if (a <= 0 && b <= 0) {
+      return 0.5;
+    }
+    if (a <= 0) {
+      return 1.0;
+    }
+    if (b <= 0) {
+      return 0;
+    }
+    string spoon = to_string(a) + ":" + to_string(b);
+    // 递归加缓存
+    if (cache_.find(spoon) == cache_.end()) {
+      cache_[spoon] = 0.25 * (f(a - 100, b) + f(a - 75, b - 25) + f(a - 50, b - 50) + f(a - 25, b - 75));
+    }
+
+    return cache_[spoon];
+  }
+private:
+  unordered_map<string, double> cache_;
+};
