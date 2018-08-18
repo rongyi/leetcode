45d7d9fe4f386a520d4527da4a1920446568fb01
rongyi <sqrongyi@163.com>
2018-08-18 21:16:48 +0800

    Add champagne

---
+++ src/champagne-tower.cc
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/champagne-tower/description/
+#include "one.h"
+class Solution {
+public:
+  // dp[i][j] 表示i层第j个酒杯摊到的酒杯
+  double champagneTower(int poured, int query_row, int query_glass) {
+    vector<vector<double>> dp(101, vector<double>(101, 0));
+    dp[0][0]= poured;
+    for (int i = 0; i <= query_row; ++i) {
+      for (int j = 0; j <= i; ++j) {
+        if (dp[i][j] >= 1) {
+          dp[i + 1][j] += (dp[i][j] - 1) / 2.0;
+          dp[i + 1][j+1] += (dp[i][j] - 1) / 2.0;
+        }
+      }
+    }
+    return min(1.0, dp[query_row][query_glass]);
+  }
+};
