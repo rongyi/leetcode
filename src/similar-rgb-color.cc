e131a5f74f050a19bd1b8110d26707808988ae88
rongyi <sqrongyi@163.com>
2018-08-18 20:53:22 +0800

    Add similarRGB

---
+++ src/similar-rgb-color.cc
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/similar-rgb-color/description/
+#include "one.h"
+class Solution {
+public:
+  string similarRGB(string color) {
+    auto fn = [](string str) -> string {
+      const static string dict = "0123456789abcdef";
+      int num = stoi(str, nullptr, 16);
+      int idx = num / 17 + ((num % 17) > 8 ? 1 : 0);
+      return string(2, dict[idx]);
+    };
+    return "#" + fn(color.substr(1, 2)) + fn(color.substr(3, 2)) +
+           fn(color.substr(5, 2));
+  }
+};
+
+
+
+int main() {
+  Solution so;
+  auto near = so.similarRGB("#09f166");
+  cout << near << endl;
+}
