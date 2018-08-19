90b45826b451ad3e35b6b3b2f80c19410ac4a3c9
rongyi <sqrongyi@163.com>
2018-08-19 11:36:36 +0800

    Update smallest

--- src/smallest-rotation-with-highest-score.cc
+++ src/smallest-rotation-with-highest-score.cc
@@ -2,6 +2,9 @@
 #include "one.h"
 class Solution {
 public:
+  // https://leetcode.com/problems/smallest-rotation-with-highest-score/discuss/118725/Easy-and-Concise-5-lines-Solution-C++JavaPython?page=1
+  // 1. 要求某一位置上值 <= 当前index  ==> A[i] <= i，对于数组的取值范围： 0-数组长度，0放哪里都满足条件，
+  // N放哪里都不满足条件
   int bestRotation(vector<int> &A) {
     const int n = A.size();
     vector<int> change(n, 0);
@@ -15,3 +18,10 @@ public:
     return max_element(change.begin(), change.end()) - change.begin();
   }
 };
+
+int main() {
+  Solution so;
+  vector<int> input{2, 3, 1, 4, 0};
+  auto test = so.bestRotation(input);
+  cout << test << endl;
+}
