7e84ebc775316de94268f5720c4d221fc53d6103
rongyi <sqrongyi@163.com>
2018-08-25 07:48:13 +0800

    Update largestTriangleArea

---
+++ src/largest-triangle-area.cc
@@ -0,0 +1,35 @@
+// http://leetcode.com/problems/largest-triangle-area/description/
+#include "one.h"
+
+class Solution {
+public:
+  double largestTriangleArea(vector<vector<int>> &points) {
+    double ret = 0.0;
+    const int n = points.size();
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        for (int k = j + 1; k < n; k++) {
+          auto a = eucianDistance(points[i], points[j]);
+          auto b = eucianDistance(points[j], points[k]);
+          auto c = eucianDistance(points[i], points[k]);
+          if (isValidTriangle(a, b, c)) {
+            auto s = (a + b + c) / 2;
+            ret = max(ret, sqrt(s * (s - a) * (s - b) * (s - c)));
+          }
+        }
+      }
+    }
+
+    return ret;
+  }
+
+private:
+  double eucianDistance(vector<int> &p1, vector<int> &p2) {
+    int x = p1[0] - p2[0];
+    int y = p1[1] - p2[1];
+    return ::sqrt(x * x + y * y);
+  }
+  bool isValidTriangle(double a, double b, double c) {
+    return (a + b) > c && (b + c) > a && (a + c) > b;
+  }
+};
