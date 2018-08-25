c24efb397fca98068e6517b060092d36aebc22fe
rongyi <sqrongyi@163.com>
2018-08-25 19:42:49 +0800

    Add morse_str

---
+++ src/unique-morse-code-words.cc
@@ -0,0 +1,19 @@
+// http://leetcode.com/problems/unique-morse-code-words/description/
+#include "one.h"
+
+class Solution {
+public:
+  int uniqueMorseRepresentations(vector<string>& words) {
+    vector<string> dict{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+    unordered_set<string> ret;
+    for (auto w : words) {
+      string morse_str("");
+      for (auto c : w) {
+        morse_str += dict[c - 'a'];
+      }
+      ret.insert(morse_str);
+    }
+
+    return ret.size();
+  }
+};
