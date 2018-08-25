d024890fe8246326bad41236984148eeabfb7a56
rongyi <sqrongyi@163.com>
2018-08-25 19:54:05 +0800

    Add allPathsSourceTarget

---
+++ src/all-paths-from-source-to-target.cc
@@ -0,0 +1,21 @@
+// http://leetcode.com/problems/all-paths-from-source-to-target/description/
+#include "one.h"
+
+class Solution {
+public:
+  vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+    vector<vector<int>> ret;
+    dfs(graph, 0, {}, ret);
+    return ret;
+  }
+private:
+  void dfs(vector<vector<int>> &graph, int cur_node, vector<int> path, vector<vector<int>> &ret) {
+    path.push_back(cur_node);
+    if (cur_node == graph.size() - 1) {
+      ret.push_back(path);
+    }
+    for (auto n : graph[cur_node]) {
+      dfs(graph, n, path, ret);
+    }
+  }
+};
