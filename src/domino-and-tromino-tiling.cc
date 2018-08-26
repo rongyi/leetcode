b986992b1ca2dac28f348ff1339bf7a6b5805076
rongyi <sqrongyi@163.com>
2018-08-26 08:56:52 +0800

    Add domino tiling

---
+++ src/domino-and-tromino-tiling.cc
@@ -0,0 +1,22 @@
+// http://leetcode.com/problems/domino-and-tromino-tiling/description/
+// ../image/domino.png
+#include "one.h"
+class Solution {
+public:
+  // dp[n] = dp[n-1] + dp[n-2] + 2 * (dp[n-3] + ... + dp[0])
+  //       = dp[n-1] + dp[n-3] + dp[n-2] + dp[n-3] + 2 * (dp[n-4] + ... dp[0])
+  //       = dp[n-1] + dp[n-3] + dp[n-1]
+  //       = 2 * dp[n-1] + dp[n-3]
+  int numTilings(int N) {
+    const static int m = 1e9 + 7;
+    vector<long> dp(N + 1);
+    dp[0] = 1;
+    dp[1] = 1;
+    dp[2] = 2;
+    for (int i = 3; i <= N; ++i) {
+      dp[i] = (dp[i - 1] * 2 + dp[i - 3]) % m;
+    }
+
+    return dp[N];
+  }
+};
