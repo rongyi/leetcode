da3874b036a762458bbc88981ba817e27a71e83e
rongyi <sqrongyi@163.com>
2018-08-26 09:11:30 +0800

    Add escapeGhosts

---
+++ src/escape-the-ghosts.cc
@@ -0,0 +1,15 @@
+// http://leetcode.com/problems/escape-the-ghosts/description/
+#include "one.h"
+class Solution {
+public:
+  bool escapeGhosts(vector<vector<int>> &ghosts, vector<int> &target) {
+    const int dist = abs(target[0]) + abs(target[1]);
+    for (auto g : ghosts) {
+      int t = abs(g[0] - target[0]) + abs(g[1] - target[1]);
+      if (t <= dist) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
