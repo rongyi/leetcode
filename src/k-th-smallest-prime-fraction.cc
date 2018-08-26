e44006258ad9610a04ecfb9a5d158d331f5eb4c7
rongyi <sqrongyi@163.com>
2018-08-26 11:38:09 +0800

    Add kthSmallestPrimeFraction

---
+++ src/k-th-smallest-prime-fraction.cc
@@ -0,0 +1,49 @@
+// http://leetcode.com/problems/k-th-smallest-prime-fraction/description/
+#include "one.h"
+
+class Solution {
+public:
+  // [1, 7, 23, 29, 47]，那么有：
+  // 1/47   < 1/29    < 1/23 < 1/7
+  // 7/47   < 7/29    < 7/23
+  // 23/47  < 23/29
+  // 29/47
+  vector<int> kthSmallestPrimeFraction(vector<int> &A, int K) {
+    priority_queue<fraction, vector<fraction>, greater<fraction>> pq;
+    const int n = A.size();
+    // 先插入第一列
+    for (int i = 0; i < n - 1; ++i) {
+      pq.push({double(A[i]) / A.back(), i, n - 1});
+    }
+
+    for (int i = 0; i < K - 1; i++) {
+      auto cur = pq.top();
+      // 为什么要pop？
+      // 每一列只允许有一个值存在
+      pq.pop();
+      --cur.demoninator_;
+      pq.push({double(A[cur.numerator_]) / A[cur.demoninator_], cur.numerator_,
+               cur.demoninator_});
+    }
+    return {A[pq.top().numerator_], A[pq.top().demoninator_]};
+  }
+
+private:
+  struct fraction {
+    double f_;
+    int numerator_;
+    int demoninator_;
+    bool operator<(fraction other) const { return f_ < other.f_; }
+    bool operator>(fraction other) const { return f_ > other.f_; }
+  };
+};
+
+
+int main() {
+  Solution so;
+
+  // [1, 7, 23, 29, 47]，那么有：
+  vector<int> input{1, 7, 23, 29, 47};
+  auto ret = so.kthSmallestPrimeFraction(input, 2);
+  cout << ret[0] << ":" <<  ret[1] << endl;
+}
