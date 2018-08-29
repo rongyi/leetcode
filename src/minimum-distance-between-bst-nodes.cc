c0b40d8fe92436cc80ef991106f553fb40cd2507
rongyi <sqrongyi@163.com>
2018-08-30 07:21:48 +0800

    Add minimum distance between bst nodes

---
+++ src/minimum-distance-between-bst-nodes.cc
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/minimum-distance-between-bst-nodes/description/
+#include "one.h"
+
+// * Definition for a binary tree node.
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+  int minDiffInBST(TreeNode *root) {
+    int ret = numeric_limits<int>::max();
+    int pre = -1;
+    inorder(root, ret, pre);
+    return ret;
+  }
+
+private:
+  void inorder(TreeNode *root, int &ret, int &pre) {
+    if (!root) {
+      return;
+    }
+    inorder(root->left, ret, pre);
+    if (pre != -1) {
+      ret = min(ret, root->val - pre);
+    }
+    pre = root->val;
+    inorder(root->right, ret, pre);
+  }
+};
+
+int main() {
+  Solution so;
+  TreeNode root(2), left(1), right(9);
+  root.left = &left;
+  root.right = &right;
+  auto ret = so.minDiffInBST(&root);
+  cout << ret << endl;
+}
