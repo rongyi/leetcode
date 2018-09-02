1cd6ead1b775e09ad8aec23ea6c036aa3103d3c4
rongyi <sqrongyi@163.com>
2018-09-02 08:11:49 +0800

    Add reachingPoints

---
+++ src/reaching-points.cc
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/reaching-points/description/
+
+#include "one.h"
+
+class Solution {
+public:
+  bool reachingPoints(int sx, int sy, int tx, int ty) {
+    if (tx < sx || ty < sy) {
+      return false;
+    }
+    if (tx == sx && (ty - sy) % sx == 0) {
+      return true;
+    }
+    if (ty == sy && (tx - sx) % sy == 0) {
+      return true;
+    }
+    return reachingPoints(sx, sy, tx % ty, ty % tx);
+  }
+};
