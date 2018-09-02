6b14a70316df8fc05ebd8e1903da89dc637acc75
rongyi <sqrongyi@163.com>
2018-09-02 10:17:54 +0800

    Update swin test case

--- src/swim-in-rising-water.cc
+++ src/swim-in-rising-water.cc
@@ -48,7 +48,11 @@ private:
 
 int main() {
   Solution so;
-  vector<vector<int>> input{{0, 2}, {1, 3}};
+  vector<vector<int>> input{{0, 1, 2, 3, 4},
+                            {24, 23, 22, 21, 5},
+                            {12, 13, 14, 15, 16},
+                            {11, 17, 18, 19, 20},
+                            {10, 9, 8, 7, 6}};
   auto ret = so.swimInWater(input);
   cout << ret << endl;
 }
