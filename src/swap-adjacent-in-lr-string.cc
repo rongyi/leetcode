0818fe4e05126302218fc464d1cb09e39f397673
rongyi <sqrongyi@163.com>
2018-09-03 07:42:06 +0800

    Add canTransform

---
+++ src/swap-adjacent-in-lr-string.cc
@@ -0,0 +1,34 @@
+// http://www.lintcode.com/zh-cn/problem/swap-adjacent-in-lr-string
+
+#include "one.h"
+
+class Solution {
+public:
+  bool canTransform(string start, string end) {
+    const int n = start.size();
+    int i = 0;
+    int j = 0;
+    while (i < n && j < n) {
+      while (start[i] == 'X') {
+        ++i;
+      }
+      while (end[j] == 'X') {
+        ++j;
+      }
+      if (start[i] != end[j]) {
+        return false;
+      }
+      // XL ==> LX 所以L的index在变小
+      if (start[i] == 'L' && i < j) {
+        return false;
+      }
+      // RX ==> XR 同样的，R的index在变大
+      if (start[i] == 'R' && i > j) {
+        return false;
+      }
+      ++i;
+      ++j;
+    }
+    return true;
+  }
+};
