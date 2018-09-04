56c7ccc6bb9773df56364da4e4c527503777e80d
rongyi <sqrongyi@163.com>
2018-09-04 22:47:24 +0800

    Add comment

--- src/minimize-max-distance-to-gas-station.cc
+++ src/minimize-max-distance-to-gas-station.cc
@@ -1,6 +1,9 @@
 // http://www.lintcode.com/zh-cn/problem/minimize-max-distance-to-gas-station
 #include "one.h"
 
+// 一种贪心的做法是，找到最大的gap，插入1个station，依此类推，但很遗憾，这种贪心策略是错误的。问题的难点在于我们无法确定到底哪两个station之间需要插入station，插入几个station也无法得知。
+
+// 换个思路，如果我们假设知道了答案会怎么样？因为知道了最大间隔，所以如果目前的两个station之间的gap没有符合最大间隔的约束，那么我们就必须添加新的station来让它们符合最大间隔的约束，这样一来，对于每个gap我们是能够求得需要添加station的个数。如果需求数<=K，说明我们还可以进一步减小最大间隔，直到需求数>K。
 class Solution {
 public:
   double minmaxGasDist(vector<int> &stations, int K) {
