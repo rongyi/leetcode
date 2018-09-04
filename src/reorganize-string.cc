0f8c105c5b3505a6f822c80f0c3bc7deaf45db8e
rongyi <sqrongyi@163.com>
2018-09-05 07:42:16 +0800

    Update reorganize string, add more comment

--- src/reorganize-string.cc
+++ src/reorganize-string.cc
@@ -5,7 +5,9 @@ class Solution {
 public:
   string reorganizeString(string s) {
     string ret = "";
+    // 记录每个字符出现的次数 char ==> count
     unordered_map<char, int> char_count;
+    // 记录出现这么多次的字符, 按字数进行排序
     priority_queue<pair<int, char>> q;
     for (char c : s) {
       ++char_count[c];
@@ -16,6 +18,8 @@ public:
       q.push({cn.second, cn.first});
     }
 
+    // 然后每次出两个，把字符放一起，这样来保证相邻不重复
+    // 相当于重组的一种方法，从出现多的到少的，一次一个一个的拿
     while (q.size() >= 2) {
       auto t1 = q.top();
       q.pop();
