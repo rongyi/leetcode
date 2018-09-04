3988b7ea56143e76e3b64a6710825f7ba8d9e304
rongyi <sqrongyi@163.com>
2018-09-04 21:59:05 +0800

    Add global and local inversions

---
+++ src/global-and-local-inversions.cc
@@ -0,0 +1,18 @@
+// http://www.lintcode.com/zh-cn/problem/global-and-local-inversions
+
+#include "one.h"
+
+class Solution {
+public:
+  bool isIdealPermutation(vector<int> &A) {
+    const int n = A.size();
+    int cur_min = numeric_limits<int>::max();
+    for (int i = n - 1; i >= 2; --i) {
+      cur_min = min(cur_min, A[i]);
+      if (A[i - 2] > cur_min) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
