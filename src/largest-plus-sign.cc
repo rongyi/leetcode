633c9cc6a8ab5f39cddcf0347bd396ace879e197
rongyi <sqrongyi@163.com>
2018-09-08 09:57:02 +0800

    Update largest plus sign

--- src/largest-plus-sign.cc
+++ src/largest-plus-sign.cc
@@ -5,6 +5,36 @@ class Solution {
 public:
   int orderOfLargestPlusSign(int N, vector<vector<int>> &mines) {
     // construct the matrix
-    vec
+    vector<vector<int>> matrix(N, vector<int>(N, 1));
+    for (auto cord : mines) {
+      matrix[cord[0]][cord[1]] = 0;
+    }
+    int ret = 0;
+    for (int i = 0; i < N; ++i) {
+      for (int j = 0; j < N; ++j) {
+        int k = 0;
+
+        while (canExpand(matrix, i, j, k, N)) {
+          ++k;
+        }
+        ret = max(ret, k);
+      }
+    }
+
+    return ret;
+  }
+
+private:
+  bool canExpand(vector<vector<int>> &matrix, int i, int j, int k,
+                 const int n) {
+    // boundary check
+    if (i + k >= n || i - k < 0 || j + k >= n || j - k < 0) {
+      return false;
+    }
+    return matrix[i + k][j] && matrix[i - k][j] && matrix[i][j - k] && matrix[i][j + k];
   }
 };
+
+int main() {
+
+}
