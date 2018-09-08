92b2519180847f1401b921a5811420bb2e65c62f
rongyi <sqrongyi@163.com>
2018-09-08 09:31:09 +0800

    Add plus sign

---
+++ src/largest-plus-sign.cc
@@ -0,0 +1,10 @@
+// http://www.lintcode.com/zh-cn/problem/largest-plus-sign
+#include "one.h"
+
+class Solution {
+public:
+  int orderOfLargestPlusSign(int N, vector<vector<int>> &mines) {
+    // construct the matrix
+    vec
+  }
+};
