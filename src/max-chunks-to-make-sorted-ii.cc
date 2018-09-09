b9c4005a16dd43824ed59254ae65fe1a7bc6f37c
rongyi <sqrongyi@163.com>
2018-09-09 10:25:34 +0800

    Update maxChunksToSorted

--- src/max-chunks-to-make-sorted-ii.cc
+++ src/max-chunks-to-make-sorted-ii.cc
@@ -6,6 +6,7 @@ public:
     const int n = arr.size();
     // one total chunk
     int ret = 1;
+    // 能在某个位置劈开的条件是，前面一坨的最大值都不能比后面最小的还要小，否则就拼不起来了
     vector<int> max_till_cur = arr;
     vector<int> min_after_cur = arr;
     for (int i = 1; i < n; ++i) {
