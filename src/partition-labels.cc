3c43a991f9f9faa3eb0956f4f550a15583c2446a
rongyi <sqrongyi@163.com>
2018-09-13 19:00:15 +0800

    Update partitionLabels

---
+++ src/partition-labels.cc
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/partition-labels/description/
+#include "one.h"
+
+class Solution {
+public:
+  vector<int> partitionLabels(string S) {
+    const int n = S.size();
+    vector<int> ret;
+    unordered_set<char> cache;
+
+
+    return ret;
+  }
+};
+
+
+int main() {
+  Solution so;
+  // string input{"zababcbacadefegdehijhklij"};
+  string input{"zaz"};
+  so.partitionLabels(input);
+}
