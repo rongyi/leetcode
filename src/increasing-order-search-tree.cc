d492f3eaf24e36511cce5470783b899b3ee9b036
rongyi <sqrongyi@163.com>
2018-09-15 22:17:19 +0800

    Add increasingBST

---
+++ src/increasing-order-search-tree.cc
@@ -0,0 +1,56 @@
+// https://leetcode.com/problems/increasing-order-search-tree/description/
+#include "one.h"
+
+/**
+ * Definition for a binary tree node.
+ struct TreeNode {
+ int val;
+ TreeNode *left;
+ TreeNode *right;
+ TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+*/
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+  TreeNode *increasingBST(TreeNode *root) {
+    vector<TreeNode *> vec;
+    inorder(root, vec);
+    const int n = vec.size();
+    for (int i = 1; i < n; ++i) {
+      auto cur = vec[i - 1];
+      cur->left = nullptr;
+      cur->right = vec[i];
+    }
+    vec.back()->left = nullptr;
+    return vec.front();
+  }
+
+public:
+  void inorder(TreeNode *cur, vector<TreeNode *> &vec) {
+    if (!cur) {
+      return;
+    }
+    inorder(cur->left, vec);
+    vec.push_back(cur);
+    inorder(cur->right, vec);
+  }
+};
+
+int main() {
+  Solution so;
+  TreeNode root(2);
+  TreeNode left(1);
+  TreeNode right(3);
+  root.left = &left;
+  root.right = &right;
+  auto ret = so.increasingBST(&root);
+  cout << ret->val <<endl;
+}
