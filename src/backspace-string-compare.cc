e196ed274964f2f1b32fec9ec4e555c758d35d3a
rongyi <sqrongyi@163.com>
2018-09-15 20:02:36 +0800

    Add backspaceCompare

---
+++ src/backspace-string-compare.cc
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/backspace-string-compare/description/
+#include "one.h"
+class Solution {
+public:
+  bool backspaceCompare(string S, string T) {
+    stack<char> sstk;
+    stack<char> tstk;
+    auto help = [](string &str) -> stack<char> {
+      stack<char> ret;
+      for (auto cur : str) {
+        if (cur == '#') {
+          if (!ret.empty()) {
+            ret.pop();
+          }
+        } else {
+          ret.push(cur);
+        }
+      }
+      return ret;
+    };
+
+    auto shelp = help(S);
+    auto thelp = help(T);
+    while (!shelp.empty() && !thelp.empty()) {
+      if (shelp.top() != thelp.top()) {
+        return false;
+      }
+      shelp.pop();
+      thelp.pop();
+    }
+
+    return shelp.empty() && thelp.empty();
+  }
+};
