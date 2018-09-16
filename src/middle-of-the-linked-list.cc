52eef1214e5d1f0b79398a5458b5b55c9a530347
rongyi <sqrongyi@163.com>
2018-09-17 07:28:57 +0800

    Add middleNode of linked list

---
+++ src/middle-of-the-linked-list.cc
@@ -0,0 +1,51 @@
+// https://leetcode.com/problems/middle-of-the-linked-list/description/
+#include "one.h"
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+  ListNode *middleNode(ListNode *head) {
+    auto cp = head;
+    auto l = len(cp);
+    auto mid = l / 2;
+    int i = 0;
+    auto ret = head;
+    while (i < mid) {
+      ret = ret->next;
+      ++i;
+    }
+    return ret;
+  }
+private:
+  int len(ListNode *head) {
+    int ret = 0;
+    while (head) {
+      ++ret;
+      head = head->next;
+    }
+
+    return ret;
+  }
+};
+
+int main() {
+  Solution so;
+  ListNode l1(1), l2(2), l3(3);
+  l1.next = &l2;
+  l2.next = &l3;
+  auto ret = so.middleNode(&l1);
+  cout << ret->val << endl;
+}
