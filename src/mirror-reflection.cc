7547d7f6fad10de500eb75ac2c1664d2004cc458
rongyi <sqrongyi@163.com>
2018-09-16 12:23:04 +0800

    Add mirrorReflection

---
+++ src/mirror-reflection.cc
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/mirror-reflection/description/
+
+#include "one.h"
+class Solution {
+public:
+  int mirrorReflection(int p, int q) {
+    int k = 1;
+    while (p * k % q != 0) {
+      ++k;
+    }
+    if (k & 1) {
+      auto d = p * k / q;
+      if (d & 1) {
+        return 1;
+      }
+      return 2;
+    }
+    return 0;
+  }
+};
