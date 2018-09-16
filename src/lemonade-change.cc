f9b781246e228eaa43d280a842953b704c9a9b9b
rongyi <sqrongyi@163.com>
2018-09-16 11:27:27 +0800

    Add lemonade change

---
+++ src/lemonade-change.cc
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/lemonade-change/description/
+
+#include "one.h"
+
+class Solution {
+public:
+  bool lemonadeChange(vector<int> &bills) {
+    int cnt5 = 0;
+    int cnt10 = 0;
+    const int n = bills.size();
+
+    for (int i = 0; i < n; ++i) {
+      if (bills[i] == 5) {
+        ++cnt5;
+      } else if (bills[i] == 10) {
+        ++cnt10;
+        if (cnt5 == 0) {
+          return false;
+        }
+        // 找人家5块钱
+        --cnt5;
+      } else {
+        // 一张10块，一张5块
+        if (cnt10 > 0 && cnt5 > 0) {
+          --cnt10;
+          --cnt5;
+        } else if (cnt5 >= 3) {
+          cnt5 -= 3;
+        } else {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+};
