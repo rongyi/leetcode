10f8123502040e29a1ead395d04e18b6d10981a8
rongyi <sqrongyi@163.com>
2018-09-16 14:14:23 +0800

    Add transpose matrix

---
+++ src/transpose-matrix.cc
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/transpose-matrix/description/
+#include "one.h"
+
+class Solution {
+public:
+  vector<vector<int>> transpose(vector<vector<int>> &A) {
+    const int m = A.size();
+    const int n = A[0].size();
+
+    vector<vector<int>> ret(n, vector<int>(m, 0));
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        ret[j][i] = A[i][j];
+      }
+    }
+
+    return ret;
+  }
+};
