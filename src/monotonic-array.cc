b5f8ba7bd20fecd1d795fdab558bf57e08d952bc
rongyi <sqrongyi@163.com>
2018-09-17 06:59:37 +0800

    Add monotonic array

---
+++ src/monotonic-array.cc
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/monotonic-array/description/
+#include "one.h"
+class Solution {
+public:
+  bool isMonotonic(vector<int> &A) {
+    return isIncrease(A) || isDecrease(A);
+  }
+
+private:
+  bool isIncrease(vector<int> &a) {
+    const int n = a.size();
+    int i = 0;
+    for (i = 0; i < n - 1;) {
+      if (a[i + 1] >= a[i]) {
+        ++i;
+      } else {
+        break;
+      }
+    }
+    return i == n - 1;
+  }
+
+  bool isDecrease(vector<int> &a) {
+    const int n = a.size();
+    int i = 0;
+    for (i = 0; i < n - 1;) {
+      if (a[i + 1] <= a[i]) {
+        i++;
+      } else {
+        break;
+      }
+    }
+    return i == n - 1;
+  }
+};
