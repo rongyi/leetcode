9001c01036ae4ce9089801251d1c383d4303b578
rongyi <sqrongyi@163.com>
2018-09-17 07:49:26 +0800

    Add smallest subtreeWithAllDeepest

---
+++ src/smallest-subtree-with-all-the-deepest-nodes.cc
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes/description/
+#include "one.h"
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+  TreeNode *subtreeWithAllDeepest(TreeNode *root) {
+    auto l = depth(root->left);
+    auto r = depth(root->right);
+    if (l == r) {
+      return root;
+    } else if (l > r) {
+      return subtreeWithAllDeepest(root->left);
+    }
+    return subtreeWithAllDeepest(root->right);
+  }
+  int depth(TreeNode *root) {
+    if (!root) {
+      return 0;
+    }
+    auto left = depth(root->left);
+    auto right = depth(root->right);
+
+    return max(left, right) + 1;
+  }
+};
