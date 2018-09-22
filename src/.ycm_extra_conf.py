505552c82f3afdac491554b7ac6ee701ba6b13d0
rongyi <sqrongyi@163.com>
2018-09-22 08:26:49 +0800

    Update local trash

--- src/.ycm_extra_conf.py
+++ src/.ycm_extra_conf.py
@@ -52,8 +52,8 @@ flags = [
 # For a C project, you would set this to something like 'c99' instead of
 # 'c++11'.
 '-std=c++11',
-# -'-stdlib=libstdc++',
-'-stdlib=libclang',  # need install libc++-dev
+'-stdlib=libstdc++',
+#'-stdlib=libclang',  # need install libc++-dev
 # ...and the same thing goes for the magic -x option which specifies the
 # language that the files to be compiled are written in. This is mostly
 # relevant for c++ headers.
