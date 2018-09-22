f2d64e8bb1f41757581e8c1a2cce6e27be657926
rongyi <sqrongyi@163.com>
2018-09-22 22:38:58 +0800

    Add more

---
+++ src/longest-substring-without-repeating-characters.cc
@@ -0,0 +1,22 @@
+// http://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+#include "one.h"
+
+class Solution {
+public:
+  int lengthOfLongestSubstring(string s) {
+    unordered_set<char> charset;
+    const int n = s.size();
+    int left = 0;
+    int ret = 0;
+    for (int i = 0; i < n; ) {
+      if (charset.find(s[i]) == charset.end()) {
+        charset.insert(s[i++]);
+        ret = max(ret, (int)charset.size());
+      } else {
+        charset.erase(s[left++]);
+      }
+    }
+
+    return ret;
+  }
+};
---
+++ src/median-of-two-sorted-arrays.cc
@@ -0,0 +1,15 @@
+// http://leetcode.com/problems/median-of-two-sorted-arrays/description/
+#include "one.h"
+
+class Solution {
+public:
+  double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
+    nums1.insert(nums1.end(), nums2.begin(), nums2.end());
+    sort(nums1.begin(), nums1.end());
+    auto n = nums1.size();
+    if (n % 2 == 1) {
+      return nums1[n / 2];
+    }
+    return ((double)(nums1[n / 2]) + (double)(nums1[n / 2 - 1]))/ 2;
+  }
+};
