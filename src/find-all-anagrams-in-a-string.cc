d4c2ad25a358597b6a7fe59a71efa75d49bbb3eb
rongyi <sqrongyi@163.com>
2018-09-22 10:22:33 +0800

    Add findAnagrams

---
+++ src/find-all-anagrams-in-a-string.cc
@@ -0,0 +1,35 @@
+// http://leetcode.com/problems/find-all-anagrams-in-a-string/description/
+#include "one.h"
+
+class Solution {
+public:
+  vector<int> findAnagrams(string s, string p) {
+    vector<int> ret;
+    // sort(p.begin(), p.end());
+    const int m = s.size();
+    const int n = p.size();
+    // using unordered_map tle?
+    vector<int> pmap(128, 0);
+
+    for (auto c : p) {
+      ++pmap[c];
+    }
+
+    for (int i = 0; i < m; ++i) {
+      bool succ = true;
+      auto cp = pmap;
+
+      for (int j = i; j < i + n; ++j) {
+        if (--cp[s[j]] < 0) {
+          succ = false;
+          break;
+        }
+      }
+
+      if (succ) {
+        ret.push_back(i);
+      }
+    }
+    return ret;
+  }
+};
