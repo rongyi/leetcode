4dddf9273691e7a10d2101768c0da14fd103281c
rongyi <sqrongyi@163.com>
2018-09-22 20:35:06 +0800

    Update two sum

--- src/scratch.cc
+++ src/scratch.cc
@@ -9,5 +9,40 @@ int main() {
       break;
     }
   }
-
 }
+struct Point {
+  int x;
+  int y;
+  Point() : x(0), y(0) {}
+  Point(int a, int b) : x(a), y(b) {}
+};
+
+class Solution {
+public:
+  vector<vector<int>> dirs{{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+  int numberofDistinctIslands(vector<vector<int>> &grid) {
+    int m = grid.size(), n = grid[0].size();
+    set<vector<pair<int, int>>> res;
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if (grid[i][j] != 1)
+          continue;
+        vector<pair<int, int>> v;
+        helper(grid, i, j, i, j, v);
+        res.insert(v);
+      }
+    }
+    return res.size();
+  }
+  void helper(vector<vector<int>> &grid, int x0, int y0, int i, int j,
+              vector<pair<int, int>> &v) {
+    int m = grid.size(), n = grid[0].size();
+    if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] <= 0)
+      return;
+    grid[i][j] *= -1;
+    v.push_back({i - x0, j - y0});
+    for (auto dir : dirs) {
+      helper(grid, x0, y0, i + dir[0], j + dir[1], v);
+    }
+  }
+};
---
+++ src/two-sum.cc
@@ -0,0 +1,25 @@
+// http://leetcode.com/problems/two-sum/description/
+#include "one.h"
+class Solution {
+public:
+  vector<int> twoSum(vector<int> &nums, int target) {
+    unordered_map<int, vector<int>> dict;
+    // remember the index;
+    for (int i = 0; i < nums.size(); ++i) {
+      dict[nums[i]].push_back(i);
+    }
+    for (auto kv : dict) {
+      auto delta = target - kv.first;
+      if (dict.find(delta) != dict.end()) {
+        if (delta == kv.first) {
+          if (kv.second.size() > 1) {
+            return vector<int>{kv.second[0], kv.second[1]};
+          }
+        } else {
+          return vector<int>{kv.second[0], dict[delta][0]};
+        }
+      }
+    }
+    return vector<int>{};
+  }
+};
