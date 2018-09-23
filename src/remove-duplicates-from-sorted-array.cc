8d22042192abd2d47a32fbf8ec904424202b046a
rongyi <sqrongyi@163.com>
2018-09-24 07:59:57 +0800

    Add remove duplicates

---
+++ src/remove-duplicates-from-sorted-array.cc
@@ -0,0 +1,21 @@
+// http://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+#include "one.h"
+
+class Solution {
+public:
+  int removeDuplicates(vector<int> &nums) {
+
+    const int n = nums.size();
+    if (n <= 1) {
+      return n;
+    }
+    // uniq index
+    int ui = 1;
+    for (int i = 1; i < n; ++i) {
+      if (nums[i] != nums[ui - 1]) {
+        nums[ui++] = nums[i];
+      }
+    }
+    return ui;
+  }
+};
