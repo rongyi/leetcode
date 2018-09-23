8b5b2daa8e08c71858c7345aa0b581fce4d5a13e
rongyi <sqrongyi@163.com>
2018-09-23 19:37:24 +0800

    Add more code

---
+++ src/generate-parentheses.cc
@@ -0,0 +1,25 @@
+// http://leetcode.com/problems/generate-parentheses/description/
+#include "one.h"
+
+class Solution {
+public:
+  vector<string> generateParenthesis(int n) {
+    vector<string> ret;
+    string s{""};
+    rec(n, n, s, ret);
+    return ret;
+  }
+
+private:
+  void rec(int left, int right, string cur, vector<string> &ret) {
+    if (left == 0 && right == 0) {
+      ret.push_back(cur);
+    }
+    if (left > 0) {
+      rec(left - 1, right, cur + "(", ret);
+    }
+    if (right > 0 && left < right) {
+      rec(left, right - 1, cur + ")", ret);
+    }
+  }
+};
---
+++ src/merge-k-sorted-lists.cc
@@ -0,0 +1,55 @@
+// http://leetcode.com/problems/leetcode/merge-k-sorted-lists/description/
+#include "one.h"
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+  ListNode *mergeKLists(vector<ListNode *> &lists) {
+    if (lists.empty()) {
+      return nullptr;
+    }
+    ListNode *p = lists[0];
+    for (int i = 1; i < lists.size(); ++i) {
+      p = mergeTwoLists(p, lists[i]);
+    }
+    return p;
+  }
+private:
+ ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+    ListNode dummy(-1);
+    ListNode *cur = &dummy;
+    auto p1 = l1;
+    auto p2 = l2;
+    while (p1 && p2) {
+      if (p1->val <= p2->val) {
+        cur->next = p1;
+        p1 = p1->next;
+      } else {
+        cur->next = p2;
+        p2 = p2->next;
+      }
+      cur = cur->next;
+    }
+    if (p1) {
+      cur->next = p1;
+    }
+    if (p2) {
+      cur->next = p2;
+    }
+
+    return dummy.next;
+  }
+};
