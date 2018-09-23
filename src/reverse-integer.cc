1fc2d845d6398f3b56c01354de8b82d6aacc7e03
rongyi <sqrongyi@163.com>
2018-09-23 08:22:30 +0800

    Add reverse integer

---
+++ src/reverse-integer.cc
@@ -0,0 +1,36 @@
+// http://leetcode.com/problems/reverse-integer/description/
+#include "one.h"
+
+class Solution {
+public:
+  int reverse(int x) {
+    long long i = x;
+    bool pos = true;
+    if (x < 0) {
+      pos = false;
+      i *= -1;
+    }
+    cout << i << endl;
+    long long ret = 0;
+    // auto max = numeric_limits<int>::max();
+    while (i) {
+      ret = ret * 10 + i % 10;
+      i /= 10;
+    }
+
+    if (ret > numeric_limits<int>::max()) {
+      return 0;
+    }
+    if (pos) {
+      return ret;
+    }
+
+    return -ret;
+  }
+};
+
+int main() {
+  Solution so;
+  auto ret = so.reverse(-123);
+  cout << ret << endl;
+}
