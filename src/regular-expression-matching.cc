271531476d7d86a7a81f0c30f5c07f2059f55f0d
rongyi <sqrongyi@163.com>
2018-09-23 12:50:08 +0800

    Add regular expression matching

--- src/regular-expression-matching.cc
+++ src/regular-expression-matching.cc
@@ -3,34 +3,36 @@
 
 class Solution {
 public:
-  bool isMatch(string s, string p) {
-    return isMatch(s.c_str(), p.c_str());
-  }
+  bool isMatch(string s, string p) { return is(s.c_str(), p.c_str()); }
+
 private:
-  bool isMatch(const char *s, const char *p) {
+  bool is(const char *s, const char *p) {
     if (*p == '\0') {
       return *s == '\0';
     }
-    // no star
+    // no star, one by one exactly or dot match
     if (*(p + 1) != '*') {
       if (*p == *s || (*p == '.' && *s != '\0')) {
-        return isMatch(s + 1, p + 1);
+        return is(s + 1, p + 1);
       }
 
       return false;
     } else {
       while (*p == *s || (*p == '.' && *s != '\0')) {
-        if (isMatch(s, p + 2)) {
+        if (is(s, p + 2)) {
           return true;
         }
+        // 这里就是1个或者多个的匹配
         s++;
       }
-      return isMatch(s, p + 2);
+      // 直接跳过，也即使用*的0个匹配，遇到不等的，那就从上面跳出来，使用后续exp匹配了。
+      return is(s, p + 2);
     }
   }
 };
 
 int main() {
   Solution so;
-
+  auto ret = so.isMatch("aab", "c*a*b");
+  cout << ret << endl;
 }
