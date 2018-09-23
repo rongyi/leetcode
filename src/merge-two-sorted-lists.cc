23fd00ede1513ffa6f05624e545bb8c271052a79
rongyi <sqrongyi@163.com>
2018-09-23 17:30:52 +0800

    Add mergeTwoLists

---
+++ src/merge-two-sorted-lists.cc
@@ -0,0 +1,44 @@
+// http://leetcode.com/problems/merge-two-sorted-lists/description/
+#include "one.h"
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+  ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+    ListNode dummy(-1);
+    ListNode *cur = &dummy;
+    auto p1 = l1;
+    auto p2 = l2;
+    while (p1 && p2) {
+      if (p1->val <= p2->val) {
+        cur->next = p1;
+        p1 = p1->next;
+      } else {
+        cur->next = p2;
+        p2 = p2->next;
+      }
+      cur = cur->next;
+    }
+    if (p1) {
+      cur->next = p1;
+    }
+    if (p2) {
+      cur->next = p2;
+    }
+
+    return dummy.next;
+  }
+};
