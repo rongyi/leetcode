4a803b997dbf231b298a350479226412b8707c36
rongyi <sqrongyi@163.com>
2018-09-23 17:03:22 +0800

    Add valid parentheses

---
+++ src/valid-parentheses.cc
@@ -0,0 +1,27 @@
+// http://leetcode.com/problems/valid-parentheses/description/
+#include "one.h"
+
+class Solution {
+public:
+  bool isValid(string s) {
+    const string left{"([{"};
+    const string right{")]}"};
+    stack<char> stk;
+    for (auto c : s) {
+      if (c == '(' || c == '[' || c == '{') {
+        stk.push(c);
+      } else {
+        if (stk.empty()) {
+          return false;
+        }
+        auto tmp = stk.top();
+        if (right.find(c) != left.find(tmp)) {
+          return false;
+        }
+        stk.pop();
+      }
+    }
+
+    return stk.empty();
+  }
+};
