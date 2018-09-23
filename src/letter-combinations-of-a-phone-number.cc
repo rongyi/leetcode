a63faacd100f5e6e62679fdadd3e548173ef9697
rongyi <sqrongyi@163.com>
2018-09-23 16:21:43 +0800

    Add letterCombinations

---
+++ src/letter-combinations-of-a-phone-number.cc
@@ -0,0 +1,88 @@
+// http://leetcode.com/problems/leetcode/letter-combinations-of-a-phone-number/description/
+#include "one.h"
+class Solution {
+public:
+  Solution() {
+    cd_map_.clear();
+
+    cd_map_.insert({2, vector<char>{'a', 'b', 'c'}});
+    cd_map_.insert({3, vector<char>{'d', 'e', 'f'}});
+    cd_map_.insert({4, vector<char>{'g', 'h', 'i'}});
+    cd_map_.insert({5, vector<char>{'j', 'k', 'l'}});
+    cd_map_.insert({6, vector<char>{'m', 'n', 'o'}});
+    cd_map_.insert({7, vector<char>{'p', 'q', 'r', 's'}});
+    cd_map_.insert({8, vector<char>{'t', 'u', 'v'}});
+    cd_map_.insert({9, vector<char>{'w', 'x', 'y', 'z'}});
+  }
+  vector<string> letterCombinations(string digits) {
+    const int n = digits.size();
+    vector<string> ret;
+    if (n == 0) {
+      return ret;
+    }
+    if (n == 1) {
+      vector<string> tmp;
+
+      return reduce(cd_map_[digits[0] - '0'], tmp);
+
+    } else if (digits.size() == 2) {
+      return map(cd_map_[digits[0] - '0'],
+                 cd_map_[digits[1] - '0']);
+    } else {
+      // lisp name convention
+      string cdr = digits.substr(1);
+      auto cdr_ret = letterCombinations(cdr);
+      return reduce(cd_map_[digits[0] - '0'], cdr_ret);
+    }
+
+    return ret;
+  }
+
+private:
+  vector<string> map(const vector<char> &left, const vector<char> &right) {
+    vector<string> ret;
+    for (const auto &l : left) {
+      string cur;
+      cur.push_back(l);
+      for (const auto &r : right) {
+        cur.push_back(r);
+        ret.push_back(cur);
+        cur.pop_back();
+      }
+    }
+
+    return ret;
+  }
+
+  vector<string> reduce(const vector<char> &left, vector<string> &acc) {
+    vector<string> ret;
+    for (const auto &l : left) {
+      string cur{l};
+      if (acc.empty()) {
+        ret.push_back(cur);
+        cur.pop_back();
+      } else {
+        for (const auto &s : acc) {
+          cur += s;
+          ret.push_back(cur);
+          cur.clear();
+          cur.push_back(l);
+        }
+      }
+    }
+
+    return ret;
+  }
+
+private:
+  // cellphone digit map
+  unordered_map<char, vector<char>> cd_map_;
+};
+
+int main() {
+  Solution so;
+  auto ret = so.letterCombinations("2");
+  for (auto s : ret) {
+    cout << s << endl;
+  }
+}
