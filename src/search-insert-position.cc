d86e7c9770c8d48ca25f0654a8d4413cea06cb8d
rongyi <sqrongyi@163.com>
2018-09-24 09:53:16 +0800

    Add search in insert

---
+++ src/search-insert-position.cc
@@ -0,0 +1,21 @@
+// http://leetcode.com/problems/search-insert-position/description/
+#include "one.h"
+class Solution {
+public:
+  int searchInsert(vector<int> &nums, int target) {
+    int first = 0;
+    int last = nums.size();
+    while (first < last) {
+      const int mid = first + (last - first) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] > target) {
+        last = mid;
+      } else {
+        first = mid + 1;
+      }
+    }
+    // now we have first == last
+    return first;
+  }
+};
