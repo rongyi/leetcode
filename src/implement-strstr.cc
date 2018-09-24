58b6dce9b92ad07694d77fd68a49747bfa3ea0e7
rongyi <sqrongyi@163.com>
2018-09-24 08:18:33 +0800

    Add strstr

---
+++ src/implement-strstr.cc
@@ -0,0 +1,35 @@
+// http://leetcode.com/problems/implement-strstr/description/
+#include "one.h"
+
+class Solution {
+public:
+  int strStr(string haystack, string needle) {
+    const int m = haystack.size();
+    const int n = needle.size();
+    if (n == 0) {
+      return 0;
+    }
+    if (m == 0 || n > m) {
+      return -1;
+    }
+    for (int i = 0; i < m; ) {
+      int start = i;
+      int j = 0;
+      while (i < m && j < n && needle[j] == haystack[i]) {
+        i++;
+        j++;
+      }
+      if (j == n) {
+        return start;
+      }
+      i = start + 1;
+    }
+    return -1;
+  }
+};
+
+int main() {
+  Solution so;
+  auto ret = so.strStr("hello", "ll");
+  cout << ret << endl;
+}
