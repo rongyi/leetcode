9aa9482bc5ca0e7d16c6cc9a6ab97f71eed66573
rongyi <sqrongyi@163.com>
2018-09-24 09:29:00 +0800

    Add longestValidParentheses

---
+++ src/longest-valid-parentheses.cc
@@ -0,0 +1,33 @@
+// http://leetcode.com/problems/longest-valid-parentheses/description/
+#include "one.h"
+class Solution {
+public:
+  int longestValidParentheses(string s) {
+    stack<int> stk;
+    int ret = 0;
+    int start = 0;
+    const int n = s.size();
+    for (int i = 0; i < n; ++i) {
+      if (s[i] == '(') {
+        stk.push(i);
+      } else {
+        // this is not valid
+        if (stk.empty()) {
+          start = i + 1;
+        } else {
+          stk.pop();
+          if (stk.empty()) {
+            ret = max(ret, i - start + 1);
+          } else {
+            ret = max(ret, i - stk.top());
+          }
+        }
+      }
+    }
+    return ret;
+  }
+};
+int main() {
+  Solution so;
+  so.longestValidParentheses(")()())");
+}
