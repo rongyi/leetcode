420ece106b5e951c53194b81fc1382ca15d02c67
rongyi <sqrongyi@163.com>
2018-09-24 08:08:19 +0800

    Update removeElement

---
+++ src/remove-element.cc
@@ -0,0 +1,15 @@
+// http://leetcode.com/problems/remove-element/description/
+#include "one.h"
+
+class Solution {
+public:
+  int removeElement(vector<int> &nums, int val) {
+    int ui = 0;
+    for (int i = 0; i < nums.size(); i++) {
+      if (nums[i] != val) {
+        nums[ui++] = nums[i];
+      }
+    }
+    return ui;
+  }
+};
