40f8d8fd9b672da03d5dbc95846cf7a996e29b8d
rongyi <sqrongyi@163.com>
2018-09-26 20:03:00 +0800

    Add jump game

---
+++ src/jump-game.cc
@@ -0,0 +1,24 @@
+// http://leetcode.com/problems/jump-game/description/
+#include "one.h"
+
+class Solution {
+public:
+  bool canJump(vector<int> &nums) {
+    const int n = nums.size();
+    int reach = 0;
+    for (int i = 0; i <= reach; ++i) {
+      reach = max(reach, nums[i] + i);
+      if (reach >= n - 1) {
+        return true;
+      }
+    }
+    return false;
+  }
+};
+
+int main() {
+  Solution so;
+  vector<int> input{3,2,1,0,4};
+  auto ret = so.canJump(input);
+  cout << ret << endl;
+}
