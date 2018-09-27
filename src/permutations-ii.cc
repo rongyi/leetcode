baeaad52b52affd67e3ab314406d800beafb0712
rongyi <sqrongyi@163.com>
2018-09-28 07:07:18 +0800

    Update ycmd and add permutations uniq

---
+++ src/permutations-ii.cc
@@ -0,0 +1,39 @@
+// http://www.lintcode.com/zh-cn/problem/leetcode/permutations-ii
+#include "one.h"
+
+class Solution {
+public:
+  vector<vector<int>> permuteUnique(vector<int> &nums) {
+
+    vector<vector<int>> ret;
+    if (nums.size() == 0) {
+      return ret;
+    }
+    vector<int> cur;
+    doPermute(nums, ret, cur, 0);
+
+    return ret;
+  }
+
+private:
+  void doPermute(vector<int> &nums, vector<vector<int>> &ret, vector<int> &cur,
+                 unsigned index) {
+    if (index == nums.size()) {
+      ret.push_back(cur);
+      return;
+    }
+    unordered_set<int> uniq;
+    for (unsigned i = index; i < nums.size(); ++i) {
+      if (uniq.find(nums[i]) == uniq.end()) {
+        uniq.insert(nums[i]);
+
+        swap(nums[index], nums[i]);
+        cur.push_back(nums[index]);
+        doPermute(nums, ret, cur, index + 1);
+        // restore
+        cur.pop_back();
+        swap(nums[index], nums[i]);
+      }
+    }
+  }
+};
