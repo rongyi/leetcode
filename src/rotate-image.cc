716ac3dd9ae0a67979932372e437ff829bb92753
rongyi <sqrongyi@163.com>
2018-09-28 08:00:42 +0800

    Add rotate image

---
+++ src/rotate-image.cc
@@ -0,0 +1,24 @@
+// http://www.lintcode.com/zh-cn/problem/rotate-image
+#include "one.h"
+
+class Solution {
+public:
+  void rotate(vector<vector<int>> &matrix) {
+    const int n = matrix.size();
+    int k = n - 1;
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < k; j++) {
+        auto ri = n - j - 1;
+        auto rj = n - i - 1;
+
+        swap(matrix[i][j], matrix[ri][rj]);
+      }
+      k--;
+    }
+    for (int i = 0; i < n / 2; ++i) {
+      for (int j = 0; j < n; ++j) {
+        swap(matrix[i][j], matrix[n - i - 1][j]);
+      }
+    }
+  }
+};
