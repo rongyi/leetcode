c65082b1f43d829fdf99c75814af73c92823687f
rongyi <sqrongyi@163.com>
2018-10-05 16:35:08 +0800

    Add search in 2d matrix ii

---
+++ src/search-a-2d-matrix-ii.cc
@@ -0,0 +1,33 @@
+// http://leetcode.com/problems/search-a-2d-matrix-ii/description/
+#include "one.h"
+
+class Solution {
+public:
+  bool searchMatrix(vector<vector<int>> &matrix, int target) {
+    const int m = matrix.size();
+    if (m == 0) {
+      return false;
+    }
+
+    const int n = matrix[0].size();
+    if (n == 0) {
+      return false;
+    }
+    // 这题没办法折半的，只能在O(n)的基础上快一点，一旦比一行的最小的还小，那肯定不在这一行上
+    // 最小值在左上角， 最大值在右下角
+    if (target < matrix[0][0] || target > matrix[m - 1][n - 1]) {
+      return false;
+    }
+    for (int i = m - 1, j = 0; i >= 0 && j < n;) {
+      if (matrix[i][j] == target) {
+        return true;
+      }
+      if (matrix[i][j] < target) {
+        j++;
+      } else {
+        i--;
+      }
+    }
+    return false;
+  }
+};
