076bb9b6eae3928a589a9e2647625c14005666e9
rongyi <sqrongyi@163.com>
2018-10-05 10:37:19 +0800

    Update edit distance

---
+++ src/edit-distance.cc
@@ -0,0 +1,39 @@
+// http://leetcode.com/problems/edit-distance/description/
+#include "one.h"
+
+class Solution {
+public:
+  int minDistance(string word1, string word2) {
+    if (word1.empty()) {
+      return word2.length();
+    }
+    if (word2.empty()) {
+      return word1.length();
+    }
+    const int m = word1.size();
+    const int n = word2.size();
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+    // 什么意思？就是把Word1变成空字符串的成本
+    for (int i = 0; i <= m; ++i) {
+      dp[i][0] = i;
+    }
+
+    // 同上
+    for (int j = 0; j <= n; ++j) {
+      dp[0][j] = j;
+    }
+
+    for (int i = 1; i <= m; ++i) {
+      for (int j = 1; j <= n; ++j) {
+        if (word1[i - 1] == word2[j - 1]) {
+          dp[i][j] = dp[i - 1][j - 1];
+        } else {
+          dp[i][j] = min(min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j - 1]) + 1;
+        }
+      }
+    }
+
+    return dp[m][n];
+  }
+};
