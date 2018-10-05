f5de466bd41ae28fd77af431bdebade734ea48b4
rongyi <sqrongyi@163.com>
2018-10-05 09:49:14 +0800

    Add simplifyPath

---
+++ src/simplify-path.cc
@@ -0,0 +1,57 @@
+// http://leetcode.com/problems/simplify-path/description/
+#include "one.h"
+
+class Solution {
+public:
+  string simplifyPath(string path) {
+    vector<string> ret;
+    const int n = path.size();
+    if (path[n - 1] == '/') {
+      path = path.substr(0, n - 1);
+    }
+    auto i = string::npos;
+    auto j = string::npos;
+    // 一直截取两个斜线之间的内容，然后判断
+    while (true) {
+      i = path.find('/');
+      if (i == string::npos) {
+        break;
+      }
+      j = path.find('/', i + 1);
+      string sub;
+      if (j == string::npos) {
+        // cout << path.substr(i + 1) << endl;
+        sub = path.substr(i + 1);
+      } else {
+        // cout << path.substr(i + 1, j - i - 1) << endl;
+        sub = path.substr(i + 1, j - i - 1);
+      }
+
+      if (sub == "..") {
+        if (ret.size() > 0) {
+          ret.pop_back();
+        }
+      } else if (sub != "." && sub != "") {
+        ret.push_back(sub);
+      }
+      path = path.substr(i + 1);
+    }
+    stringstream ss;
+    ss << "/";
+    for (int i = 0; i < ret.size(); ++i) {
+      ss << ret[i];
+      if (i != ret.size() - 1) {
+        ss << '/';
+      }
+    }
+
+    return ss.str();
+  }
+};
+int main() {
+  Solution so;
+  // string input{"/a/./b/../../c/"};
+  string input{"/abc/..."};
+  auto ret = so.simplifyPath(input);
+  cout << ret << endl;
+}
