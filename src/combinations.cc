6d845c7333b2502b8c9db523550c3e94b13e553e
rongyi <sqrongyi@163.com>
2018-10-05 17:45:43 +0800

    Add combinations

---
+++ src/combinations.cc
@@ -0,0 +1,31 @@
+// http://leetcode.com/problems/combinations/description/
+#include "one.h"
+
+class Solution {
+public:
+  vector<vector<int>> combine(int n, int k) {
+    vector<vector<int>> ret;
+    vector<int> cur;
+    combine(ret, cur, 1, n, k);
+
+    return ret;
+  }
+private:
+  void combine(vector<vector<int>> &ret, vector<int> cur, int index,
+               int n, int k){
+    if (cur.size() == k) {
+      ret.push_back(cur);
+      return;
+    }
+
+    if (index > n) {
+      return;
+    }
+    // don't pick current node
+    combine(ret, cur, index + 1, n, k);
+
+    // pick current node
+    cur.push_back(index);
+    combine(ret, cur, index + 1, n, k);
+  }
+};
