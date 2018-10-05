18fa53ec84ed789c6ad9e10118108d3aab65d9cf
rongyi <sqrongyi@163.com>
2018-10-05 17:57:36 +0800

    Add subset

---
+++ src/subsets.cc
@@ -0,0 +1,33 @@
+// http://leetcode.com/problems/subsets/description/
+#include "one.h"
+
+class Solution {
+public:
+  vector<vector<int>> subsets(vector<int> &nums) {
+    const int n = nums.size();
+    vector<vector<int>> ret;
+    for (int i = 0; i < (1 << n); ++i) {
+      vector<int> cur;
+      for (int j = 0; j < n; ++j) {
+        if ((i >> j) & 0x1) {
+          cur.push_back(nums[j]);
+        }
+      }
+
+      ret.push_back(cur);
+    }
+    return ret;
+  }
+};
+
+int main() {
+  Solution so;
+  vector<int> input{1, 2, 3};
+  auto ret = so.subsets(input);
+  for (auto v: ret) {
+    for (auto i : v) {
+      cout << i << " ";
+    }
+    cout << endl;
+  }
+}
