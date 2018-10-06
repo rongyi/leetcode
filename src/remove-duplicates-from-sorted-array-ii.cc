8b3ed54c1de90a127210a0de630d9ff739d47d86
rongyi <sqrongyi@163.com>
2018-10-06 09:22:00 +0800

    Add removeDuplicates ii

---
+++ src/remove-duplicates-from-sorted-array-ii.cc
@@ -0,0 +1,20 @@
+// http://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description/
+#include "one.h"
+
+class Solution {
+public:
+  int removeDuplicates(vector<int> &nums) {
+    const int n = nums.size();
+    if (n < 3) {
+      return n;
+    }
+    int idx = 2;
+    for (int i = 2; i < n; ++i) {
+      if (nums[i] != nums[idx - 2]) {
+        nums[idx++] = nums[i];
+      }
+    }
+
+    return idx;
+  }
+};
