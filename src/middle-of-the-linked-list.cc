681bca3c7229665149582c018478f8cd3446fef2
rongyi <sqrongyi@163.com>
2018-10-06 14:48:32 +0800

    Drop all link list node struct

--- src/middle-of-the-linked-list.cc
+++ src/middle-of-the-linked-list.cc
@@ -9,11 +9,6 @@
  * };
  */
 
-struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode(int x) : val(x), next(NULL) {}
-};
 
 class Solution {
 public:
--- src/remove-nth-node-from-end-of-list.cc
+++ src/remove-nth-node-from-end-of-list.cc
@@ -10,11 +10,6 @@
  * };
  */
 
-struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode(int x) : val(x), next(NULL) {}
-};
 
 class Solution {
 public:
