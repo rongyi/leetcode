879bd73f5970b770dde6f8108858759182a65acc
rongyi <sqrongyi@163.com>
2018-10-06 09:35:31 +0800

    Add search in rotated array

---
+++ src/search-in-rotated-sorted-array-ii.cc
@@ -0,0 +1,34 @@
+// http://leetcode.com/problems/leetcode/search-in-rotated-sorted-array-ii/description/
+#include "one.h"
+
+class Solution {
+public:
+  bool search(vector<int> &nums, int target) {
+    int first = 0;
+    int last = nums.size();
+    while (first != last) {
+      const int mid = first + (last - first) / 2;
+      if (nums[mid] == target) {
+        return true;
+      }
+      // in a normal range
+      if (nums[first] < nums[mid]) {
+        if (nums[first] <= target && target < nums[mid]) {
+          last = mid;
+        } else {
+          first = mid + 1;
+        }
+      } else if (nums[first] > nums[mid]) { // in a reversed range
+        if (nums[mid] < target && target <= nums[last - 1]) {
+          first = mid + 1;
+        } else {
+          last = mid;
+        }
+      } else {                  // they are equal, just ignore
+        first++;
+      }
+    }
+
+    return false;
+  }
+};
