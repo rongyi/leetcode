5de9e6b7fd19149c18d872ba8e4925b44b4b0493
rongyi <sqrongyi@163.com>
2018-10-07 20:51:33 +0800

    Add partition list

---
+++ src/partition-list.cc
@@ -0,0 +1,35 @@
+// http://leetcode.com/problems/partition-list/description/
+#include "one.h"
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode *partition(ListNode *head, int x) {
+    ListNode lt(-1);
+    ListNode gt(-1);
+    auto lcur = &lt;
+    auto gcur = &gt;
+    while (head) {
+      if (head->val < x) {
+        lcur->next = head;
+        lcur = lcur->next;
+      } else {
+        gcur->next = head;
+        gcur = gcur->next;
+      }
+      head = head->next;
+    }
+    // seal
+    gcur->next = nullptr;
+    // chain together
+    lcur->next = gt.next;
+
+    return lt.next;
+  }
+};
