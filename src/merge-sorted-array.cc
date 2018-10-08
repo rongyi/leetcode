062ded3bea91b53415363a3436db38f5fc2160bb
rongyi <sqrongyi@163.com>
2018-10-08 08:41:16 +0800

    Add merge sort

---
+++ src/merge-sorted-array.cc
@@ -0,0 +1,32 @@
+// http://leetcode.com/problems/merge-sorted-array/description/
+#include "one.h"
+
+class Solution {
+public:
+  void merge(vector<int> &nums1, int m, vector<int> &nums2, int n) {
+    auto cp1 = nums1;
+
+    auto idx = 0;
+    int i = 0;
+    int j = 0;
+    for (; i < m && j < n;) {
+      if (cp1[i] < nums2[j]) {
+        nums1[idx++] = cp1[i];
+        i++;
+      } else {
+        nums1[idx++] = nums2[j];
+        j++;
+      }
+    }
+    if (i < m) {
+      for (; i < m; ++i) {
+        nums1[idx++] = cp1[i];
+      }
+    }
+    if (j < n) {
+      for (; j < n; ++j) {
+        nums1[idx++] = nums2[j];
+      }
+    }
+  }
+};
