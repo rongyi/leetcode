4a158c2f14100e6103a1e17f81320166c7c1479f
rongyi <sqrongyi@163.com>
2018-10-09 06:50:37 +0800

    Add reverse link list

---
+++ src/reverse-linked-list.cc
@@ -0,0 +1,26 @@
+// http://leetcode.com/problems/leetcode/reverse-linked-list/description/
+#include "one.h"
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode *reverseList(ListNode *head) {
+    ListNode dummy(-1);
+    ListNode *cur = &dummy;
+    while (head) {
+      auto tmp = head->next;
+      head->next = cur->next;
+      cur->next = head;
+
+      head = tmp;
+    }
+    return dummy.next;
+  }
+};
