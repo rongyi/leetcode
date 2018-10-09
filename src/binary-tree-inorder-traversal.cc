c3c967a2ef8db9d325d1c44ecc0c77f769b9bf21
rongyi <sqrongyi@163.com>
2018-10-10 06:05:02 +0800

    Add binary preorder and inorder traversal

---
+++ src/binary-tree-inorder-traversal.cc
@@ -0,0 +1,48 @@
+// http://leetcode.com/problems/leetcode/binary-tree-inorder-traversal/description/
+#include "one.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  vector<int> inorderTraversalRecursive(TreeNode *root) {
+    vector<int> ret;
+    action(root, ret);
+    return ret;
+  }
+  vector<int> inorderTraversal(TreeNode *root) {
+    stack<TreeNode *> stk;
+    TreeNode *p = root;
+    vector<int> ret;
+    while (!stk.empty() || p != nullptr) {
+      if (p != nullptr) {
+        stk.push(p);
+        p = p->left;
+      } else {
+        p = stk.top();
+        stk.pop();
+        ret.push_back(p->val);
+        p = p->right;
+      }
+    }
+    return ret;
+  }
+
+private:
+  void action(TreeNode *root, vector<int> &ret) {
+    if (!root) {
+      return;
+    }
+
+    action(root->left, ret);
+    ret.push_back(root->val);
+    action(root->right, ret);
+  }
+};
---
+++ src/binary-tree-preorder-traversal.cc
@@ -0,0 +1,28 @@
+// http://leetcode.com/problems/binary-tree-preorder-traversal/description/
+#include "one.h"
+
+class Solution {
+public:
+  vector<int> preorderTraversal(TreeNode *root) {
+    vector<int> ret;
+    if (!root) {
+      return ret;
+    }
+    stack<TreeNode *> stk;
+    stk.push(root);
+    while (!stk.empty()) {
+      auto p = stk.top();
+      stk.pop();
+      ret.push_back(p->val);
+
+      if (p->right) {
+        stk.push(p->right);
+      }
+      if (p->left) {
+        stk.push(p->left);
+      }
+    }
+
+    return ret;
+  }
+};
