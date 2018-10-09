29ee51a6cdabb04681a232ab441de8fae810029a
rongyi <sqrongyi@163.com>
2018-10-09 22:09:23 +0800

    Add validate binary search tree

---
+++ src/validate-binary-search-tree.cc
@@ -0,0 +1,31 @@
+// http://leetcode.com/problems/validate-binary-search-tree/description/
+
+#include "one.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  bool isValidBST(TreeNode *root) {
+    long long left = numeric_limits<long long>::min();
+    long long right = numeric_limits<long long>::max();
+    return pressure(root, left, right);
+  }
+
+private:
+  bool pressure(TreeNode *root, long long left, long long right) {
+    if (!root) {
+      return true;
+    }
+    return root->val < right && root->val > left &&
+           pressure(root->left, left, root->val) &&
+           pressure(root->right, root->val, right);
+  }
+};
