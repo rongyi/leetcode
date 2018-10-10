6cd892715d403bd68f9871309cd1bf2e471a6ff2
rongyi <sqrongyi@163.com>
2018-10-10 20:47:19 +0800

    Add comment

--- src/unique-binary-search-trees.cc
+++ src/unique-binary-search-trees.cc
@@ -3,11 +3,16 @@
 class Solution {
 public:
   int numTrees(int n) {
+    // 除掉当前根节点还余下n - 1个，
+    // ==> 全放左边的*右边为空 f(n - 1) * f(0)
+    // ==> f(n - 2) * f(1)
+    // ...
+    // 得到公式
     // f(n) = f(n-1)f(0) + f(n-2)f(1) + f(n-3)f(2) + ... + f(1)f(n-2) +  f(n-1)f(0)
     // ​      i
     // f(i)=∑   f(k−1)xf(i−k)
     // ​      k=1
-    //
+
     vector<int> ret(n + 1, 0);
     ret[0] = 1;
     ret[1] = 1;
