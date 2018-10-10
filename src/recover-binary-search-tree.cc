1a2173b6ab74c740d71aed1da46f772cdb3580b3
rongyi <sqrongyi@163.com>
2018-10-11 06:33:10 +0800

    Add recover binary search tree

---
+++ src/recover-binary-search-tree.cc
@@ -0,0 +1,39 @@
+// http://leetcode.com/problems/recover-binary-search-tree/description/
+#include "one.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  void recoverTree(TreeNode *root) {
+    vector<TreeNode *> ps;
+    vector<int> vs;
+
+    inorder(root, ps, vs);
+    sort(vs.begin(), vs.end());
+
+    for (int i = 0; i < ps.size(); ++i) {
+      ps[i]->val = vs[i];
+    }
+  }
+
+private:
+  void inorder(TreeNode *root, vector<TreeNode *> &ps, vector<int> &vs) {
+    if (!root) {
+      return;
+    }
+    inorder(root->left, ps, vs);
+
+    ps.push_back(root);
+    vs.push_back(root->val);
+
+    inorder(root->right, ps, vs);
+  }
+};
