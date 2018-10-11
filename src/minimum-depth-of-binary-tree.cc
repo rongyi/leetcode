8417e07014d1c296ad2cc904d5cfdafb5cac06ee
rongyi <sqrongyi@163.com>
2018-10-12 06:49:15 +0800

    Add minimum height

---
+++ src/minimum-depth-of-binary-tree.cc
@@ -0,0 +1,36 @@
+// http://leetcode.com/problems/minimum-depth-of-binary-tree/description/
+#include "one.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  int minDepth(TreeNode *root) {
+    if (!root) {
+      return 0;
+    }
+    int ret = numeric_limits<int>::max();
+    dfs(root, ret, 1);
+    return ret;
+  }
+
+  void dfs(TreeNode *root, int &ret, int cur) {
+    if (!root) {
+      return;
+    }
+    // a leaf, and check
+    if (!root->left && !root->right) {
+      ret = min(ret, cur);
+      return;
+    }
+    dfs(root->left, ret, cur + 1);
+    dfs(root->right, ret, cur + 1);
+  }
+};
