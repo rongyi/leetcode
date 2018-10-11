9617b82ee3a8920d24eadf0ac2ae4d2d3349a5f8
rongyi <sqrongyi@163.com>
2018-10-12 06:33:53 +0800

    Add maximum depth

---
+++ src/maximum-depth-of-binary-tree.cc
@@ -0,0 +1,23 @@
+// http://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+#include "one.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  int maxDepth(TreeNode *root) {
+    if (!root) {
+      return 0;
+    }
+    auto left = maxDepth(root->left);
+    auto right = maxDepth(root->right);
+    return max(left, right) + 1;
+  }
+};
