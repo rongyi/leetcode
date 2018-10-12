3c4cb708921d4cfc84dfad600f18a348f155f07e
rongyi <sqrongyi@163.com>
2018-10-12 16:23:02 +0800

    Add path sum iii

---
+++ src/path-sum-iii.cc
@@ -0,0 +1,26 @@
+// http://leetcode.com/problems/path-sum-iii/description/
+#include "one.h"
+
+class Solution {
+public:
+  int pathSum(TreeNode *root, int sum) {
+    if (!root) {
+      return 0;
+    }
+
+    return dfs(root, sum, 0) + pathSum(root->left, sum) + pathSum(root->right, sum);
+  }
+
+private:
+  int dfs(TreeNode *root, const int target, int cur) {
+    if (!root) {
+      return 0;
+    }
+    // plus current node
+    cur += root->val;
+    if (cur == target) {
+      return 1 + dfs(root->left, target, cur) + dfs(root->right, target, cur);
+    }
+    return dfs(root->left, target, cur) + dfs(root->right, target, cur);
+  }
+};
