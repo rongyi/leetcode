0398dc01fb063c817b603edbd6317b30e225553e
rongyi <sqrongyi@163.com>
2018-10-14 07:11:19 +0800

    Add best time to sell stock

---
+++ src/best-time-to-buy-and-sell-stock.cc
@@ -0,0 +1,20 @@
+// http://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+#include "one.h"
+
+class Solution {
+public:
+  int maxProfit(vector<int> &prices) {
+    const int n = prices.size();
+    int ret = 0;
+    int cur_max = 0;
+    for (int i = n - 2; i >= 0; --i) {
+      cur_max = max(cur_max, prices[i + 1]);
+      // 比当前节点大，说明从这点买进可以在后续某个时间点卖出
+      if (cur_max > prices[i]) {
+        ret = max(ret, cur_max - prices[i]);
+      }
+    }
+
+    return ret;
+  }
+};
