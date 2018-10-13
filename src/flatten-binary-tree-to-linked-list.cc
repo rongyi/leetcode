476410a73345452a7e4755584d3090ccaefef465
rongyi <sqrongyi@163.com>
2018-10-13 08:10:33 +0800

    Add flatten binary tree to linked list

---
+++ src/flatten-binary-tree-to-linked-list.cc
@@ -0,0 +1,30 @@
+// http://leetcode.com/problems/flatten-binary-tree-to-linked-list/description/
+#include "one.h"
+
+class Solution {
+public:
+  void flatten(TreeNode *root) {
+    if (!root) {
+      return;
+    }
+    if (root->left) {
+      flatten(root->left);
+    }
+    if (root->right) {
+      flatten(root->right);
+    }
+    // insert left to right before the real right
+    // cache real right
+    auto *tmp = root->right;
+    root->right = root->left;
+    root->left = nullptr;
+    // now chain the last to real right
+    // find the last node
+    while (root->right) {
+      root = root->right;
+    }
+    root->right = tmp;
+  }
+
+
+};
