ab3d0e58b457ce4371b591f6d0d38f5c8afa5058
rongyi <sqrongyi@163.com>
2018-10-13 15:29:55 +0800

    Add convert sorted array to binary tree

---
+++ src/convert-sorted-array-to-binary-search-tree.cc
@@ -0,0 +1,34 @@
+// http://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/description/
+#include "one.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  TreeNode *sortedArrayToBST(vector<int> &nums) {
+    const int n = nums.size();
+
+    return doSort(nums, 0, n);
+  }
+
+private:
+  TreeNode *doSort(vector<int> &nums, int begin, int end) {
+    const auto len = end - begin;
+    if (len <= 0) {
+      return nullptr;
+    }
+    auto mid = begin + len / 2;
+    TreeNode *root = new TreeNode(nums[mid]);
+    root->left = doSort(nums, begin, mid);
+    root->right = doSort(nums, mid + 1, end);
+
+    return root;
+  }
+};
