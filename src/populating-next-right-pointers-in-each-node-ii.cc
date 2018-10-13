8a40d47a490b34f417050ded884f3dbd5d97eabb
rongyi <sqrongyi@163.com>
2018-10-13 17:22:49 +0800

    Add populating link node tree

---
+++ src/populating-next-right-pointers-in-each-node-ii.cc
@@ -0,0 +1,19 @@
+// http://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/description/
+#include "one.h"
+
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  void connect(TreeLinkNode *root) {
+    if (!root) {
+      return;
+    }
+  }
+};
