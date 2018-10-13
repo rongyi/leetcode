fcc2d29f6c99b5e1a1088902600eed162bfd679f
rongyi <sqrongyi@163.com>
2018-10-13 17:53:42 +0800

    Add pascals triangle

---
+++ src/pascals-triangle-ii.cc
@@ -0,0 +1,23 @@
+// http://leetcode.com/problems/pascals-triangle-ii/description/
+#include "one.h"
+
+class Solution {
+public:
+  vector<int> getRow(int rowIndex) {
+    vector<int> row(rowIndex + 1, 1);
+    // consider this:
+    //  1 3 3 1 0
+    //+ 0 1 3 3 1
+    // --------------
+    //= 1 4 6 4 1
+    for (int i = 1; i <= rowIndex; ++i) {
+      int prev = 1;
+      for (int j = 1; j < i; j++) {
+        auto tmp = row[j];
+        row[j] += prev;
+        prev = tmp;
+      }
+    }
+    return row;
+  }
+};
