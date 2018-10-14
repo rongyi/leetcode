a423a9293d49ad44e95dab26d94b4cb73dcc893e
rongyi <sqrongyi@163.com>
2018-10-14 10:51:00 +0800

    Add link list cycle

---
+++ src/linked-list-cycle.cc
@@ -0,0 +1,23 @@
+// http://leetcode.com/problems/linked-list-cycle/description/
+#include "one.h"
+
+class Solution {
+public:
+  bool hasCycle(ListNode *head) {
+
+    if (!head)
+      return false;
+    if (!head->next || !head->next->next)
+      return false;
+    ListNode *slow = head;
+    ListNode *fast = head;
+    while (slow && fast && fast->next) {
+      slow = slow->next;
+      fast = fast->next->next;
+      if (slow == fast) {
+        return true;
+      }
+    }
+    return false;
+  }
+};
