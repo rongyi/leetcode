90e63bb56765a7f106010b701f4905171618e014
rongyi <sqrongyi@163.com>
2018-10-15 06:57:46 +0800

    Add gas station

---
+++ src/gas-station.cc
@@ -0,0 +1,23 @@
+// http://leetcode.com/problems/gas-station/description/
+#include "one.h"
+
+class Solution {
+public:
+  int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+    int start = -1;
+    int sum = 0;
+    int cur = 0;
+    for (int i = 0; i < gas.size(); i++) {
+      cur += gas[i] - cost[i];
+
+      // 不管从哪里开始兜一圈的油耗总和不变，
+      sum += gas[i] - cost[i];
+      // 表示从前面某个点开始到当前节点“够不着”
+      if (cur < 0) {
+        start = i;
+        cur = 0;
+      }
+    }
+    return sum >= 0 ? start + 1 : -1;
+  }
+};
