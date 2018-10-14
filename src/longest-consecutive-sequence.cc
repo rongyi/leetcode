0cffcaad4ccb091e0859fd2add91fdad8f02854d
rongyi <sqrongyi@163.com>
2018-10-14 11:58:35 +0800

    Add longestConsecutive

---
+++ src/longest-consecutive-sequence.cc
@@ -0,0 +1,32 @@
+// http://leetcode.com/problems/longest-consecutive-sequence/description/
+#include "one.h"
+
+class Solution {
+public:
+  int longestConsecutive(vector<int> &nums) {
+    int ret = 0;
+    unordered_set<int> dict;
+    for (auto i : nums) {
+      dict.insert(i);
+    }
+
+    for (int i = 0; i < nums.size(); ++i) {
+      int cur_range = 0;
+      if (dict.find(nums[i]) != dict.end()) {
+        cur_range++;
+        dict.erase(nums[i]);
+      }
+      for (int j = nums[i] + 1; dict.find(j) != dict.end(); ++j) {
+        cur_range++;
+        dict.erase(j);
+      }
+      for (int j = nums[i] - 1; dict.find(j) != dict.end(); --j) {
+        cur_range++;
+        dict.erase(j);
+      }
+      ret = max(cur_range, ret);
+    }
+
+    return ret;
+  }
+};
