5c9fc6bf6e0588fff224009c7de4d9520e81fbe9
rongyi <sqrongyi@163.com>
2018-10-15 07:14:19 +0800

    Add candy

---
+++ src/candy.cc
@@ -0,0 +1,34 @@
+// http://leetcode.com/problems/candy/description/
+#include "one.h"
+
+class Solution {
+public:
+  int candy(vector<int> &ratings) {
+    const int n = ratings.size();
+    if (n == 0) {
+      return 0;
+    }
+    // 这里算的是相对值
+    vector<int> peak(n, 0);
+    // 多的情况下至少比别人多一块
+    int candy = 1;
+    for (int i = 1; i < n; i++) {
+      if (ratings[i] > ratings[i - 1]) {
+        peak[i] = candy++;
+      } else {
+        // 最少的一块
+        candy = 1;
+      }
+    }
+    candy = 1;
+    for (int i = n - 2; i >= 0; i--) {
+      if (ratings[i] > ratings[i + 1]) {
+        peak[i] = max(candy++, peak[i]);
+      } else {
+        candy = 1;
+      }
+    }
+    // 每个小朋友至少一块，分配n块
+    return accumulate(peak.begin(), peak.end(), n);
+  }
+};
