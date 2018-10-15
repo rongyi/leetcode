cf0021043a08d9b3aa9d0a09d9a10653768e601e
rongyi <sqrongyi@163.com>
2018-10-16 07:39:40 +0800

    Add linked list cycle ii

---
+++ src/linked-list-cycle-ii.cc
@@ -0,0 +1,24 @@
+// http://leetcode.com/problems/leetcode/linked-list-cycle-ii/description/
+#include "one.h"
+
+class Solution {
+public:
+  ListNode *detectCycle(ListNode *head) {
+    auto *slow = head;
+    auto *fast = head;
+    while (fast && fast->next) {
+      slow = slow->next;
+      fast = fast->next->next;
+      if (slow == fast) {
+        auto *slow2 = head;
+        while (slow2 != slow) {
+          slow2 = slow2->next;
+          slow = slow->next;
+        }
+        return slow2;
+      }
+    }
+
+    return nullptr;
+  }
+};
