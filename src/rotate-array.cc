3b9ca984111359d27ee6c3fb8b5f3d7454ddc64d
rongyi <sqrongyi@163.com>
2018-10-16 08:03:19 +0800

    Add rotate array

---
+++ src/rotate-array.cc
@@ -0,0 +1,32 @@
+// http://leetcode.com/problems/rotate-array/description/
+#include "one.h"
+
+class Solution {
+public:
+  void rotate(vector<int> &nums, int k) {
+    const int n = nums.size();
+    if (n == 0) {
+      return;
+    }
+    k %= n;
+    if (k == 0) {
+      return;
+    }
+    for (int i = n - k, compensate = 0; i < n; ++i) {
+      nums.insert(nums.begin() + compensate, nums[i + compensate]);
+      compensate++;
+    }
+    nums.resize(n);
+  }
+};
+
+int main() {
+  Solution so;
+  vector<int> input{1, 2, 3, 4, 5, 6, 7};
+
+  so.rotate(input, 3);
+  for (auto i : input) {
+    cout << i << " ";
+  }
+  cout << endl;
+}
