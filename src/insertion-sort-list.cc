9c57de55e0f4b04418dd334d047f9f3958950305
rongyi <sqrongyi@163.com>
2018-10-19 07:27:11 +0800

    Add insert list

---
+++ src/insertion-sort-list.cc
@@ -0,0 +1,28 @@
+// http://leetcode.com/problems/insertion-sort-list/description/
+#include "one.h"
+
+class Solution {
+public:
+  ListNode *insertionSortList(ListNode *head) {
+    if (!head) {
+      return nullptr;
+    }
+
+    ListNode dummy(-1);
+    ListNode *cur = &dummy;
+    auto *p = head;
+    while (p) {
+      auto *next = p->next;
+      cur = &dummy;
+      while (cur->next && cur->next->val < p->val) {
+        cur = cur->next;
+      }
+      p->next = cur->next;
+      cur->next = p;
+
+      p = next;
+    }
+
+    return dummy.next;
+  }
+};
