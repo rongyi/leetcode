8586e3e6a0ea8bbd1932e014395cf6c0d32f4805
rongyi <sqrongyi@163.com>
2018-10-20 10:17:43 +0800

    Update majorityElement

--- src/majority-element.cc
+++ src/majority-element.cc
@@ -8,6 +8,9 @@ public:
     unordered_map<int, int> count;
     for (auto i : nums) {
       ++count[i];
+      if (count[i] > n) {
+        return i;
+      }
     }
     for (auto kv : count) {
       if (count[kv.first] > n) {
