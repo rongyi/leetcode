4b3b88eeb99db5b90765e0e9adeffc7bfec9bc51
rongyi <sqrongyi@163.com>
2018-10-20 14:57:52 +0800

    Add aced problems

---
+++ src/find-minimum-in-rotated-sorted-array.cc
@@ -0,0 +1,27 @@
+// http://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+#include "one.h"
+
+class Solution {
+public:
+  // we prefer the recursive version
+  int findMin(vector<int> &nums) { return dofind(nums, 0, nums.size() - 1); }
+
+private:
+  int dofind(vector<int> &nums, int left, int right) {
+    if (right == left) {
+      return nums[left];
+    }
+    if (right == left + 1) {
+      return min(nums[left], nums[right]);
+    }
+    int mid = left + (right - left) / 2;
+    if (nums[right] > nums[left]) {
+      return nums[left];
+    } else if (nums[right] == nums[left]) {
+      return dofind(nums, left + 1, right);
+    } else if (nums[mid] >= nums[left]) {
+      return dofind(nums, mid, right);
+    }
+    return dofind(nums, left, mid);
+  }
+};
---
+++ src/intersection-of-two-linked-lists.cc
@@ -0,0 +1,43 @@
+// http://leetcode.com/problems/intersection-of-two-linked-lists/description/
+#include "one.h"
+
+class Solution {
+public:
+  ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    int la = len(headA);
+    int lb = len(headB);
+    auto *p = headA;
+    auto *q = headB;
+    if (la > lb) {
+      auto diff = la - lb;
+      while (diff) {
+        p = p->next;
+        diff--;
+      }
+    } else if (la < lb) {
+      auto diff = lb - la;
+      while (diff) {
+        q = q->next;
+        diff--;
+      }
+    }
+    while (p && q) {
+      if (p == q) {
+        return p;
+      }
+      p = p->next;
+      q = q->next;
+    }
+    return nullptr;
+  }
+
+private:
+  int len(ListNode *head) {
+    int ret = 0;
+    while (head) {
+      ++ret;
+      head = head->next;
+    }
+    return ret;
+  }
+};
