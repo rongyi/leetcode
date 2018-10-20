6b9d81551adebc614c6a346f78e4fc5d7c6c0525
rongyi <sqrongyi@163.com>
2018-10-20 16:52:02 +0800

    Update maximumGap

--- src/find-peak-element.cc
+++ src/find-peak-element.cc
@@ -4,6 +4,7 @@
 class Solution {
 public:
   int findPeakElement(vector<int> &nums) {
+    // 看成是一个递增序列，如果出现下坡，则前面这个是最大值
     for (int i = 1; i < nums.size(); i++) {
       if (nums[i] < nums[i - 1]) {
         return i - 1;
---
+++ src/maximum-gap.cc
@@ -0,0 +1,17 @@
+// http://leetcode.com/problems/maximum-gap/description/
+#include "one.h"
+
+class Solution {
+public:
+  int maximumGap(vector<int> &nums) {
+    if (nums.size() < 2) {
+      return 0;
+    }
+    sort(nums.begin(), nums.end());
+    int ret = 0;
+    for (int i = 1; i < nums.size();  i++) {
+      ret = max(ret, nums[i] - nums[i - 1]);
+    }
+    return ret;
+  }
+};
