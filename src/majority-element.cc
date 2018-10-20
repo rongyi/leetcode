75e5fe44b4d4c30402c04110eaacb0191db48fe3
rongyi <sqrongyi@163.com>
2018-10-20 10:16:42 +0800

    Update majorityElement

---
+++ src/majority-element.cc
@@ -0,0 +1,19 @@
+// http://leetcode.com/problems/majority-element/description/
+#include "one.h"
+
+class Solution {
+public:
+  int majorityElement(vector<int> &nums) {
+    const int n = nums.size() / 2;
+    unordered_map<int, int> count;
+    for (auto i : nums) {
+      ++count[i];
+    }
+    for (auto kv : count) {
+      if (count[kv.first] > n) {
+        return kv.first;
+      }
+    }
+    return -1;
+  }
+};
