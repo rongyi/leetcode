9f7d0e9e75211b389c15433eb683ca54bb79138a
rongyi <sqrongyi@163.com>
2018-10-22 07:22:04 +0800

    Update excel sheet column number

---
+++ src/excel-sheet-column-number.cc
@@ -0,0 +1,19 @@
+// http://leetcode.com/problems/excel-sheet-column-number/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  int titleToNumber(string s) {
+    unordered_map<char, int> table;
+    int i = 1;
+    for (int j = 0; j < 26; j++) {
+      table['A' + j] = i++;
+    }
+
+    int ret = 0;
+    for (auto c: s) {
+      ret = ret * 26 + table[c];
+    }
+    return ret;
+  }
+};
---
+++ src/fraction-to-recurring-decimal.cc
@@ -0,0 +1,44 @@
+// http://leetcode.com/problems/fraction-to-recurring-decimal/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  string fractionToDecimal(int numerator, int denominator) {
+    if (numerator == 0) {
+      return "0";
+    }
+    string ret;
+
+    if (numerator < 0 ^ denominator < 0) {
+      ret += '-';
+    }
+    // make it in large range
+    long long n = numerator;
+    n = llabs(n);
+    long long d = denominator;
+    d = llabs(d);
+
+    ret += to_string(n / d);
+    long long int r = n % d;
+    if (r == 0) {
+      return ret;
+    } else {
+      ret += '.';
+    }
+    // fraction part
+    unordered_map<int, int> cache;
+    while (r) {
+      if (cache.find(r) != cache.end()) {
+        ret.insert(cache[r], 1, '(');
+        ret += ')';
+        break;
+      }
+      cache[r] = ret.size();
+
+      r *= 10;
+      ret += to_string(r / d);
+      r = r % d;
+    }
+    return ret;
+  }
+};
