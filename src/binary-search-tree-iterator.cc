bf5de82aa56d49f9346c1dd118c6837426fb6e95
rongyi <sqrongyi@163.com>
2018-10-22 07:35:05 +0800

    Add binary search iterator

---
+++ src/binary-search-tree-iterator.cc
@@ -0,0 +1,40 @@
+// http://leetcode.com/problems/binary-search-tree-iterator/description/
+#include "simpleone.h"
+
+class BSTIterator {
+public:
+  BSTIterator(TreeNode *root) {
+    while (root) {
+      s_.push(root);
+      root = root->left;
+    }
+  }
+
+  /** @return whether we have a next smallest number */
+  bool hasNext() {
+    return !s_.empty();
+  }
+
+  /** @return the next smallest number */
+  int next() {
+    TreeNode *cur = s_.top();
+    s_.pop();
+    TreeNode *ret = cur;
+    if (cur->right) {
+      cur = cur->right;
+      while (cur) {
+        s_.push(cur);
+        cur = cur->left;
+      }
+    }
+    return ret->val;
+  }
+private:
+  stack<TreeNode *> s_;
+};
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = BSTIterator(root);
+ * while (i.hasNext()) cout << i.next();
+ */
