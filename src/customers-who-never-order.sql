35daac5497479b87dbce511b068f015cc7ae85b8
rongyi <sqrongyi@163.com>
2018-10-22 07:45:49 +0800

    Add customers never order

---
+++ src/customers-who-never-order.sql
@@ -0,0 +1,4 @@
+-- http://leetcode.com/problems/customers-who-never-order/description/
+
+# Write your MySQL query statement below
+select Name as Customers from Customers where Id not in (select CustomerId from Orders)
