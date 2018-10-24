d29bb067454b09b001ba9f10c7d768209388d083
rongyi <sqrongyi@163.com>
2018-10-25 07:21:26 +0800

    Update nth highest salary

---
+++ src/nth-highest-salary.sql
@@ -0,0 +1,9 @@
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  SET N = N - 1;
+  RETURN (
+      SELECT DISTINCT Salary FROM Employee GROUP BY Salary
+      ORDER BY Salary DESC LIMIT 1 OFFSET N
+  );
+END
