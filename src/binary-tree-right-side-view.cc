19a6a415e90e1eade4af9afee97a5cedad6abab7
rongyi <sqrongyi@163.com>
2018-10-27 10:37:07 +0800

    Update right side view

---
+++ src/binary-tree-right-side-view.cc
@@ -0,0 +1,51 @@
+// http://leetcode.com/problems/binary-tree-right-side-view/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  vector<int> rightSideView(TreeNode *root) {
+    vector<int> ret;
+    auto level_list = levelOrderBottom(root);
+    for (auto &level: level_list) {
+      ret.push_back(level.back());
+    }
+    return ret;
+  }
+private:
+  void recursive(TreeNode *root, vector<int> &ret) {
+    if (!root) {
+      return;
+    }
+    ret.push_back(root->val);
+    if (root->right) {
+      recursive(root->right, ret);
+    } else {
+      recursive(root->left, ret);
+    }
+  }
+
+vector<vector<int>> levelOrderBottom(TreeNode *root) {
+    vector<vector<int>> ret;
+    if (!root) {
+      return ret;
+    }
+    queue<TreeNode *> q;
+    q.push(root);
+    while (!q.empty()) {
+      vector<int> cur;
+      int cur_cnt = q.size();
+      for (int i = 0; i < cur_cnt; ++i) {
+        cur.push_back(q.front()->val);
+        if (q.front()->left) {
+          q.push(q.front()->left);
+        }
+        if (q.front()->right) {
+          q.push(q.front()->right);
+        }
+        q.pop();
+      }
+      ret.push_back(cur);
+    }
+    return ret;
+  }
+};
--- src/populating-next-right-pointers-in-each-node-ii.cc
+++ src/populating-next-right-pointers-in-each-node-ii.cc
@@ -9,11 +9,32 @@
  *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
  * };
  */
+
 class Solution {
 public:
   void connect(TreeLinkNode *root) {
     if (!root) {
       return;
     }
+    TreeLinkNode dummy(-1);
+
+    TreeLinkNode *cur = nullptr;
+    TreeLinkNode *prev = &dummy;
+
+    // 在这里过桥，桥在上一层遍历时被修好
+    for (cur = root; cur; cur = cur->next) {
+      if (cur->left) {
+        // 这里修桥，为下一层遍历准备
+        prev->next = cur->left;
+        prev = prev->next;
+      }
+      if (cur->right) {
+        // 这里修桥
+        prev->next = cur->right;
+        prev = prev->next;
+      }
+    }
+    // 显然就是下一层的第一个节点
+    connect(dummy.next);
   }
 };
