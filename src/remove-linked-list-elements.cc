e04175ba13a50548742bb74a8d1d13d78f8a7e7a
rongyi <sqrongyi@163.com>
2018-10-27 16:10:41 +0800

    Update removeElements

---
+++ src/remove-linked-list-elements.cc
@@ -0,0 +1,41 @@
+// http://leetcode.com/problems/remove-linked-list-elements/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  ListNode *removeElements(ListNode *head, int val) {
+    if (!head) {
+      return nullptr;
+    }
+
+    ListNode dummy(-1);
+    dummy.next = head;
+    ListNode *cur = &dummy;
+    while (cur->next && cur->next->val != val) {
+      cur = cur->next;
+    }
+    // now cur->next is the target to delete
+    ListNode *target = cur->next;
+    // the target maybe nullptr
+    if (target) {
+      cur->next = target->next;
+      target->next = nullptr;
+    }
+
+    // recursive the problem
+    cur->next = removeElements(cur->next, val);
+
+    // delete(target);
+    return dummy.next;
+  }
+};
+
+int main() {
+  Solution so;
+  ListNode l1(1), l2(1);
+  l1.next = &l2;
+  auto head = so.removeElements(&l1, 1);
+  if (!head) {
+    cout << "right" << endl;
+  }
+}
