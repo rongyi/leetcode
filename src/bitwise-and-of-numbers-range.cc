1b5c1704d6c1ec7eeda98f2ad11ab7c26b2cc975
rongyi <sqrongyi@163.com>
2018-10-27 15:43:26 +0800

    Update bitwise number range

---
+++ src/bitwise-and-of-numbers-range.cc
@@ -0,0 +1,15 @@
+// http://leetcode.com/problems/bitwise-and-of-numbers-range/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  int rangeBitwiseAnd(int m, int n) {
+    int shift = 0;
+    while (m != n) {
+      m >>= 1;
+      n >>= 1;
+      shift++;
+    }
+    return m << shift;
+  }
+};
