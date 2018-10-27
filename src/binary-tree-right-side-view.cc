bbee64dcc772f8cca22fbbdef8907f6b7f3c8bb2
rongyi <sqrongyi@163.com>
2018-10-27 10:56:51 +0800

    Update binary right side view

--- src/binary-tree-right-side-view.cc
+++ src/binary-tree-right-side-view.cc
@@ -3,15 +3,46 @@
 
 class Solution {
 public:
-  vector<int> rightSideView(TreeNode *root) {
+  vector<int> rightSideViewRaw(TreeNode *root) {
     vector<int> ret;
     auto level_list = levelOrderBottom(root);
-    for (auto &level: level_list) {
+    for (auto &level : level_list) {
       ret.push_back(level.back());
     }
     return ret;
   }
+
+  // 直接精简过后的版本
+  vector<int> rightSideView(TreeNode *root) {
+    vector<int> ret;
+    if (!root) {
+      return ret;
+    }
+    queue<TreeNode *> q;
+    q.push(root);
+
+    while (!q.empty()) {
+      // 对于每一层就只保存最后面那位
+      ret.push_back(q.back()->val);
+
+      int cur_cnt = q.size();
+      for (int i = 0; i < cur_cnt; ++i) {
+        auto *cur = q.front();
+        q.pop();
+        if (cur->left) {
+          q.push(cur->left);
+        }
+        if (cur->right) {
+          q.push(cur->right);
+        }
+      }
+    }
+
+    return ret;
+  }
+
 private:
+  // not work!! need level order
   void recursive(TreeNode *root, vector<int> &ret) {
     if (!root) {
       return;
@@ -24,7 +55,8 @@ private:
     }
   }
 
-vector<vector<int>> levelOrderBottom(TreeNode *root) {
+
+  vector<vector<int>> levelOrderBottom(TreeNode *root) {
     vector<vector<int>> ret;
     if (!root) {
       return ret;
