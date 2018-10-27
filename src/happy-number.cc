afa74856be2b43d770e64f3531fdf70d9e570bb5
rongyi <sqrongyi@163.com>
2018-10-27 15:12:59 +0800

    Add happy number

---
+++ src/happy-number.cc
@@ -0,0 +1,31 @@
+// http://leetcode.com/problems/happy-number/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  bool isHappy(int n) {
+    if (n == 1) {
+      return true;
+    }
+    unordered_set<int> cache;
+    while (true) {
+      auto next = happy(n);
+      if (cache.find(next) != cache.end()) {
+        return next == 1;
+      }
+      n = next;
+      cache.insert(next);
+    }
+    return false;
+  }
+  int happy(int n) {
+    int sum = 0;
+    while (n) {
+      auto d = n % 10;
+      sum += d * d;
+      n /= 10;
+    }
+
+    return sum;
+  }
+};
