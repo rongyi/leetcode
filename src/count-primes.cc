bfe5d1545b231d8433a88cd41ab225c1fa46ea12
rongyi <sqrongyi@163.com>
2018-10-27 12:21:54 +0800

    Add count prime

---
+++ src/count-primes.cc
@@ -0,0 +1,24 @@
+// http://leetcode.com/problems/count-primes/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  int countPrimes(int n) {
+    int ret = 0;
+    for (int i = 2; i < n; ++i) {
+      if (isPrime(i)) {
+        ret++;
+      }
+    }
+    return ret;
+  }
+private:
+  bool isPrime(int n) {
+    for (int i = 2; i * i <= n; i++) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
