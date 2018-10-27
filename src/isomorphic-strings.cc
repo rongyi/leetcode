8cafcb585ba69af8367b2e5f1f14e8379c7d6ad0
rongyi <sqrongyi@163.com>
2018-10-27 12:17:02 +0800

    Update isomorphic string

---
+++ src/isomorphic-strings.cc
@@ -0,0 +1,35 @@
+// http://leetcode.com/problems/isomorphic-strings/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  bool isIsomorphic(string s, string t) {
+    return doisIsomorphic(s, t) && doisIsomorphic(t, s);
+  }
+
+private:
+  bool doisIsomorphic(string s, string t) {
+    if (s.size() != t.size()) {
+      return false;
+    }
+    // projection from s ==> t
+    unordered_map<char, char> p;
+    for (int i = 0; i < s.size(); i++) {
+      // construct the project
+      if (p.find(s[i]) == p.end()) {
+        p[s[i]] = t[i];
+      } else {
+        if (p[s[i]] != t[i]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+};
+
+int main() {
+  Solution so;
+  auto ret = so.isIsomorphic("ab", "ba");
+  cout << ret << endl;
+}
