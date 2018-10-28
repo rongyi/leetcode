9d7dbae90bcbe13ab35f1eb09238887e295da02e
rongyi <sqrongyi@163.com>
2018-10-29 06:39:19 +0800

    Add contains duplicate

---
+++ src/contains-duplicate.cc
@@ -0,0 +1,16 @@
+// http://leetcode.com/problems/contains-duplicate/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  bool containsDuplicate(vector<int> &nums) {
+    unordered_set<int> s;
+    for (auto i : nums) {
+      if (s.find(i) != s.end()) {
+        return true;
+      }
+      s.insert(i);
+    }
+    return false;
+  }
+};
