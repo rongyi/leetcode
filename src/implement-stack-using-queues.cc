4e2141559e293e8867a24bce2aba9fc903ef3459
rongyi <sqrongyi@163.com>
2018-10-30 08:12:22 +0800

    Add implement queue

---
+++ src/implement-stack-using-queues.cc
@@ -0,0 +1,60 @@
+// http://leetcode.com/problems/implement-stack-using-queues/description/
+#include "simpleone.h"
+
+class MyStack {
+public:
+  /** Initialize your data structure here. */
+  MyStack() {}
+
+  /** Push element x onto stack. */
+  void push(int x) {
+    while (!q2.empty()) {
+      q1.push(q2.front());
+      q2.pop();
+    }
+    q2.push(x);
+  }
+
+  /** Removes the element on top of the stack and returns that element. */
+  int pop() {
+    auto i = top();
+    q2.pop();
+
+    return i;
+  }
+
+  /** Get the top element. */
+  int top() {
+    if (q2.empty()) {
+      for (int i = 0; i < q1.size() - 1; ++i) {
+        q1.push(q1.front());
+        q1.pop();
+      }
+
+      q2.push(q1.front());
+      q1.pop();
+    }
+
+    return q2.front();
+  }
+
+  /** Returns whether the stack is empty. */
+  bool empty() {
+    return q1.empty() && q2.empty();
+  }
+private:
+  // help q2 to be a stack
+  queue<int> q1;
+  // for insert
+  // 有元素过来先插入q2，然后保证q2里面至多有一个，这样pop的时候直接出p2即可
+  queue<int> q2;
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * bool param_4 = obj.empty();
+ */
