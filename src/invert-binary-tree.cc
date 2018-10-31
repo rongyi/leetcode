310b8af333e2bd9baf38d3817d5a41e59f67438f
rongyi <sqrongyi@163.com>
2018-10-31 18:47:29 +0800

    Add invertTree

---
+++ src/invert-binary-tree.cc
@@ -0,0 +1,19 @@
+// http://leetcode.com/problems/invert-binary-tree/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  TreeNode *invertTree(TreeNode *root) {
+    if (!root) {
+      return nullptr;
+    }
+    auto *tmp = root->left;
+    root->left = root->right;
+    root->right = tmp;
+
+    invertTree(root->left);
+    invertTree(root->right);
+
+    return root;
+  }
+};
