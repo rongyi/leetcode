c7816ba13b96723f20d1d162732dc3d74916081f
rongyi <sqrongyi@163.com>
2018-10-31 20:20:24 +0800

    Add gap

--- src/product-of-array-except-self.cc
+++ src/product-of-array-except-self.cc
@@ -8,16 +8,19 @@ public:
     // 和从最后一个元素开始到隔壁右边的成绩
     // 这样要求的就是这两个数组乘积
     const int n = nums.size();
+
     vector<long long> left(n, 0);
     left[0] = 1;
     for (int i = 1; i < n; i++) {
       left[i] = left[i - 1] * nums[i - 1];
     }
+
     vector<long long> right(n, 0);
     right[n - 1] = 1;
     for (int i = n - 2; i >= 0; i--) {
       right[i] = right[i + 1] * nums[i + 1];
     }
+
     vector<int> ret(n, 0);
     for (int i = 0; i < n; i++) {
       ret[i] = left[i] * right[i];
