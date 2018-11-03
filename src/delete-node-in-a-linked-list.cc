8b011bbaf2892be2610c643b32b299593279ec32
rongyi <sqrongyi@163.com>
2018-11-04 07:25:10 +0800

    Update delete node in linked list

---
+++ src/delete-node-in-a-linked-list.cc
@@ -0,0 +1,14 @@
+// http://leetcode.com/problems/palindrome-linked-list/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  // 只能swap咯，没办法的事情
+  void deleteNode(ListNode *node) {
+    auto *next = node->next;
+    node->val = next->val;
+    // now we delete next
+    node->next = next->next;
+    next->next = nullptr;
+  }
+};
