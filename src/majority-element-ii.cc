5608abeeb791e853b28156ca4a38b32e53e39d6e
rongyi <sqrongyi@163.com>
2018-11-04 11:25:24 +0800

    Update majorityElement

---
+++ src/majority-element-ii.cc
@@ -0,0 +1,58 @@
+// http://leetcode.com/problems/majority-element-ii/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  vector<int> majorityElement(vector<int> &nums) {
+    vector<int> ret{};
+    if (nums.empty()) {
+      return ret;
+    }
+    int candi1 = 0;
+    int candi2 = 0;
+    int count1 = 0;
+    int count2 = 0;
+    for (auto i : nums) {
+      if (i == candi1) {
+        count1++;
+        continue;
+      }
+      if (i == candi2) {
+        count2++;
+        continue;
+      }
+      if (count1 == 0) {
+        candi1 = i;
+        count1++;
+        continue;
+      }
+      if (count2 == 0) {
+        candi2 = i;
+        count2++;
+        continue;
+      }
+
+      count1--;
+      count2--;
+    }
+
+    // check again
+    count1 = 0;
+    count2 = 0;
+    for (auto i : nums) {
+      if (i == candi1) {
+        count1++;
+      } else if (i == candi2) {
+        count2++;
+      }
+    }
+    if (count1 > nums.size() / 3) {
+      ret.push_back(candi1);
+    }
+    if (count2 > nums.size() / 3) {
+      ret.push_back(candi2);
+    }
+
+    return ret;
+  }
+};
--- src/majority-element.cc
+++ src/majority-element.cc
@@ -3,15 +3,18 @@
 
 class Solution {
 public:
+  // 其实人家考察的是选举算法，这么写没什么意思
   int majorityElement(vector<int> &nums) {
     const int n = nums.size() / 2;
     unordered_map<int, int> count;
+
     for (auto i : nums) {
       ++count[i];
       if (count[i] > n) {
         return i;
       }
     }
+
     for (auto kv : count) {
       if (count[kv.first] > n) {
         return kv.first;
