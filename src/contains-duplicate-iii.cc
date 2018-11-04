d18e8279e11dafc46274bce63649c6e39d977574
rongyi <sqrongyi@163.com>
2018-11-04 08:35:28 +0800

    Update containsNearbyAlmostDuplicate

--- src/contains-duplicate-iii.cc
+++ src/contains-duplicate-iii.cc
@@ -11,7 +11,9 @@ public:
       if (i > k) {
         window.erase(nums[i - k - 1]);
       }
-      auto pos = window.lower_bound((long long)nums[i] - t);
+      // |x - nums[i]| <= t  ==> -t <= x - nums[i] <= t;
+      auto pos = window.lower_bound((long long)nums[i] - t); // x-nums[i] >= -t ==> x >= nums[i]-t
+      // x - nums[i] <= t ==> |x - nums[i]| <= t
       if (pos != window.end() && *pos - nums[i] <= t) {
         return true;
       }
