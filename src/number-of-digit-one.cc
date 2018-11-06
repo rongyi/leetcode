fd6aed02f9eb5b53b8dd46a6684a5857189df626
rongyi <sqrongyi@163.com>
2018-11-06 08:51:39 +0800

    Update count 1

--- src/number-of-digit-one.cc
+++ src/number-of-digit-one.cc
@@ -1,2 +1,16 @@
 // http://leetcode.com/problems/number-of-digit-one/description/
 #include "simpleone.h"
+
+class Solution {
+public:
+  // 这道题没啥意思
+  // https://leetcode.com/articles/number-of-digit-one/
+  int countDigitOne(int n) {
+    int countr = 0;
+    for (long long i = 1; i <= n; i *= 10) {
+      long long divider = i * 10;
+      countr += (n / divider) * i + min(max(n % divider - i + 1, 0LL), i);
+    }
+    return countr;
+  }
+};
