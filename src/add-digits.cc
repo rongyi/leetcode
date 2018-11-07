fb1e46f2a7565202052cddf3e4fc77ae41d8dc84
rongyi <sqrongyi@163.com>
2018-11-07 19:43:14 +0800

    Add digit

---
+++ src/add-digits.cc
@@ -0,0 +1,29 @@
+// http://leetcode.com/problems/add-digits/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  int addDigitsCSWay(int num) {
+    auto ls = digit(num);
+    if (ls.size() == 1) {
+      return ls[0];
+    }
+    return addDigitsCSWay(accumulate(ls.begin(), ls.end(), 0));
+  }
+  // the math way
+  int addDigits(int num) { return (num - 1) % 9 + 1; }
+
+private:
+  vector<int> digit(int num) {
+    if (num == 0) {
+      return vector<int>{0};
+    }
+    vector<int> ret;
+    while (num) {
+      ret.push_back(num % 10);
+      num /= 10;
+    }
+
+    return ret;
+  }
+};
