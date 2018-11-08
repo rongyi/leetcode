3dedc2eff003d5e28772fe2bac1810dd23e56a52
rongyi <sqrongyi@163.com>
2018-11-08 08:52:36 +0800

    Add ugly number

---
+++ src/ugly-number.cc
@@ -0,0 +1,22 @@
+// http://leetcode.com/problems/ugly-number/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  bool isUgly(int num) {
+    if (num <= 0) {
+      return false;
+    }
+    while (num % 2 == 0) {
+      num /= 2;
+    }
+
+    while (num % 3 == 0) {
+      num /= 3;
+    }
+    while (num % 5 ==0) {
+      num /= 5;
+    }
+    return num == 1;
+  }
+};
