ee8d24cfa4878aa7c62c9a246e13303bc4c2aa12
rongyi <sqrongyi@163.com>
2018-11-14 20:06:50 +0800

    Update bulls and cows

--- src/bulls-and-cows.cc
+++ src/bulls-and-cows.cc
@@ -1,2 +1,27 @@
 // http://leetcode.com/problems/bulls-and-cows/description/
 #include "simpleone.h"
+
+class Solution {
+public:
+  string getHint(string secret, string guess) {
+    unordered_map<char, int> gset;
+    int exactly = 0;
+    int misorder = 0;
+    unordered_map<char, int> sset;
+    for (int i = 0; i < secret.size(); i++) {
+      if (secret[i] == guess[i]) {
+        exactly++;
+      } else {
+        gset[secret[i]]++;
+        sset[guess[i]]++;
+      }
+    }
+    for (auto kv : sset) {
+      if (gset.find(kv.first) != gset.end()) {
+        misorder += min(kv.second, gset[kv.first]);
+      }
+    }
+
+    return to_string(exactly) + "A" + to_string(misorder) + "B";
+  }
+};
