024b3f97094e41b217786be70daebd40cb8337f8
rongyi <sqrongyi@163.com>
2018-11-17 20:56:50 +0800

    Update power of three

---
+++ src/power-of-three.cc
@@ -0,0 +1,18 @@
+// http://leetcode.com/problems/power-of-three/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  bool isPowerOfThree(int n) {
+    if (n <= 0) {
+      return false;
+    }
+    if (n == 1) {
+      return true;
+    }
+    if (n % 3 != 0) {
+      return false;
+    }
+    return isPowerOfThree(n / 3);
+  }
+};
