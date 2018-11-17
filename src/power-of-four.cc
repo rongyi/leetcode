66febcf1925b9563574fe21294d32c1c8ffa25ed
rongyi <sqrongyi@163.com>
2018-11-17 21:05:27 +0800

    Add power of four

---
+++ src/power-of-four.cc
@@ -0,0 +1,22 @@
+// http://leetcode.com/problems/power-of-four/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  bool isPowerOfFour(int num) {
+    if (num <= 0) {
+      return false;
+    }
+    if (num == 1) {
+      return true;
+    }
+    // power of 4 must be power of 2, fast path
+    if ((num & (num - 1)) != 0) {
+      return false;
+    }
+    if (num % 4 != 0) {
+      return false;
+    }
+    return isPowerOfFour(num / 4);
+  }
+};
