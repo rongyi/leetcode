517d9ce962c40a6f208101b87b082fa2a4d85ae1
rongyi <sqrongyi@163.com>
2018-11-18 22:40:57 +0800

    Add oddEvenList

---
+++ src/odd-even-linked-list.cc
@@ -0,0 +1,39 @@
+// http://leetcode.com/problems/odd-even-linked-list/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  ListNode *oddEvenList(ListNode *head) {
+    if (!head) {
+      return nullptr;
+    }
+    // save odd
+    ListNode d1(-1);
+    ListNode *cur1 = &d1;
+    // save even
+    ListNode d2(-2);
+    ListNode *cur2 = &d2;
+
+    ListNode *p = head;
+    while (p) {
+      auto *next = p->next;
+      // chain odd
+      cur1->next = p;
+      cur1 = cur1->next;
+
+      // chain even
+      cur2->next = next;
+      cur2 = cur2->next;
+
+      if (next) {
+        p = next->next;
+      } else {
+        // 意思就是两个两个走，只剩一个的情况下，这个时候就退出
+        break;
+      }
+    }
+    cur1->next = d2.next;
+
+    return d1.next;
+  }
+};
