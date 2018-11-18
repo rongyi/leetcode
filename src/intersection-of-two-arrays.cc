b8c2e681dcff2cad4e3addf6e4332f3eac3d0d34
rongyi <sqrongyi@163.com>
2018-11-18 21:09:47 +0800

    Update intersection

---
+++ src/intersection-of-two-arrays.cc
@@ -0,0 +1,26 @@
+// http://leetcode.com/problems/intersection-of-two-arrays/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  vector<int> intersection(vector<int> &nums1, vector<int> &nums2) {
+    unordered_set<int> s1;
+    unordered_set<int> s2;
+
+    for (auto i : nums1) {
+      s1.insert(i);
+    }
+    for (auto i : nums2) {
+      s2.insert(i);
+    }
+
+    vector<int> ret;
+    // intersection
+    for (auto k : s1) {
+      if (s2.find(k) != s2.end()) {
+        ret.push_back(k);
+      }
+    }
+    return ret;
+  }
+};
