5a57c8689903f6ef11773891e601a3a522498a39
rongyi <sqrongyi@163.com>
2018-11-18 22:30:58 +0800

    Update integer break;

---
+++ src/integer-break.cc
@@ -0,0 +1,22 @@
+// http://leetcode.com/problems/integer-break/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  int integerBreak(int n) {
+    if (n == 2) {
+      return 1;
+    }
+    if (n == 3) {
+      return 2;
+    }
+    int ret = 1;
+    while (n > 4) {
+      ret *= 3;
+      n -= 3;
+    }
+    ret *= n;
+
+    return ret;
+  }
+};
