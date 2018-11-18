5393c71c8e2fe86e1ac10270735a4b959dc6f10c
rongyi <sqrongyi@163.com>
2018-11-18 20:46:01 +0800

    Update reverse vowels

---
+++ src/reverse-vowels-of-a-string.cc
@@ -0,0 +1,29 @@
+// http://leetcode.com/problems/reverse-vowels-of-a-string/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  string reverseVowels(string s) {
+    const unordered_set<char> key{'a', 'o', 'e', 'i', 'u', 'A', 'O', 'E', 'I', 'U'};
+    const int n = s.size();
+    int i = 0;
+    int j = n - 1;
+    while (i < j) {
+      if (key.find(s[i]) == key.end()) {
+        i++;
+        continue;
+      }
+      if (key.find(s[j]) == key.end()) {
+        --j;
+        continue;
+      }
+      // 遇到非元音字符了，不等的时候再换，否则懒得换了
+      if (s[i] != s[j]) {
+        swap(s[i], s[j]);
+      }
+      i++;
+      --j;
+    }
+    return s;
+  }
+};
