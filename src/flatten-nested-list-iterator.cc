472ae5880f46c824a6526cabda041abb9c2f36f9
rongyi <sqrongyi@163.com>
2018-11-20 07:22:24 +0800

    Update flatten nestedList

---
+++ src/flatten-nested-list-iterator.cc
@@ -0,0 +1,69 @@
+// http://leetcode.com/problems/flatten-nested-list-iterator/description/
+#include "simpleone.h"
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ */
+
+
+
+class NestedInteger {
+public:
+  // Return true if this NestedInteger holds a single integer, rather than a nested list.
+  bool isInteger() const;
+
+  // Return the single integer that this NestedInteger holds, if it holds a single integer
+  // The result is undefined if this NestedInteger holds a nested list
+  int getInteger() const;
+
+  // Return the nested list that this NestedInteger holds, if it holds a nested list
+  // The result is undefined if this NestedInteger holds a single integer
+  const vector<NestedInteger> &getList() const;
+};
+
+class NestedIterator {
+public:
+  NestedIterator(vector<NestedInteger> &nestedList) {
+    lst_ = vector<int>{};
+    index_ = 0;
+    // now we get a flat list
+    for (auto cur_ni : nestedList) {
+      recur(cur_ni, lst_);
+    }
+  }
+
+  int next() {
+    auto i = lst_[index_++];
+    // cout << i << endl;
+    return i;
+  }
+
+  bool hasNext() {
+    if (lst_.empty()) {
+      return false;
+    }
+    return index_ < lst_.size();
+  }
+private:
+  void recur(NestedInteger &ni, vector<int> &lst) {
+    if (ni.isInteger()) {
+      lst.push_back(ni.getInteger());
+      return;
+    }
+    // then this is a list
+    auto nest_lst = ni.getList();
+    for (auto cur_ni : nest_lst) {
+      recur(cur_ni, lst);
+    }
+  }
+private:
+  vector<int> lst_;
+  int index_;
+};
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i(nestedList);
+ * while (i.hasNext()) cout << i.next();
+ */
