2b80acef8c6361e0cc08458cbb78504eb00a7f66
rongyi <sqrongyi@163.com>
2018-11-19 20:21:40 +0800

    Update coinChange

--- src/coin-change.cc
+++ src/coin-change.cc
@@ -4,19 +4,19 @@
 class Solution {
 public:
   int coinChange(vector<int> &coins, int amount) {
-    // 最多就1块的分割嘛，所以最大值设成一个不可能的值
-    // 自己写的卡在这里
-    vector<int> dp(amount + 1, amount + 1);
+    // 自己最一开始的思路
+    vector<int> dp(amount + 1, numeric_limits<int>::max());
     dp[0] = 0;
 
     for (int i = 1; i <= amount; i++) {
       for (auto c : coins) {
-        if (i - c >= 0) {
+        if (i - c >= 0 && dp[i - c] != numeric_limits<int>::max()) {
           dp[i] = min(dp[i], dp[i - c] + 1);
         }
       }
     }
-    return dp[amount] > amount ? -1 : dp[amount];
+
+    return dp[amount] == numeric_limits<int>::max() ? -1 : dp[amount];
   }
 };
 
