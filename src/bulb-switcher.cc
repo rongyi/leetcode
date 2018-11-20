9263c75df844411fd153bd1c50bf2ed9b3e62d76
rongyi <sqrongyi@163.com>
2018-11-21 06:55:50 +0800

    Update bulbSwitch

---
+++ src/bulb-switcher.cc
@@ -0,0 +1,13 @@
+// http://leetcode.com/problems/bulb-switcher/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  int bulbSwitch(int n) {
+    int ret = 0;
+    for (int i = 1; i * i <= n; i++) {
+      ++ret;
+    }
+    return ret;
+  }
+};
