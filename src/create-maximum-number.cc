b11be97c9a9599ae842cfbf6ed69009b6bc1e9c8
rongyi <sqrongyi@163.com>
2018-11-27 19:47:41 +0800

    Update create maximum number

---
+++ src/create-maximum-number.cc
@@ -0,0 +1,9 @@
+// http://leetcode.com/problems/create-maximum-number/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  vector<int> maxNumber(vector<int> &nums1, vector<int> &nums2, int k) {
+
+  }
+};
