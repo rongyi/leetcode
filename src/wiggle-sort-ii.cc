2eefab94d1dcc6417bf896a05c71d9318f79510b
rongyi <sqrongyi@163.com>
2018-11-28 08:49:42 +0800

    Add wiggle sort

---
+++ src/wiggle-sort-ii.cc
@@ -0,0 +1,14 @@
+// http://leetcode.com/problems/wiggle-sort-ii/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  void wiggleSort(vector<int> &nums) {
+    vector<int> sorted = nums;
+    sort(sorted.begin(), sorted.end());
+    const int n = nums.size();
+    for (int i = 0, j = (n - 1) / 2, k = n - 1; i < n; i++) {
+      nums[i] = (i & 1) ? sorted[k--] : sorted[j--];
+    }
+  }
+};
