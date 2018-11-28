43e0cddc4d874a82e56360c8b26b6d04ba81ee3f
rongyi <sqrongyi@163.com>
2018-11-28 09:09:49 +0800

    Add countRangeSum

---
+++ src/count-of-range-sum.cc
@@ -0,0 +1,9 @@
+// http://leetcode.com/problems/count-of-range-sum/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  int countRangeSum(vector<int> &nums, int lower, int upper) {
+
+  }
+};
