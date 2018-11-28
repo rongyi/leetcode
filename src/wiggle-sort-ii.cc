90985e6d731dff7558cb34b2815557e7b0202d9c
rongyi <sqrongyi@163.com>
2018-11-28 08:51:05 +0800

    Update wiggle sort

--- src/wiggle-sort-ii.cc
+++ src/wiggle-sort-ii.cc
@@ -7,6 +7,7 @@ public:
     vector<int> sorted = nums;
     sort(sorted.begin(), sorted.end());
     const int n = nums.size();
+    // 排序好一个一个抽取放一起
     for (int i = 0, j = (n - 1) / 2, k = n - 1; i < n; i++) {
       nums[i] = (i & 1) ? sorted[k--] : sorted[j--];
     }
