41a50a03f85f220a2c9652d480412d197f6b67f2
rongyi <sqrongyi@163.com>
2018-12-07 07:35:46 +0800

    Update self crossing

---
+++ src/self-crossing.cc
@@ -0,0 +1,34 @@
+// http://leetcode.com/problems/self-crossing/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  bool isSelfCrossing(vector<int> &x) {
+    vector<vector<int>> directions{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+    set<pair<int, int>> cache;
+    pair<int, int> start{0, 0};
+    cache.insert(start);
+
+    for (int i = 0; i < x.size(); i++) {
+      int nextx = start.first;
+      int nexty = start.second;
+      for (int j = 0; j < x[i]; j++) {
+        nextx += directions[i % 4][0];
+        nexty += directions[i % 4][1];
+        pair<int, int> npoint{nextx, nexty};
+        if (cache.find(npoint) == cache.end()) {
+          cache.insert(npoint);
+        } else {
+          return true;
+        }
+        start = npoint;
+      }
+    }
+
+    return false;
+  }
+};
+int main() {
+  Solution so;
+
+}
