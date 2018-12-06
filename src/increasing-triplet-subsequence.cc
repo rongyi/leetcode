17ce38b7b6a85bfa8a9d1e3d8dfa42e7af49cb00
rongyi <sqrongyi@163.com>
2018-12-07 07:11:44 +0800

    Update increasingTriplet

--- src/increasing-triplet-subsequence.cc
+++ src/increasing-triplet-subsequence.cc
@@ -8,6 +8,7 @@ public:
     if (n < 3) {
       return false;
     }
+    // 缓存两个小的，如果再遇到比这两个大的那就有三个了。
     int small1 = numeric_limits<int>::max();
     int small2 = numeric_limits<int>::max();
     for (auto i : nums) {
