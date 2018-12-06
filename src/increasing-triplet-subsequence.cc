af2e93cc34e06d313616d761484353bfd8c6d9e1
rongyi <sqrongyi@163.com>
2018-12-07 07:01:12 +0800

    Update increasingTriplet

--- src/increasing-triplet-subsequence.cc
+++ src/increasing-triplet-subsequence.cc
@@ -8,6 +8,17 @@ public:
     if (n < 3) {
       return false;
     }
+    int small1 = numeric_limits<int>::max();
+    int small2 = numeric_limits<int>::max();
+    for (auto i : nums) {
+      if (i <= small1) {
+        small1 = i;
+      } else if (i <= small2) {
+        small2 = i;
+      } else {
+        return true;
+      }
+    }
 
     return false;
   }
