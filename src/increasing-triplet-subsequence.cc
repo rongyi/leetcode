f357c892b5f77ddf182f57fe008b3e89cceb088f
rongyi <sqrongyi@163.com>
2018-12-06 19:36:12 +0800

    Update increasingTriplet

---
+++ src/increasing-triplet-subsequence.cc
@@ -0,0 +1,12 @@
+// http://leetcode.com/problems/increasing-triplet-subsequence/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  bool increasingTriplet(vector<int> &nums) {
+    const int n = nums.size();
+    if (n < 3) {
+      return false;
+    }
+  }
+};
