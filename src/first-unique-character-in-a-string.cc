678627069cf0f117c8c9bbb627c73a1af758f586
rongyi <sqrongyi@163.com>
2018-12-22 21:08:08 +0800

    Update firstUniqChar

--- src/first-unique-character-in-a-string.cc
+++ src/first-unique-character-in-a-string.cc
@@ -3,5 +3,16 @@
 
 class Solution {
 public:
-  int firstUniqChar(string s) {}
+  int firstUniqChar(string s) {
+    unordered_map<char, int> dict;
+    for (auto c : s) {
+      dict[c]++;
+    }
+    for (int i = 0; i < s.size(); i++) {
+      if (dict[s[i]] == 1) {
+        return i;
+      }
+    }
+    return -1;
+  }
 };
