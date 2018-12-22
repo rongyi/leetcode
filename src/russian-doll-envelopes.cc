55d9613b4a744c9641fabbbebe33fd4e851577f8
rongyi <sqrongyi@163.com>
2018-12-22 09:31:51 +0800

    Update russian doll envelopes

--- src/russian-doll-envelopes.cc
+++ src/russian-doll-envelopes.cc
@@ -12,11 +12,13 @@ public:
     for (int i = 0; i < envelopes.size(); i++) {
       // 看前面有多少个比他小的，总是比这哥们多一个
       for (int j = 0; j < i; j++) {
-        if (envelopes[j].first < envelopes[i].first && envelopes[j].second < envelopes[i].second) {
+        if (envelopes[j].first < envelopes[i].first &&
+            envelopes[j].second < envelopes[i].second) {
           dp[i] = max(dp[i], dp[j] + 1);
         }
       }
     }
+
     return *max_element(dp.begin(), dp.end());
   }
 };
