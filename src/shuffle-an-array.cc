adf62c21f9912617df7460c78b8fd91ad510a22f
rongyi <sqrongyi@163.com>
2018-12-22 21:27:32 +0800

    Update shuffle an array

---
+++ src/shuffle-an-array.cc
@@ -0,0 +1,32 @@
+// http://leetcode.com/problems/shuffle-an-array/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  Solution(vector<int> nums) {
+    origin_ = nums;
+  }
+
+  /** Resets the array to its original configuration and return it. */
+  vector<int> reset() {
+    return origin_;
+  }
+
+  /** Returns a random shuffling of the array. */
+  vector<int> shuffle() {
+    vector<int> cp = origin_;
+    mt19937_64 g;
+    g.seed(random_device()());
+    std::shuffle(cp.begin(), cp.end(), g);
+    return cp;
+  }
+private:
+  vector<int> origin_;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * vector<int> param_1 = obj.reset();
+ * vector<int> param_2 = obj.shuffle();
+ */
