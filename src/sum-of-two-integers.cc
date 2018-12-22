61e6125d9ed5de6d39eb5671daf5deed234bc811
rongyi <sqrongyi@163.com>
2018-12-22 10:04:11 +0800

    Update sum of two integers

---
+++ src/sum-of-two-integers.cc
@@ -0,0 +1,19 @@
+// http://leetcode.com/problems/sum-of-two-integers/description/
+#include "simpleone.h"
+
+class Solution {
+public:
+  // do not use + or -
+  int getSum(int a, int b) {
+    int carry = 0;
+    int ret = 0;
+
+    for (unsigned mask = 1; mask; mask <<= 1) {
+      // 自己卡在这里的表达怎么算
+      ret |= (a ^ b ^ carry) & mask;
+      carry = ((a & b) | ((a ^ b) & carry)) << 1;
+    }
+
+    return ret;
+  }
+};
