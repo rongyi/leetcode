be6ffae2c78893dfe406e57bc09e58c4bdd0d32a
rongyi <sqrongyi@163.com>
2018-12-23 16:24:57 +0800

    Update find missing number

---
+++ src/find-all-numbers-disappeared-in-an-array.cc
@@ -0,0 +1,20 @@
+// http://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<int> findDisappearedNumbers(vector<int> &nums) {
+    unordered_map<int, int> count;
+    for (auto i : nums) {
+      count[i]++;
+    }
+    vector<int> ret;
+    for (int i = 1; i <= nums.size(); i++) {
+      if (count.find(i) == count.end()) {
+        ret.push_back(i);
+      }
+    }
+
+    return ret;
+  }
+};
