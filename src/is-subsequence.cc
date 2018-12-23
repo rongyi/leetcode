8938b4cd2683f97d0d33d3ffa7a7e55a20d69204
rongyi <sqrongyi@163.com>
2018-12-23 15:23:36 +0800

    Update is subsequences

---
+++ src/is-subsequence.cc
@@ -0,0 +1,32 @@
+// http://leetcode.com/problems/is-subsequence/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  bool isSubsequence(string s, string t) {
+    if (s.empty()) {
+      return true;
+    }
+    int j = 0;
+    const int m = s.size();
+    const int n = t.size();
+
+    for (int i = 0; i < n; i++) {
+      if (s[j] == t[i]) {
+        j++;
+      }
+
+      if (j == m) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+};
+
+int main() {
+  Solution so;
+  auto ret = so.isSubsequence("abc", "ahbgdc");
+  cout << ret << endl;
+}
