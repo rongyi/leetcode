5a5ec69c8840ff3cbeb210b139c42d14bac103d5
rongyi <sqrongyi@163.com>
2018-12-23 11:14:09 +0800

    Update leetcode

---
+++ src/combination-sum-iv.cc
@@ -0,0 +1,42 @@
+// http://leetcode.com/problems/combination-sum-iv/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int combinationSum4TLE(vector<int> &nums, int target) {
+    if (target == 0) {
+      return 1;
+    }
+    int ret = 0;
+    for (int i = 0; i < nums.size(); i++) {
+      if (target >= nums[i]) {
+        ret += combinationSum4TLE(nums, target - nums[i]);
+      }
+    }
+
+    return ret;
+  }
+
+  // Think about the recurrence relation first. How does the # of combinations of the target related to the # of combinations of numbers that are smaller than the target?
+
+  // So we know that target is the sum of numbers in the array. Imagine we only need one more number to reach target,
+  // this number can be any one in the array, right? So the # of combinations of target, comb[target] = sum(comb[target - nums[i]]),
+  // where 0 <= i < nums.length, and target >= nums[i].
+
+  // In the example given, we can actually find the # of combinations of 4 with the #
+  // of combinations of 3(4 - 1), 2(4- 2) and 1(4 - 3). As a result, comb[4] = comb[4-1] + comb[4-2] + comb[4-3] = comb[3] + comb[2] + comb[1].
+  // Then think about the base case. Since if the target is 0, there is only one way to get zero, which is using 0, we can set comb[0] = 1.
+  int combinationSum4(vector<int> &nums, int target) {
+    vector<int> dp(target + 1, 0);
+    // empty case
+    dp[0] = 1;
+    for (int i = 1; i < dp.size(); i++) {
+      for (int j = 0; j < nums.size(); j++) {
+        if (i - nums[j] >= 0) {
+          dp[i] += dp[i - nums[j]];
+        }
+      }
+    }
+    return dp[target];
+  }
+};
---
+++ src/super-pow.cc
@@ -0,0 +1,39 @@
+// http://leetcode.com/problems/super-pow/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  // 这其实是数学问题，找到循环的规律即可
+  int superPow(int a, vector<int> &b) {
+    if (a == 1) {
+      return 1;
+    }
+    if (a == 1337) {
+      return 0;
+    }
+    a %= 1337;
+
+    int count = 1;
+    int rem = a;
+    unordered_map<int, int> cache;
+    while (true) {
+      cache[count] = rem;
+      count++;
+      rem *= a;
+      rem %= 1337;
+      if (rem == a) {
+        break;
+      }
+    }
+    int cycle = cache.size();
+
+    int val = 0;
+    int size = b.size();
+    for (int i = 0; i < size; i++) {
+      val = val * 10 + b[i];
+      val = val % cycle;
+    }
+
+    return cache[val];
+  }
+};
