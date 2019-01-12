e9ed8c9044fe3c58e9e06e0d243ba95abf70eb15
rongyi <sqrongyi@163.com>
2019-01-12 11:19:20 +0800

    Update wiggle sort

---
+++ src/wiggle-subsequence.cc
@@ -0,0 +1,28 @@
+// http://leetcode.com/problems/wiggle-subsequence/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int wiggleMaxLength(vector<int> &nums) {
+    const int n = nums.size();
+    if (n <= 1) {
+      return n;
+    }
+    vector<int> reduce;
+    for (int i = 1; i < n; i++) {
+      if (nums[i] - nums[i - 1] > 0) {
+        reduce.push_back(1);
+      } else if (nums[i] - nums[i - 1] < 0) {
+        reduce.push_back(-1);
+      }
+    }
+    int ret = 1;
+    for (int i = 0; i < reduce.size(); i++) {
+      if (i == 0 || reduce[i] != reduce[i - 1]) {
+        ret++;
+      }
+    }
+
+    return ret;
+  }
+};
