dc337189a29cd8832a926e1d0e4737508ca5ee61
rongyi <sqrongyi@163.com>
2019-01-13 10:01:13 +0800

    Update largestDivisibleSubset

---
+++ src/largest-divisible-subset.cc
@@ -0,0 +1,25 @@
+// http://leetcode.com/problems/largest-divisible-subset/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<int> largestDivisibleSubset(vector<int> &nums) {
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> result(nums.size());
+    vector<int> ret;
+    for (int i = 0; i < nums.size(); i++) {
+      for (int j = 0; j < i; j++) {
+        if (nums[i] % nums[j] == 0 && result[j].size() > result[i].size()) {
+          result[i] = result[j];
+        }
+      }
+
+      result[i].push_back(nums[i]);
+      if (result[i].size() > ret.size()) {
+        ret = result[i];
+      }
+    }
+
+    return ret;
+  }
+};
