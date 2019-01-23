61c9379abf98213036e7fd9bd6bb14dd0a9b4ee0
rongyi <sqrongyi@163.com>
2019-01-24 07:40:44 +0800

    Update right intervals

---
+++ src/find-right-interval.cc
@@ -0,0 +1,26 @@
+// http://leetcode.com/problems/find-right-interval/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<int> findRightInterval(vector<Interval> &intervals) {
+    const int n = intervals.size();
+    vector<int> ret;
+    map<int, int> cache;
+
+    for (int i = 0; i < n; ++i) {
+      cache[intervals[i].start] = i;
+    }
+
+    for (auto in : intervals) {
+      auto it = cache.lower_bound(in.end);
+      if (it == cache.end()) {
+        ret.push_back(-1);
+      } else {
+        ret.push_back(it->second);
+      }
+    }
+
+    return ret;
+  }
+};
