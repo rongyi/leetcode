2f64483b35832bcafd6e823c27cfe557e2e71853
rongyi <sqrongyi@163.com>
2019-01-26 10:15:33 +0800

    Update frog jump

--- src/frog-jump.cc
+++ src/frog-jump.cc
@@ -7,6 +7,8 @@ public:
 
 public:
   bool canCross(vector<int> &stones, int pos, int k) {
+    // The number of stones is less than 1100 so pos will always be less than
+    // 2^11 (2048).
     int key = pos | (k << 11);
 
     if (dp_.count(key) > 0) {
@@ -14,18 +16,25 @@ public:
     }
     for (int i = pos + 1; i < stones.size(); ++i) {
       int gap = stones[i] - stones[pos];
+      // 为什么有这个判断？
+      // 因为序列是递增的
+      // 所以小了放过这个元素继续看后面的元素
       if (gap < k - 1) {
         continue;
       }
+      // 当前的gap都已经比k + 1大了，后面的元素与当前这块石头的距离只能是更大，所以
+      // 后面不用做了，直接返回
       if (gap > k + 1) {
         dp_[key] = false;
         return false;
       }
+      // 在条件 k - 1, k, k + 1范围内，继续递归调用
       if (canCross(stones, i, gap)) {
         dp_[key] = true;
         return true;
       }
     }
+
     dp_[key] = (pos == int(stones.size() - 1));
     return dp_[key];
   }
@@ -33,3 +42,10 @@ public:
 private:
   unordered_map<int, int> dp_;
 };
+
+int main() {
+  Solution so;
+  vector<int> input{0, 1, 3, 5, 6, 8, 12, 17};
+  auto ret = so.canCross(input);
+  cout << ret << endl;
+}
