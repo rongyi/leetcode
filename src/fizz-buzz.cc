91c44e09aa46aac06253e8c3cc95bdb008c35c2d
rongyi <sqrongyi@163.com>
2019-01-28 07:39:55 +0800

    Update fizz buzz

---
+++ src/fizz-buzz.cc
@@ -0,0 +1,22 @@
+// http://leetcode.com/problems/fizz-buzz/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<string> fizzBuzz(int n) {
+    vector<string> ret;
+    for (int i = 1; i <= n; ++i) {
+      if ((i % 3 == 0) && (i % 5 == 0)) {
+        ret.push_back("FizzBuzz");
+      } else if (i % 3 == 0) {
+        ret.push_back("Fizz");
+      } else if (i % 5 == 0) {
+        ret.push_back("Buzz");
+      } else {
+        ret.push_back(to_string(i));
+      }
+    }
+
+    return ret;
+  }
+};
