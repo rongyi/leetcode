297c51f121a81698ac27ce1e79eb985a03ba4924
rongyi <sqrongyi@163.com>
2019-01-29 08:23:34 +0800

    Add some comment

--- src/arithmetic-slices.cc
+++ src/arithmetic-slices.cc
@@ -7,28 +7,33 @@ public:
     if (A.size() < 3) {
       return 0;
     }
+
     const int n = A.size();
     int ret = 0;
+
     for (int i = 0; i < n; ++i) {
+      // 所有子数组长度 >= 3才可以来判断
       for (int j = i + 2; j < n; ++j) {
-        // cout << "testing " << i << "   " << j << endl;
         if (isArithmetic(A, i, j)) {
           ++ret;
         }
       }
     }
+
     return ret;
   }
 
 public:
   // i, j included
   bool isArithmetic(vector<int> &nums, int i, int j) {
-    auto delta = nums[i + 1] - nums[i];
+    // 哥给你打个样
+    const auto delta = nums[i + 1] - nums[i];
     for (int k = i + 2; k <= j; k++) {
       if (nums[k] - nums[k - 1] != delta) {
         return false;
       }
     }
+
     return true;
   }
 };
