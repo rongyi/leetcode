50dcf732983ced051dc2dabd3031a4d0cd5773f1
rongyi <sqrongyi@163.com>
2019-01-31 10:23:21 +0800

    Update partition equal subset

--- src/partition-equal-subset-sum.cc
+++ src/partition-equal-subset-sum.cc
@@ -30,4 +30,32 @@ public:
     }
     return false;
   }
+  // 方便理解，过不了大数集
+  bool canPartitionTLE(vector<int> &nums) {
+    int half = accumulate(nums.begin(), nums.end(), 0);
+    // must be even
+    if (half & 1) {
+      return false;
+    }
+    half >>= 1;
+    if (*max_element(nums.begin(), nums.end()) > half) {
+      return false;
+    }
+
+    return recur(nums, nums.size(), half);
+  }
+
+private:
+  bool recur(vector<int> &nums, int n, int sum) {
+    if (sum == 0) {
+      return true;
+    }
+    if (n == 0) {
+      return false;
+    }
+
+    // 对于每个数可取可不取
+    return recur(nums, n - 1, sum) /*不取这个数*/ ||
+           recur(nums, n - 1, sum - nums[n - 1]); /*取这个数*/
+  }
 };
