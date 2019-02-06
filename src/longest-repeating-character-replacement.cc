a0cd59643869b9d5822ada2cbc41860b14252667
rongyi <sqrongyi@163.com>
2019-02-07 07:36:51 +0800

    Update longest repeating characterReplacement

--- src/longest-repeating-character-replacement.cc
+++ src/longest-repeating-character-replacement.cc
@@ -3,8 +3,14 @@
 
 class Solution {
 public:
+  // https://leetcode.com/problems/longest-repeating-character-replacement/discuss/91285/Sliding-window-similar-to-finding-longest-substring-with-k-distinct-characters
+  // 先不看k，如果是整个字符串替换，替换次数最少是多少？
+  // ==> 整个字符串长度 - 里面出现最多的那个字符出现的次数
+  // 加上K的约束呢？
+  // ==> 在子字符串中 整个子串的长度 - 出现次数最多的那个字符出现的次数 <= k
   int characterReplacement(string s, int k) {
     int ret = 0;
+    // 当前sliding window中出现次数最多的那个字符
     int cur_max = 0;
     int start = 0;
     const int n = s.size();
