716c860b3d0dfdeffe7512bb4e2b044d64f085c6
rongyi <sqrongyi@163.com>
2019-02-13 09:50:26 +0800

    Update maximum depth of nary tree

---
+++ src/maximum-depth-of-n-ary-tree.cc
@@ -0,0 +1,24 @@
+// http://leetcode.com/problems/maximum-depth-of-n-ary-tree/description/
+#include "xxx.h"
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+public:
+    int maxDepth(Node* root) {
+
+    }
+};
