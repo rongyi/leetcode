f971b8e3e168dad6466bf9fcb1ccaa5635b04469
rongyi <sqrongyi@163.com>
2019-02-13 09:47:58 +0800

    Update nary postorder traversal

---
+++ src/n-ary-tree-postorder-traversal.cc
@@ -0,0 +1,54 @@
+// http://leetcode.com/problems/n-ary-tree-postorder-traversal/description/
+#include "xxx.h"
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Node {
+public:
+  int val;
+  vector<Node *> children;
+
+  Node() {}
+
+  Node(int _val, vector<Node *> _children) {
+    val = _val;
+    children = _children;
+  }
+};
+
+class Solution {
+public:
+  vector<int> postorder(Node *root) {
+    vector<int> ret;
+
+    recur(root, ret);
+
+    return ret;
+  }
+private:
+  void recur(Node *root, vector<int> &ret) {
+    if (!root) {
+      return;
+    }
+    // children first
+    for (auto p : root->children) {
+      recur(p, ret);
+    }
+    // then this node
+    ret.push_back(root->val);
+  }
+};
