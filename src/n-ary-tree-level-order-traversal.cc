c8b058ba77750c2b64ebd93233733c4dc47cc839
rongyi <sqrongyi@163.com>
2019-02-13 09:29:23 +0800

    Add iterate version

--- src/n-ary-tree-level-order-traversal.cc
+++ src/n-ary-tree-level-order-traversal.cc
@@ -33,7 +33,7 @@ public:
 class Solution {
 
 public:
-  vector<vector<int>> levelOrder(Node *root) {
+  vector<vector<int>> levelOrderRecursive(Node *root) {
     vector<vector<int>> ret;
     recur(root, ret, 1);
     return ret;
@@ -51,5 +51,28 @@ public:
     for (auto child : root->children) {
       recur(child, ret, cur_level + 1);
     }
+  }
+
+  vector<vector<int>> levelOrder(Node *root) {
+    vector<vector<int>> ret;
+    queue<Node *> q;
+    if (!root) {
+      return ret;
+    }
+    q.push(root);
+    while (!q.empty()) {
+      const int level_size = q.size();
+      vector<int> cur_level{};
+      for (int i = 0; i < level_size; i++) {
+        Node *n = q.front();
+        q.pop();
+        cur_level.push_back(n->val);
+        for (auto p : n->children) {
+          q.push(p);
+        }
+      }
+      ret.push_back(cur_level);
+    }
+    return ret;
   }
 };
