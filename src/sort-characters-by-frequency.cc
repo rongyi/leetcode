07b132123efd5800e4c8ec9a2a6e6fc20987b43c
rongyi <sqrongyi@163.com>
2019-02-19 17:28:51 +0800

    Update sort characters

--- src/sort-characters-by-frequency.cc
+++ src/sort-characters-by-frequency.cc
@@ -5,20 +5,24 @@ class Solution {
 public:
   string frequencySort(string s) {
     // char --> count
+    // 先统计
     unordered_map<char, int> count;
     for (auto c : s) {
       count[c]++;
     }
+    // 然后按数字归类
     unordered_map<int, unordered_set<char>> group;
     for (auto kv : count) {
       group[kv.second].insert(kv.first);
     }
 
+    // 然后把数字从高到低排列
     vector<int> nums;
     for (auto kv : group) {
       nums.push_back(kv.first);
     }
     sort(nums.begin(), nums.end(), greater<int>());
+    // 输出
     stringstream ss;
     for (auto num : nums) {
       auto chars = group[num];
