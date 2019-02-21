786d1a751f4888e5424df8f8ca5f895a31c9408b
rongyi <sqrongyi@163.com>
2019-02-21 10:18:02 +0800

    Update circularArrayLoop

--- src/circular-array-loop.cc
+++ src/circular-array-loop.cc
@@ -5,32 +5,51 @@ class Solution {
 public:
   bool circularArrayLoop(vector<int> &nums) {
     const int n = nums.size();
+    for (auto &i : nums) {
+      i %= n;
+    }
     for (int i = 0; i < n; ++i) {
       if (nums[i] == 0) {
         continue;
       }
-      int cur = i;
+      long sign = nums[i] > 0 ? 1 : -1;
+      int j = i;
+      int k = i;
+      // 快慢指针的方式来解决
       while (true) {
-        // step有可能是0哦，比如是第二块石头，从非0跳过来
-        int step = nums[cur];
-        // 把当前格给填了，调到这loop就loop了，不loop不管从前面哪个点
-        // 跳过来都绕不起来，所以跳到我这就别跳了
-        nums[cur] = 0;
-        int next = (cur + step) % n;
-        if (next < 0) {
-          next += n;
-        }
-        // 方向相反或者
-        if (cur == next || step * nums[next] < 0) {
+        k = getIndex(nums, k);
+        if (nums[k] * sign <= 0) {
           break;
         }
-        if (cur != next && next == i) {
+        k = getIndex(nums, k);
+        if (nums[k] * sign <= 0) {
+          break;
+        }
+
+        i = getIndex(nums, i);
+        if (i == k) {
           return true;
         }
-        // 继续
-        cur = next;
+      }
+      j = i;
+      while (j != k) {
+        int l = j;
+        j = getIndex(nums, j);
+        nums[l] = 0;
       }
     }
     return false;
   }
+
+private:
+  int getIndex(vector<int> &nums, int k) {
+    return (k + nums[k] + nums.size()) % nums.size();
+  }
 };
+
+int main() {
+  Solution so;
+  vector<int> input{1, 1, 2};
+  auto ret = so.circularArrayLoop(input);
+  cout << ret << endl;
+}
