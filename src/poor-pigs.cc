797e4d1178b258af213c183b81d67f807932ef90
rongyi <sqrongyi@163.com>
2019-02-21 18:49:10 +0800

    Update poor pigs

---
+++ src/poor-pigs.cc
@@ -0,0 +1,15 @@
+// http://leetcode.com/problems/poor-pigs/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  // https://leetcode.com/problems/poor-pigs/discuss/94266/Another-explanation-and-solution
+  int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+    int pigs = 0;
+    while (pow((minutesToTest / minutesToDie) + 1, pigs) < buckets) {
+      pigs++;
+    }
+
+    return pigs;
+  }
+};
