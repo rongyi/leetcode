69eb38b7d7612c354515eb37180d2bc2ee436dd8
rongyi <sqrongyi@163.com>
2019-02-24 10:09:24 +0800

    Update count the repetitions

--- src/count-the-repetitions.cc
+++ src/count-the-repetitions.cc
@@ -12,14 +12,16 @@ public:
       }
     }
 
+    const int s1len = s1.size();
+    const int s2len = s2.size();
     int p1 = 0;
     int p2 = 0;
     int mark = 0;
 
-    while (p1 < s1.length() * n1) {
-      char c = s2[p2 % s2.length()];
+    while (p1 < s1len * n1) {
+      char c = s2[p2 % s2len];
       // 找到相等的第一个字符
-      while (s1[p1 % s1.length()] != c && p1 < s1.length() * n1) {
+      while (s1[p1 % s1len] != c && p1 < s1len * n1) {
         p1++;
       }
       // 然后一起进步，检测下一个字符
@@ -29,23 +31,23 @@ public:
       // 完整匹配一轮s2
       if (p2 % s2.length() == 0) {
         // 第一次匹配OK的时候记录下此时p1中的位置
-        if (p2 == (int)s2.length()) {
+        if (p2 == s2len) {
           mark = p1;
-        } else if (p1 % s1.length() == mark % s1.length()) {
+        } else if (p1 % s1len == mark % s1len) {
           // p1又走到相同的相对位置了，说明此时产生了重复的规律
           // p1 - mark是这段重复规律的长度
           // s1.length() * n1是总长， 减去已经走掉的 p1
-          int round = (s1.length() * n1 - p1) / (p1 - mark);
+          int round = (s1len * n1 - p1) / (p1 - mark);
           //  直接飞过去
           p1 += round * (p1 - mark);
           // 生成mark那一轮，也即第一轮要减掉
-          p2 += round * (p2 - s2.length());
+          p2 += round * (p2 - s2len);
         }
       }
     }
 
     // 如果s2在S1中重复了R次，那么S2(s2重复n2次构成)则肯定重复了 R / n2 次。
-    return p2 / s2.length() / n2;
+    return p2 / s2len / n2;
   }
 };
 
