e469d571bd05bae2861ffbfecfc4f5c98f1bb44a
rongyi <sqrongyi@163.com>
2019-02-24 09:50:23 +0800

    Add comments

--- src/count-the-repetitions.cc
+++ src/count-the-repetitions.cc
@@ -30,8 +30,12 @@ public:
         if (p2 == (int)s2.length()) {
           mark = p1;
         } else if (p1 % s1.length() == mark % s1.length()) {
+          // p1又走到相同的相对位置了，说明此时产生了重复的规律
+          // p1 - mark是这段重复规律的长度
+          // s1.length() * n1是总长， 减去已经走掉的 p1
           int round = (s1.length() * n1 - p1) / (p1 - mark);
           p1 += round * (p1 - mark);
+          // 生成mark那一轮，也即第一轮要减掉
           p2 += round * (p2 - s2.length());
         }
       }
