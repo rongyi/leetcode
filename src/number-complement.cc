b83ffc4ce9397c4a43d3b2601696dbe461870fe5
rongyi <sqrongyi@163.com>
2019-02-26 09:45:59 +0800

    Update number complement

---
+++ src/number-complement.cc
@@ -0,0 +1,25 @@
+// http://leetcode.com/problems/number-complement/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int findComplement(int num) {
+    int ret = 0;
+    int bitcount = 0;
+    int copy = num;
+    // 慢慢来，无需 num &= (num - 1) 这样的加速
+    while (copy) {
+      bitcount++;
+      copy >>= 1;
+    }
+    for (int i = 0; i < bitcount; ++i) {
+      ret |= ((((num >> i) & 1) ^ 1) << i);
+    }
+    return ret;
+  }
+};
+int main() {
+  Solution so;
+  auto ret = so.findComplement(16);
+  cout << ret << endl;
+}
