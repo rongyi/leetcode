75f8c2345a1556e90b2bdbfa6fbf469908bef385
rongyi <sqrongyi@163.com>
2019-03-04 15:49:18 +0800

    Update max consecutive ones

---
+++ src/max-consecutive-ones.cc
@@ -0,0 +1,20 @@
+// http://leetcode.com/problems/max-consecutive-ones/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int findMaxConsecutiveOnes(vector<int> &nums) {
+    int ret = 0;
+    int count = 0;
+    for (auto i : nums) {
+      if (i == 1) {
+        count++;
+        ret = max(ret, count);
+      } else {
+        count = 0;
+      }
+    }
+
+    return ret;
+  }
+};
