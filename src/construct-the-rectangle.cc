da7df989213ceff599798f29cb2b236cb4978e0e
rongyi <sqrongyi@163.com>
2019-03-06 14:38:55 +0800

    Update leetcode

---
+++ src/construct-the-rectangle.cc
@@ -0,0 +1,23 @@
+// http://leetcode.com/problems/construct-the-rectangle/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<int> constructRectangle(int area) {
+    double d = sqrt((double)area);
+
+    for (int i = (int)d; i > 0; --i) {
+      if (area % i == 0) {
+        auto w = max(i, area / i);
+        auto l = area / w;
+        return {w, l};
+      }
+    }
+    return {-1, -1};
+  }
+};
+
+int main() {
+  Solution so;
+  so.constructRectangle(4);
+}
---
+++ src/reverse-pairs.cc
@@ -0,0 +1,9 @@
+// http://leetcode.com/problems/reverse-pairs/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int reversePairs(vector<int> &nums) {
+
+  }
+};
