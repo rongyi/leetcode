1457e7eeae5f880d7381e6c23e7c91fbd8556e52
rongyi <sqrongyi@163.com>
2019-03-09 08:14:09 +0800

    Update reverse pairs

--- src/reverse-pairs.cc
+++ src/reverse-pairs.cc
@@ -4,6 +4,23 @@
 class Solution {
 public:
   int reversePairs(vector<int> &nums) {
+    return mergeSort(nums, 0, nums.size() - 1);
+  }
 
+private:
+  int mergeSort(vector<int> &nums, int s, int e) {
+    if (s >= e) {
+      return 0;
+    }
+    int mid = s + (e - s) / 2;
+    int cnt = mergeSort(nums, s, mid) + mergeSort(nums, mid + 1, e);
+    for (int i = s, j = mid + 1; i <= mid; i++) {
+      while (j <= e && nums[i] / 2.0 > nums[j]) {
+        j++;
+      }
+      cnt += j - (mid + 1);
+    }
+    sort(nums.begin() + s, nums.begin() + e + 1);
+    return cnt;
   }
 };
