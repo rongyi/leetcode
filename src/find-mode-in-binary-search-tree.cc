85a9301bf580a0aaab7e147b06acd3a2960b73ed
rongyi <sqrongyi@163.com>
2019-03-10 20:14:47 +0800

    Add find mode

---
+++ src/find-mode-in-binary-search-tree.cc
@@ -0,0 +1,43 @@
+// http://leetcode.com/problems/find-mode-in-binary-search-tree/description/
+#include "xxx.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  vector<int> findMode(TreeNode *root) {
+    if (!root) {
+      return {};
+    }
+    unordered_map<int, int> count;
+    int count_max = 0;
+    traverse(root, count, count_max);
+
+    vector<int> ret;
+    for (auto kv : count) {
+      if (kv.second == count_max) {
+        ret.push_back(kv.first);
+      }
+    }
+    return ret;
+  }
+  void traverse(TreeNode *node, unordered_map<int, int> &count,
+                int &count_max) {
+    if (!node) {
+      return;
+    }
+
+    count[node->val]++;
+    count_max = max(count_max, count[node->val]);
+
+    traverse(node->left, count, count_max);
+    traverse(node->right, count, count_max);
+  }
+};
