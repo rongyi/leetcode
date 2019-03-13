013974574f18b728dfe0e49b28368b89844386f1
rongyi <sqrongyi@163.com>
2019-03-13 14:26:28 +0800

    Update longest word in dictionary through deleting

---
+++ src/longest-word-in-dictionary-through-deleting.cc
@@ -0,0 +1,53 @@
+// http://leetcode.com/problems/longest-word-in-dictionary-through-deleting/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  string findLongestWord(string s, vector<string> &d) {
+    unordered_set<char> l;
+    for (auto c : s) {
+      l.insert(c);
+    }
+    sort(d.begin(), d.end(), [](string &l, string &r) -> bool {
+      if (l.size() != r.size()) {
+        return l.size() > r.size();
+      }
+      return l < r;
+    });
+
+    for (auto &str : d) {
+      if (str.size() > s.size()) {
+        continue;
+      }
+      if (issubseq(str, s)) {
+        return str;
+      }
+    }
+    return "";
+  }
+
+private:
+  bool issubseq(string &sub, string &match) {
+    int j = 0;
+
+    for (int i = 0; i < match.size(); i++) {
+      if (sub[j] == match[i]) {
+        j++;
+      }
+      if (j == sub.size()) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+};
+
+int main() {
+  Solution so;
+  string str{"aewfafwafjlwajflwajflwafj"};
+  vector<string> input{"apple", "ewaf",  "awefawfwaf",
+                       "awef",  "awefe", "ewafeffewafewf"};
+  auto ret = so.findLongestWord(str, input);
+  cout << ret << endl;
+}
