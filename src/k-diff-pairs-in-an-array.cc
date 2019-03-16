9c8a0988a84c7ea46cfd0acadb73d0c21357374e
rongyi <sqrongyi@163.com>
2019-03-17 07:56:31 +0800

    Update k diff pair

---
+++ src/k-diff-pairs-in-an-array.cc
@@ -0,0 +1,46 @@
+// http://leetcode.com/problems/k-diff-pairs-in-an-array/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int findPairs(vector<int> &nums, int k) {
+    if (k < 0) {
+      return 0;
+    }
+    unordered_map<int, int> count;
+    for (auto i : nums) {
+      count[i]++;
+    }
+    if (k == 0) {
+      int ret = 0;
+      for (auto kv : count) {
+        if (kv.second > 1) {
+          ret++;
+        }
+      }
+      return ret;
+    }
+
+    set<pair<int, int>> uniq;
+    for (auto i : nums) {
+      auto it = count.find(i + k);
+      if (it == count.end()) {
+        continue;
+      }
+      uniq.insert({min(i, it->first), max(i, it->first)});
+
+      it = count.find(i - k);
+      if (it == count.end()) {
+        continue;
+      }
+      uniq.insert({min(i, it->first), max(i, it->first)});
+    }
+    return uniq.size();
+  }
+};
+
+int main() {
+  Solution so;
+  vector<int> input{1, 2, 3, 4, 5};
+  so.findPairs(input, 1);
+}
