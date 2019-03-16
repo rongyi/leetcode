e20cfdd24c41806f1739314a2eaa5f6ce25d0d7b
rongyi <sqrongyi@163.com>
2019-03-16 09:41:06 +0800

    Add beautiful arrangement

---
+++ src/beautiful-arrangement.cc
@@ -0,0 +1,35 @@
+// http://leetcode.com/problems/beautiful-arrangement/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int countArrangement(int n) {
+    int ret = 0;
+    vector<bool> visited(n, false);
+    vector<int> cur;
+    dfs(n, cur, visited, ret);
+
+    return ret;
+  }
+
+  void dfs(int n, vector<int> &cur, vector<bool> &visited, int &ret) {
+    if (cur.size() == n) {
+      ++ret;
+      return;
+    }
+    // the i-th value, first is 1
+    int l = cur.size() + 1;
+    // total value
+    for (int i = 1; i <= n; ++i) {
+      // the value on this position can be put on
+      if (!visited[i] && ((l % i == 0 || i % l == 0))) {
+        visited[i] = true;
+        cur.push_back(i);
+        dfs(n, cur, visited, ret);
+
+        visited[i] = false;
+        cur.pop_back();
+      }
+    }
+  }
+};
