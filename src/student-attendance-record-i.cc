d8a809489d926359ef8e9e3d38b4d15a714aa2ad
rongyi <sqrongyi@163.com>
2019-03-22 11:15:43 +0800

    Update student attendance record

---
+++ src/student-attendance-record-i.cc
@@ -0,0 +1,25 @@
+// http://leetcode.com/problems/student-attendance-record-i/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  bool checkRecord(string s) {
+    int num_absent = count(s.begin(), s.end(), 'A');
+    if (num_absent > 1) {
+      return false;
+    }
+    for (int i = 0; i < s.size(); i++) {
+      if (s[i] == 'L') {
+        int j = i;
+        while (j < s.size() && s[j] == 'L') {
+          j++;
+        }
+        if (j - i > 2) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+};
