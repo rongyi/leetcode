7050671ffaf981812d9cc7a40762e8320f9f7c41
rongyi <sqrongyi@163.com>
2019-03-23 20:27:20 +0800

    Update reverse words in a string

---
+++ src/reverse-words-in-a-string-iii.cc
@@ -0,0 +1,24 @@
+// http://leetcode.com/problems/reverse-words-in-a-string-iii/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  string reverseWords(string s) {
+    stringstream ss(s);
+    string cur;
+    stringstream retss;
+    while (getline(ss, cur, ' ')) {
+      reverse(cur.begin(), cur.end());
+      retss << cur << ' ';
+    }
+
+    auto ret = retss.str();
+    // the last useless ' '
+    ret.pop_back();
+    return ret;
+  }
+};
+int main() {
+  Solution so;
+  so.reverseWords("Let's take LeetCode contest");
+}
