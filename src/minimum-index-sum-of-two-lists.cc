1c8f1c385dbedac5798018ff31c1eaf22d25a1ae
rongyi <sqrongyi@163.com>
2019-03-31 20:49:06 +0800

    Update minimum index sum of two list

---
+++ src/minimum-index-sum-of-two-lists.cc
@@ -0,0 +1,23 @@
+// http://leetcode.com/problems/minimum-index-sum-of-two-lists/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<string> findRestaurant(vector<string> &list1, vector<string> &list2) {
+    unordered_map<string, int> index;
+    for (int i = 0; i < list2.size(); i++) {
+      index[list2[i]] = i;
+    }
+    unordered_map<int, vector<string>> common;
+    for (int i = 0; i < list1.size(); i++) {
+      if (index.find(list1[i]) != index.end()) {
+        common[i + index[list1[i]]].push_back(list1[i]);
+      }
+    }
+    int low_key = numeric_limits<int>::max();
+    for (auto kv : common) {
+      low_key = min(low_key, kv.first);
+    }
+    return common[low_key];
+  }
+};
