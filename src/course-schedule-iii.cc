8a559a2824322f644677fbc0622167b671ffc22e
rongyi <sqrongyi@163.com>
2019-04-09 08:03:15 +0800

    Update course schedule

---
+++ src/course-schedule-iii.cc
@@ -0,0 +1,23 @@
+// http://leetcode.com/problems/course-schedule-iii/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int scheduleCourse(vector<vector<int>> &courses) {
+    sort(courses.begin(), courses.end(), [](auto &p, auto &q) {
+                                           return p[1] < q[1];
+                                         });
+    priority_queue<int> q;
+    int sum = 0;
+    for (auto c : courses) {
+      q.push(c[0]);
+      sum += c[0];
+      if (sum > c[1]) {
+        sum -= q.top();
+        q.pop();
+      }
+    }
+
+    return q.size();
+  }
+};
