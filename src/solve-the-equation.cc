b627dad8f2ca19b33107111cfb42967c9a7d9584
rongyi <sqrongyi@163.com>
2019-04-13 12:55:54 +0800

    Update solveEquation

---
+++ src/solve-the-equation.cc
@@ -0,0 +1,79 @@
+// http://leetcode.com/problems/solve-the-equation/description/
+#include "xxx.h"
+
+class Solution {
+  struct Equation {
+    int co_;
+    int sum_;
+  };
+
+public:
+  string solveEquation(string equation) {
+    auto left = equation.substr(0, equation.find('='));
+    auto right = equation.substr(equation.find('=') + 1);
+    auto leqt = reduce(left);
+    auto reqt = reduce(right);
+    if (leqt.co_ == reqt.co_) {
+      if (leqt.sum_ == reqt.sum_) {
+        return "Infinite solutions";
+      }
+      return "No solution";
+    }
+    auto co = leqt.co_ - reqt.co_;
+    auto sum = reqt.sum_ - leqt.sum_;
+    return "x=" + to_string(sum / co);
+  }
+  Equation reduce(string eqs) {
+    auto isop = [](char c) -> bool { return c == '+' || c == '-'; };
+    if (!isop(eqs[0])) {
+      eqs = '+' + eqs;
+    }
+    const int n = eqs.size();
+    Equation eqt{0, 0};
+    int i = 0;
+    while (i < n) {
+      // jump over
+      int j = i + 1;
+      char op = eqs[i];
+      while (j < n && !isop(eqs[j])) {
+        j++;
+      }
+      // cout << j << endl;
+      string cur = eqs.substr(i + 1, j - i - 1);
+      // cout << cur << endl;
+      bool has_x = cur.find('x') != string::npos;
+      if (op == '+') {
+        if (has_x) {
+          if (cur == "x") {
+            eqt.co_ += 1;
+          } else {
+            string s = cur.substr(0, cur.size() - 1);
+            eqt.co_ += stoi(s);
+          }
+        } else {
+          eqt.sum_ += stoi(cur);
+        }
+      } else {
+
+        if (has_x) {
+          if (cur == "x") {
+            eqt.co_ -= 1;
+          } else {
+            string s = cur.substr(0, cur.size() - 1);
+            eqt.co_ -= stoi(s);
+          }
+        } else {
+          eqt.sum_ -= stoi(cur);
+        }
+      }
+      i = j;
+    }
+    return eqt;
+  }
+};
+
+int main() {
+  Solution so;
+  auto ret = so.reduce("x+5-3+3x");
+  cout << "co_ is: " << ret.co_ << ": and sum: " << ret.sum_ << endl;
+}
