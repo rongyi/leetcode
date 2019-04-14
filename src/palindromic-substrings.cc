795a8dfddd94660dfcd3dd0e417839d82918fb6e
rongyi <sqrongyi@163.com>
2019-04-14 09:25:13 +0800

    Update parlindrom

---
+++ src/palindromic-substrings.cc
@@ -0,0 +1,23 @@
+// http://leetcode.com/problems/palindromic-substrings/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int countSubstrings(string s) {
+    const int n = s.size();
+    int ret = 0;
+    for (int i = 0; i < n; i++) {
+      // 以i为中心
+      for (int j = 0; i - j >= 0 && i + j < n && s[i - j] == s[i + j]; j++) {
+        ret++;
+      }
+      // 以 (i - 1), i为中心
+      for (int j = 0;
+           (i - 1 - j) >= 0 && (i + j) < n && s[i - 1 - j] == s[i + j]; j++) {
+        ret++;
+      }
+    }
+
+    return ret;
+  }
+};
