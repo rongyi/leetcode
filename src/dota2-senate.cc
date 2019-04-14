c60627a7cb9fe74dc17cc5b06b09ac5779ff4e51
rongyi <sqrongyi@163.com>
2019-04-14 22:17:20 +0800

    Update dota2 senate

---
+++ src/dota2-senate.cc
@@ -0,0 +1,36 @@
+// http://leetcode.com/problems/dota2-senate/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  string predictPartyVictory(string senate) {
+    queue<int> rq;
+    queue<int> dq;
+    const int n = senate.size();
+    for (int i = 0; i < n; i++) {
+      if (senate[i] == 'R') {
+        rq.push(i);
+      } else {
+        dq.push(i);
+      }
+    }
+    while (!rq.empty() && !dq.empty()) {
+      auto ridx = rq.front();
+      rq.pop();
+      auto didx = dq.front();
+      dq.pop();
+      // 干掉了d，同时把这个R继续入队列放入下一轮
+      // 轮与轮的区别就在于加了多少n
+      if (ridx < didx) {
+        rq.push(ridx + n);
+      } else {
+        // 干掉了R
+        dq.push(didx + n);
+      }
+    }
+    if (rq.size() > dq.size()) {
+      return "Radiant";
+    }
+    return "Dire";
+  }
+};
