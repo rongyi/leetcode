2b4dbba574e9adc29cb1c874470e10b6820a3fe8
rongyi <sqrongyi@163.com>
2019-04-18 06:38:13 +0800

    Update print tree

---
+++ src/print-binary-tree.cc
@@ -0,0 +1,40 @@
+// http://leetcode.com/problems/print-binary-tree/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<vector<string>> printTree(TreeNode *root) {
+    auto h = height(root);
+    auto w = pow(2, h) - 1;
+    vector<vector<string>> ret(h, vector<string>(w, ""));
+    recur(root, ret, 0, 0, w - 1);
+    return ret;
+  }
+private:
+  int height(TreeNode *root) {
+    if (!root) {
+      return 0;
+    }
+    auto hl = height(root->left);
+    auto hr = height(root->right);
+    return max(hl, hr) + 1;
+  }
+  int width(TreeNode *root) {
+    if (!root) {
+      return 0;
+    }
+    auto wl = width(root->left);
+    auto wr = width(root->right);
+    return max(wl, wr) * 2 + 1;
+  }
+  void recur(TreeNode *root, vector<vector<string>> &ret, int level, int l,
+             int r) {
+    if (!root) {
+      return;
+    }
+    auto mid = l + (r - l) / 2;
+    ret[level][mid] = to_string(root->val);
+    recur(root->left, ret, level + 1, l, mid - 1);
+    recur(root->right, ret, level + 1, mid + 1, r);
+  }
+};
