03354847f28097a46092aa9b6946c94e3329cdfa
rongyi <sqrongyi@163.com>
2019-04-18 20:05:16 +0800

    Update find k closest elements

---
+++ src/find-k-closest-elements.cc
@@ -0,0 +1,22 @@
+// http://leetcode.com/problems/find-k-closest-elements/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<int> findClosestElements(vector<int> &arr, int k, int x) {
+    int lo = 0;
+    int hi = arr.size() - 1;
+    while (hi - lo >= k) {
+      if (abs(arr[lo] - x) > abs(arr[hi] - x)) {
+        lo++;
+      } else {
+        hi--;
+      }
+    }
+    vector<int> ret;
+    for (int i = lo; i <= hi; i++) {
+      ret.push_back(arr[i]);
+    }
+    return ret;
+  }
+};
