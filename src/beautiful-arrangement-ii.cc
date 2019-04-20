cd9c5223564f1d49e932738185ab1188c1d3d6ae
rongyi <sqrongyi@163.com>
2019-04-20 08:52:18 +0800

    Update beautiful arrangement

---
+++ src/beautiful-arrangement-ii.cc
@@ -0,0 +1,23 @@
+// http://leetcode.com/problems/beautiful-arrangement-ii/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<int> constructArray(int n, int k) {
+    vector<int> ret;
+    int i = 1;
+    int j = n;
+    // n = 8, k = 3; res = [1 8 2 3 4 5 6 7] k:[7, 6, 1, ,1 ,1, 1, 1]
+    // n = 8, k = 4; res = [8 1 7 2 3 4 5 6]; k[7, 6, 5, 1,1, 1, 1]
+    // n = 8, k = 7; res = [8 1 7 2 6 3 5 4] ; k: [7 6 5 4 3 2 1]
+    while (i <= j) {
+      if (k > 1) {
+        ret.push_back(k-- % 2 ? i++ : j--);
+      } else {
+        ret.push_back(i++);
+      }
+    }
+
+    return ret;
+  }
+};
