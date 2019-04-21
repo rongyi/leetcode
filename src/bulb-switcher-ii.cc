8604816db21cc500082e81bb57c9b93a139ae54b
rongyi <sqrongyi@163.com>
2019-04-21 10:24:47 +0800

    Update bulb switcher

---
+++ src/bulb-switcher-ii.cc
@@ -0,0 +1,39 @@
+// http://leetcode.com/problems/bulb-switcher-ii/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  // https://leetcode.com/problems/bulb-switcher-ii/discuss/107278/Very-easy-to-understand-C%2B%2B-with-detailed-explanation
+  int flipLights(int n, int m) {
+    if (m == 0) {
+      // only the initial status: all on
+      return 1;
+    }
+    if (n == 1) {
+      if (m >= 1) {
+        return 2;
+      }
+    }
+    if (n == 2) {
+      if (m == 1) {
+        return 3;
+      }
+      if (m >= 2) {
+        return 4;
+      }
+    }
+    if (n >= 3) {
+      if (m == 1) {
+        return 4;
+      }
+      if (m == 2) {
+        return 7;
+      }
+      if (m >= 3) {
+        return 8;
+      }
+    }
+
+    return 0;
+  }
+};
