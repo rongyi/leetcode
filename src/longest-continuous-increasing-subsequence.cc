cdcb59f081db549863037e083a0cc82ea8063d0f
rongyi <sqrongyi@163.com>
2019-04-22 06:36:31 +0800

    Update longest continuous increasing subsequences

---
+++ src/longest-continuous-increasing-subsequence.cc
@@ -0,0 +1,20 @@
+// http://leetcode.com/problems/longest-continuous-increasing-subsequence/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int findLengthOfLCIS(vector<int> &nums) {
+    if (nums.empty()) {
+      return 0;
+    }
+    const int n = nums.size();
+    vector<int> dp(n, 1);
+    for (int i = 1; i < n; i++) {
+      if (nums[i - 1] < nums[i]) {
+        dp[i] += dp[i - 1];
+      }
+    }
+
+    return *max_element(dp.begin(), dp.end());
+  }
+};
