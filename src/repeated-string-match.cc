a8fb72ee8cbc34ea489bd78c958dd459c4f5cb5f
rongyi <sqrongyi@163.com>
2019-04-27 09:18:55 +0800

    Add repeatedStringMatch

---
+++ src/repeated-string-match.cc
@@ -0,0 +1,24 @@
+// http://leetcode.com/problems/repeated-string-match/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int repeatedStringMatch(string a, string b) {
+    string as = a;
+    // 很牛
+    for (int ret = 1; ret <= b.length() / a.length() + 2; ret++, as += a) {
+      if (as.find(b) != string::npos) {
+        return ret;
+      }
+    }
+    return -1;
+  }
+};
+
+int main() {
+  Solution so;
+  // auto ret = so.repeatedStringMatch("abcd", "cdabcdab");
+  // auto ret = so.repeatedStringMatch("baa", "abaab");
+  auto ret = so.repeatedStringMatch("abcd", "bcdab");
+  cout << ret << endl;
+}
