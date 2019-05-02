f211671fd4cd576d198f7ac1e0411b9ba92bc87a
rongyi <sqrongyi@163.com>
2019-05-02 18:55:08 +0800

    Update insert into binary search tree

---
+++ src/insert-into-a-binary-search-tree.cc
@@ -0,0 +1,37 @@
+// http://leetcode.com/problems/insert-into-a-binary-search-tree/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  TreeNode *insertIntoBST(TreeNode *root, int val) {
+    if (!root) {
+      return new TreeNode(val);
+    }
+    TreeNode *node = new TreeNode(val);
+    do_insert(root, node);
+
+    return root;
+  }
+
+private:
+  // node is node to be inserted to bst
+  // which root is hold by root
+  void do_insert(TreeNode *root, TreeNode *node) {
+    if (!root) {
+      return;
+    }
+    if (node->val < root->val) {
+      if (!root->left) {
+        root->left = node;
+        return;
+      }
+      do_insert(root->left, node);
+    } else {
+      if (!root->right) {
+        root->right = node;
+        return;
+      }
+      do_insert(root->right, node);
+    }
+  }
+};
