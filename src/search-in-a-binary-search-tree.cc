cea1fbbaffa0fd733b2ad2d7c7f23c7f4a4b45d9
rongyi <sqrongyi@163.com>
2019-05-02 18:43:15 +0800

    Update search in a binary search tree

---
+++ src/search-in-a-binary-search-tree.cc
@@ -0,0 +1,17 @@
+// http://leetcode.com/problems/search-in-a-binary-search-tree/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  TreeNode *searchBST(TreeNode *root, int val) {
+    if (!root) {
+      return nullptr;
+    }
+    if (root->val == val) {
+      return root;
+    } else if (val < root->val) {
+      return searchBST(root->left, val);
+    }
+    return searchBST(root->right, val);
+  }
+};
