7f561061d673bdb3b2715cc82e7133d29b3abff6
rongyi <sqrongyi@163.com>
2019-05-03 09:01:47 +0800

    Update kth largest element in a stream

---
+++ src/kth-largest-element-in-a-stream.cc
@@ -0,0 +1,26 @@
+// http://leetcode.com/problems/kth-largest-element-in-a-stream/description/
+#include "xxx.h"
+
+class KthLargest {
+public:
+  KthLargest(int k, vector<int> &nums) {
+    size_ = k;
+    for (auto num : nums) {
+      pq_.push(num);
+      if (pq_.size() > size_) {
+        pq_.pop();
+      }
+    }
+  }
+
+  int add(int val) {
+    pq_.push(val);
+    if (pq_.size() > size_) {
+      pq_.pop();
+    }
+    return pq_.top();
+  }
+private:
+  int size_;
+  priority_queue<int, vector<int>, greater<int>> pq_;
+};
