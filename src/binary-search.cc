1f544a6949811572eb4d268481ee1f1b7f7f009f
rongyi <sqrongyi@163.com>
2019-05-03 09:07:59 +0800

    Update binary search

---
+++ src/binary-search.cc
@@ -0,0 +1,22 @@
+// http://leetcode.com/problems/binary-search/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int search(vector<int> &nums, int target) {
+    const int n = nums.size();
+    int i = 0;
+    int j = n;
+    while (i < j) {
+      const int mid = i + (j - i) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] > target) {
+        j = mid;
+      } else {
+        i = mid + 1;
+      }
+    }
+    return -1;
+  }
+};
