ea2d5e882e0146a778808e8b2f4908779cd922de
rongyi <sqrongyi@163.com>
2019-05-05 12:31:50 +0800

    Update best time to buy and sell stock

--- src/best-time-to-buy-and-sell-stock-with-transaction-fee.cc
+++ src/best-time-to-buy-and-sell-stock-with-transaction-fee.cc
@@ -6,6 +6,9 @@ public:
   // 系列总结:
   // https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/discuss/108870/Most-consistent-ways-of-dealing-with-the-series-of-stock-problems
   int maxProfit(vector<int> &prices, int fee) {
+    // https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/discuss/108867/C%2B%2B-concise-solution-O(n)-time-O(1)-space
+    // s0 = profit having no stock
+    // s1 = profit having 1 stock
     // update s0 by selling the stock from s1, so s0 = max(s0, s1+p);
     // update s1 by buying the stock from s0, so s1 = max(s1, s0-p-fee);
 
