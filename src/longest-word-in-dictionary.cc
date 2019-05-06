4a834b3991a1b1e9ff202567a9d4077136342547
rongyi <sqrongyi@163.com>
2019-05-06 20:07:48 +0800

    Update longestWord

---
+++ src/longest-word-in-dictionary.cc
@@ -0,0 +1,30 @@
+// http://leetcode.com/problems/longest-word-in-dictionary/description/
+
+#include "xxx.h"
+
+class Solution {
+public:
+  string longestWord(vector<string> &words) {
+    sort(words.begin(), words.end());
+    unordered_set<string> cache;
+    string ret{};
+    for (auto &w : words) {
+      if (w.size() == 1 ||
+          cache.find(w.substr(0, w.size() - 1)) != cache.end()) {
+        // 排好序遇到的第一个就是字符排序最小的，否则只有更长的时候才可以
+        if (w.size() > ret.size()) {
+          ret = w;
+        }
+        cache.insert(w);
+      }
+    }
+    return ret;
+  }
+};
+
+int main() {
+  Solution so;
+  vector<string> input{"w", "wo", "wor", "worl", "world"};
+  auto ret = so.longestWord(input);
+  cout << ret << endl;
+}
