a77a762ce04c60e0a85c7ac2fa87fc8ba72275e3
rongyi <sqrongyi@163.com>
2019-05-07 11:24:31 +0800

    Update merge account

--- src/accounts-merge.cc
+++ src/accounts-merge.cc
@@ -17,43 +17,67 @@ public:
     for (auto &account : accounts) {
       emails.push_back(rest(account));
     }
+    // construct edge from node i to j
+    unordered_map<int, vector<int>> chain;
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        if (intersect(emails[i], emails[j])) {
+          chain[i].push_back(j);
+          chain[j].push_back(i);
+        }
+      }
+    }
 
     vector<vector<string>> ret;
     // processed index cache
-    unordered_set<int> idx;
+    unordered_set<int> visited;
     for (int i = 0; i < n; i++) {
       // already processed
-      if (idx.find(i) != idx.end()) {
+      if (visited.find(i) != visited.end()) {
         continue;
       }
-      idx.insert(i);
+      visited.insert(i);
       string cur_name = accounts[i][0];
-      unordered_set<string> &cur_set = emails[i];
+      unordered_set<string> cur_set = emails[i];
       vector<string> merged_record{cur_name};
+      dfs(visited, cur_set, emails, chain, i);
 
-      for (int j = i + 1; j < n; j++) {
-        unordered_set<string> test_set = emails[j];
-        if (intersect(cur_set, test_set)) {
-          // we merge j to i
-          // don't process accounts[j] when we iterate to j
-          idx.insert(j);
-          // merget two set ==> cur_set
-          for (auto &email : test_set) {
-            cout << "insert: " << email << endl;
-            cur_set.insert(email);
-          }
-        }
-      }
       for (auto &e : cur_set) {
         merged_record.push_back(e);
       }
+
       ret.push_back(merged_record);
     }
+
     for (auto &record : ret) {
       sort(next(record.begin()), record.end());
     }
 
     return ret;
+  }
+  void dfs(unordered_set<int> &visited, unordered_set<string> &eater,
+           vector<unordered_set<string>> &emails,
+           unordered_map<int, vector<int>> &chain, int cur_id) {
+    // already visited
+    if (visited.find(cur_id) != visited.end()) {
+      return;
+    }
+    // no route come or from this node
+    if (chain.find(cur_id) == chain.end()) {
+      return;
+    }
+    visited.insert(cur_id);
+    auto next_ids = chain[cur_id];
+    for (auto nid : next_ids) {
+      if (visited.find(nid) != visited.end()) {
+        continue;
+      }
+      // eat them all
+      for (auto &e : emails[nid]) {
+        eater.insert(e);
+      }
+      visited.insert(nid);
+    }
   }
   bool intersect(unordered_set<string> &set1, unordered_set<string> &set2) {
     for (const auto &w : set1) {
