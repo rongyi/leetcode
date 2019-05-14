0f30b7b6999d96e157ab5ccda4670ec81e44f4f9
rongyi <sqrongyi@163.com>
2019-05-14 10:00:41 +0800

    Update find smallest letters greater than target

---
+++ src/find-smallest-letter-greater-than-target.cc
@@ -0,0 +1,14 @@
+// http://leetcode.com/problems/find-smallest-letter-greater-than-target/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  char nextGreatestLetter(vector<char> &letters, char target) {
+    for (auto ch : letters) {
+      if (target < ch) {
+        return ch;
+      }
+    }
+    return letters[0];
+  }
+};
