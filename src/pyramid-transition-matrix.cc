1b07a0c16fe9103a9da0f1d29897f30d79326c68
rongyi <sqrongyi@163.com>
2019-05-18 07:40:14 +0800

    Update pyramid transition matrix

---
+++ src/pyramid-transition-matrix.cc
@@ -0,0 +1,28 @@
+// http://leetcode.com/problems/pyramid-transition-matrix/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  bool pyramidTransition(string bottom, vector<string> &allowed) {
+    unordered_map<string, vector<char>> tris;
+    for (auto &s : allowed) {
+      tris[s.substr(0, 2)].push_back(s.back());
+    }
+    return dfs(bottom, tris, 0, "");
+  }
+  bool dfs(string bottom, unordered_map<string, vector<char>> &tris, int start,
+           string next) {
+    if (bottom.size() == 1) {
+      return true;
+    }
+    if (start == (int)bottom.size() - 1) {
+      return dfs(next, tris, 0, "");
+    }
+    for (char c : tris[bottom.substr(start, 2)]) {
+      if (dfs(bottom, tris, start + 1, next + c)) {
+        return true;
+      }
+    }
+    return false;
+  }
+};
