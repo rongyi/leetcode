bdcc0a5e0ad5924f8c57ae2635824e814e6c28e5
rongyi <sqrongyi@163.com>
2019-05-21 19:20:58 +0800

    Update parlindrom integer

--- src/palindrome-number.cc
+++ src/palindrome-number.cc
@@ -10,25 +10,28 @@ public:
     if (x == 0) {
       return true;
     }
-    auto cp = x;
-    // digit count
-    int dc = 0;
-    while (x) {
-      ++dc;
-      x /= 10;
-    }
+    auto count = [](int i) {
+      int ret = 0;
+      while (i) {
+        ret++;
+        i /= 10;
+      }
+      return ret;
+    };
+
+    int dc = count(x);
     int align = 1;
     while (--dc) {
       align *= 10;
     }
 
-    x = cp;
     while (x) {
       auto high = x / align;
       auto low = x % 10;
       if (high != low) {
         return false;
       }
+      // 斩掉最高位，最低位
       x %= align;
       x /= 10;
 
