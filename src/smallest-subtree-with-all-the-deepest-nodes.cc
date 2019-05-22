c80625a7746fe15914b24ef4cd0c38c5f7c0008f
rongyi <sqrongyi@163.com>
2019-05-22 20:00:05 +0800

    Review code, drop comment

--- src/smallest-subtree-with-all-the-deepest-nodes.cc
+++ src/smallest-subtree-with-all-the-deepest-nodes.cc
@@ -1,21 +1,5 @@
 // https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes/description/
 #include "xxx.h"
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
-
-struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
 
 class Solution {
 public:
