457d498ef38982aa58ab53c00d1eae8373fe2fda
rongyi <sqrongyi@163.com>
2019-05-22 20:10:12 +0800

    Drop code

--- src/binary-tree-pruning.cc
+++ src/binary-tree-pruning.cc
@@ -1,24 +1,15 @@
 // http://www.lintcode.com/zh-cn/problem/binary-tree-pruning
 #include "xxx.h"
 
-
-// Definition for a binary tree node.
-struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
 class Solution {
 public:
-  TreeNode* pruneTree(TreeNode* root) {
+  TreeNode *pruneTree(TreeNode *root) {
     if (!root) {
       return nullptr;
     }
     root->left = pruneTree(root->left);
     root->right = pruneTree(root->right);
-    if (root->val == 0 && root->left == nullptr  && root->right == nullptr) {
+    if (root->val == 0 && root->left == nullptr && root->right == nullptr) {
       delete root;
       return nullptr;
     }
