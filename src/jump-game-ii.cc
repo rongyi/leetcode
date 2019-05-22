479fae9e2d60d39c699fa722544175db86dc841b
rongyi <sqrongyi@163.com>
2019-05-22 17:20:00 +0800

    Update jumpgame

--- src/jump-game-ii.cc
+++ src/jump-game-ii.cc
@@ -5,17 +5,18 @@ class Solution {
 public:
   int jump(vector<int> &nums) {
     const int n = nums.size();
+    int maximum = 0;
+    int next_maximum = 0;
     int ret = 0;
-    int range = 0;
-    int cur = 0;
-
-    for (int i = 0; i < n; ++i) {
-      // 出了当前最远跳的势力范围
-      if (i > range) {
+    for (int i = 0; i < n - 1; i++) {
+      next_maximum = max(next_maximum, nums[i] + i);
+      if (i == maximum) {
         ret++;
-        range = cur;
+        if (next_maximum >= n - 1) {
+          return ret;
+        }
+        maximum = next_maximum;
       }
-      cur = max(cur, i + nums[i]);
     }
     return ret;
   }
