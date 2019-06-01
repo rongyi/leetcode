dd018ddb482154956ddf37e5f315ee4b851e6124
rongyi <sqrongyi@163.com>
2019-06-01 16:38:34 +0800

    Update friends request

---
+++ src/friends-of-appropriate-ages.cc
@@ -0,0 +1,29 @@
+// http://leetcode.com/problems/friends-of-appropriate-ages/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int numFriendRequests(vector<int> &ages) {
+    unordered_map<int, int> count;
+    for (auto age : ages) {
+      count[age]++;
+    }
+    int ret = 0;
+    for (auto kv : count) {
+      for (auto kv2 : count) {
+        if (request(kv.first, kv2.first)) {
+          if (kv.first == kv2.first) {
+            ret += kv.second * (kv2.second - 1);
+          } else {
+            ret += kv.second * kv2.second;
+          }
+        }
+      }
+    }
+
+    return ret;
+  }
+  bool request(int a, int b) {
+    return !((b <= 0.5 * a + 7) || (b > a) || (b > 100 && a < 100));
+  }
+};
