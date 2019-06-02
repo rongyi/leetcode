c2e4eb60b8ac80f35ec87cfee67dda14c358983b
rongyi <sqrongyi@163.com>
2019-06-02 09:41:13 +0800

    Update positions of large groups

---
+++ src/positions-of-large-groups.cc
@@ -0,0 +1,26 @@
+// http://leetcode.com/problems/positions-of-large-groups/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<vector<int>> largeGroupPositions(string S) {
+    vector<vector<int>> ret;
+    // 和最后一个不一样即可
+    S.push_back((S[S.size() - 1] - 'a' + 1) % 26 + 'a');
+    const int n = S.size();
+    int prev = 0;
+    char cur = S[0];
+    for (int i = 1; i < n; i++) {
+      if (S[i] == cur) {
+        continue;
+      }
+      if (i - prev >= 3) {
+        ret.push_back({prev, i - 1});
+      }
+      prev = i;
+      cur = S[i];
+    }
+
+    return ret;
+  }
+};
