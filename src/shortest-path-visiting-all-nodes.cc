410f8897d303948cc594d4c097852b08bf1cbc23
rongyi <sqrongyi@163.com>
2019-06-08 20:13:47 +0800

    Update shortest path visited all nodes

---
+++ src/shortest-path-visiting-all-nodes.cc
@@ -0,0 +1,61 @@
+// http://leetcode.com/problems/shortest-path-visiting-all-nodes/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int shortestPathLength(vector<vector<int>> &graph) {
+    ret_ = numeric_limits<int>::max();
+    vector<int> cur;
+    vector<int> visited(graph.size(), 0);
+
+    int cur_count = 0;
+    for (int i = 0; i < graph.size(); i++) {
+      cur.push_back(i);
+      visited[i]++;
+      cur_count++;
+      dfs(graph, cur, visited, cur_count);
+      cur_count--;
+      visited[i]--;
+      cur.pop_back();
+    }
+    return ret_ - 1;
+  }
+
+private:
+  void dfs(vector<vector<int>> &graph, vector<int> &cur, vector<int> &visited,
+           int cur_count) {
+    if (ret_ == graph.size()) {
+      return;
+    }
+    if (cur_count == graph.size()) {
+      ret_ = min(ret_, int(cur.size()));
+    }
+    if (cur.size() >= ret_) {
+      return;
+    }
+    int pos = cur.back();
+
+    int minv = numeric_limits<int>::max();
+    for (auto n : graph[pos]) {
+      minv = min(minv, visited[n]);
+    }
+    for (auto n : graph[pos]) {
+      if (visited[n] == minv) {
+        cur.push_back(n);
+        if (!visited[n]) {
+          cur_count++;
+        }
+        visited[n]++;
+        dfs(graph, cur, visited, cur_count);
+        visited[n]--;
+        if (!visited[n]) {
+          cur_count--;
+        }
+        cur.pop_back();
+      }
+    }
+  }
+
+private:
+  int ret_;
+};
