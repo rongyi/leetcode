770f0cb4a58f3c563d54a16e19f86605cf8c5c42
rongyi <sqrongyi@163.com>
2019-06-08 08:14:31 +0800

    Update keys and rooms

---
+++ src/keys-and-rooms.cc
@@ -0,0 +1,27 @@
+// http://leetcode.com/problems/keys-and-rooms/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  bool canVisitAllRooms(vector<vector<int>> &rooms) {
+    unordered_set<int> visited;
+    queue<int> q;
+    q.push(0);
+    while (!q.empty()) {
+      auto cur = q.front();
+      q.pop();
+      if (visited.find(cur) == visited.end()) {
+        visited.insert(cur);
+        if (visited.size() == rooms.size()) {
+          return true;
+        }
+        for (auto key : rooms[cur]) {
+          if (visited.find(key) == visited.end()) {
+            q.push(key);
+          }
+        }
+      }
+    }
+    return visited.size() == rooms.size();
+  }
+};
