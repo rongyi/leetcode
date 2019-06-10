c1624ccc57e218fbe0e069f84b5cfea3f9097698
rongyi <sqrongyi@163.com>
2019-06-10 13:33:21 +0800

    Update shifting letters

---
+++ src/shifting-letters.cc
@@ -0,0 +1,18 @@
+// http://leetcode.com/problems/shifting-letters/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  string shiftingLetters(string S, vector<int> &shifts) {
+    const int n = S.size();
+    int prev = 0;
+    for (int i = n - 1; i >= 0; --i) {
+      shifts[i] = (shifts[i] % 26 + prev) % 26;
+      prev = shifts[i];
+    }
+    for (int i = 0; i < n; i++) {
+      S[i] = (S[i] - 'a' + shifts[i] ) % 26 + 'a';
+    }
+    return S;
+  }
+};
