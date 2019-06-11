0837190f35e46bfca0a2939014b3121d404e742a
rongyi <sqrongyi@163.com>
2019-06-12 07:17:54 +0800

    Update car fleet

---
+++ src/car-fleet.cc
@@ -0,0 +1,21 @@
+// http://leetcode.com/problems/car-fleet/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int carFleet(int target, vector<int> &position, vector<int> &speed) {
+    map<int, double> durations;
+    for (int i = 0; i < position.size(); i++) {
+      durations[-position[i]] = (double)(target - position[i]) / speed[i];
+    }
+    int ret = 0;
+    double cur = 0;
+    for (auto kv : durations) {
+      if (kv.second > cur) {
+        cur = kv.second;
+        ret++;
+      }
+    }
+    return ret;
+  }
+};
