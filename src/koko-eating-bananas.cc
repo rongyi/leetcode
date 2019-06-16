10ef8d9ad0d2abdde3ee41fb2de86dc9e5a91ca4
rongyi <sqrongyi@163.com>
2019-06-17 07:17:35 +0800

    Update koko eating bananas

---
+++ src/koko-eating-bananas.cc
@@ -0,0 +1,25 @@
+// http://leetcode.com/problems/koko-eating-bananas/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int minEatingSpeed(vector<int> &piles, int H) {
+    int l = 1;
+    int r = 1000000000;
+    while (l < r) {
+      int m = l + (r - l) / 2;
+      int total = 0;
+      for (auto p : piles) {
+        total += (p + m - 1) / m;
+      }
+      // 说明吃的慢了，要加快
+      if (total > H) {
+        l = m + 1;
+      } else {
+        // 说明吃的快乐，要放慢
+        r = m;
+      }
+    }
+    return l;
+  }
+};
