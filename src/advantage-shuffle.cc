9eec0a18c70bc7e8716e3743d6bdf462928e5d46
rongyi <sqrongyi@163.com>
2019-06-16 09:14:33 +0800

    Update advantage shuffle

--- src/advantage-shuffle.cc
+++ src/advantage-shuffle.cc
@@ -4,6 +4,26 @@
 class Solution {
 public:
   vector<int> advantageCount(vector<int> &A, vector<int> &B) {
+    map<int, int> count;
+    for (auto num : A) {
+      count[num]++;
+    }
+    vector<int> ret;
+    for (auto num : B) {
+      auto it = count.upper_bound(num);
+      int x = 0;
+      if (it == count.end()) {
+        x = count.begin()->first;
+      } else {
+        x = it->first;
+      }
+      count[x]--;
+      if (count[x] == 0) {
+        count.erase(x);
+      }
+      ret.push_back(x);
+    }
 
+    return ret;
   }
 };
