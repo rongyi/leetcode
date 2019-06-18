9ce68b3144f32864f8e4bdeb63d3795d8c05d742
rongyi <sqrongyi@163.com>
2019-06-19 07:42:55 +0800

    Update uncommon words from two sentences

---
+++ src/uncommon-words-from-two-sentences.cc
@@ -0,0 +1,31 @@
+// http://leetcode.com/problems/uncommon-words-from-two-sentences/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<string> uncommonFromSentences(string A, string B) {
+    auto gen = [](string &s) -> unordered_map<string, int> {
+      unordered_map<string, int> ret;
+      istringstream ss(s);
+      string cur{};
+      while (ss >> cur) {
+        ret[cur]++;
+      }
+      return ret;
+    };
+    auto acnt = gen(A);
+    auto bcnt = gen(B);
+    vector<string> ret;
+    for (auto kv : acnt) {
+      if (kv.second == 1 && bcnt.find(kv.first) == bcnt.end()) {
+        ret.push_back(kv.first);
+      }
+    }
+    for (auto kv : bcnt) {
+      if (kv.second == 1 && acnt.find(kv.first) == acnt.end()) {
+        ret.push_back(kv.first);
+      }
+    }
+    return ret;
+  }
+};
