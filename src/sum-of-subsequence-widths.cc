ddf2cddbd7386fdcf7be8e5a86a6c69b0a66cd84
rongyi <sqrongyi@163.com>
2019-06-27 09:19:18 +0800

    Update sum of subsequence wide

---
+++ src/sum-of-subsequence-widths.cc
@@ -0,0 +1,25 @@
+// http://leetcode.com/problems/sum-of-subsequence-widths/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int sumSubseqWidths(vector<int> &A) {
+    long c = 1;
+    long ret = 0;
+    long mod = pow(10, 9) + 7;
+    // For A[i]:
+    // There are i smaller numbers,
+    // so there are 2 ^ i sequences in which A[i] is maximum.
+    // we should do res += A[i] * (2 ^ i)
+
+    // There are n - i - 1 bigger numbers,
+    // so there are 2 ^ (n - i - 1) sequences in which A[i] is minimum.
+    // we should do res -= A[i] * 2 ^ (n - i - 1)
+
+    sort(A.begin(), A.end());
+    for (int i = 0; i < A.size(); ++i, c = (c << 1) % mod) {
+      ret = (ret + A[i] * c - A[A.size() - i - 1] * c) % mod;
+    }
+    return (ret + mod) % mod;
+  }
+};
