b508e36ce4434c52a14ff3cbc1533d6773ab2f5f
rongyi <sqrongyi@163.com>
2019-06-28 16:28:28 +0800

    Update range sum of bst
    commit at 呼和浩特白塔机场

---
+++ src/range-sum-of-bst.cc
@@ -0,0 +1,27 @@
+// http://leetcode.com/problems/range-sum-of-bst/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int rangeSumBST(TreeNode *root, int L, int R) {
+    int ret = 0;
+    recur(root, L, R, ret);
+
+    return ret;
+  }
+
+  void recur(TreeNode *root, int L, int R, int &ret) {
+    if (!root) {
+      return;
+    }
+    if (root->val >= L && root->val <= R) {
+      ret += root->val;
+    }
+    if (L < root->val) {
+      recur(root->left, L, R, ret);
+    }
+    if (root->val < R) {
+      recur(root->right, L, R, ret);
+    }
+  }
+};
