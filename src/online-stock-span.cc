33693cde868ea4282e32f411bb1d8fa6018e0c83
rongyi <sqrongyi@163.com>
2019-06-29 11:10:51 +0800

    Update online stock span

---
+++ src/online-stock-span.cc
@@ -0,0 +1,29 @@
+// http://leetcode.com/problems/online-stock-span/description/
+#include "xxx.h"
+
+class StockSpanner {
+public:
+  StockSpanner() {}
+
+  int next(int price) {
+    // including itself
+    int ret = 1;
+    while (!stk_.empty() && stk_.top().first <= price) {
+      ret += stk_.top().second;
+      stk_.pop();
+    }
+
+    stk_.push({price, ret});
+
+    return ret;
+  }
+
+private:
+  stack<pair<int, int>> stk_;
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
