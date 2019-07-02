6ffdcd446e3e0004414ffe3dc4f4f6dbe9ed0805
rongyi <sqrongyi@163.com>
2019-07-02 14:32:08 +0800

    Update small range i

---
+++ src/smallest-range-i.cc
@@ -0,0 +1,19 @@
+// http://leetcode.com/problems/smallest-range-i/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int smallestRangeI(vector<int> &A, int K) {
+    if (A.size() < 2) {
+      return 0;
+    }
+    int low = *min_element(A.begin(), A.end());
+    int high = *max_element(A.begin(), A.end());
+
+    auto delta = high - low;
+    if (delta - 2 * K > 0) {
+      return delta - 2 * K;
+    }
+    return 0;
+  }
+};
