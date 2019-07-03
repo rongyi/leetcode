e009fe89985d76e9614abd053c4f0abb4be261db
rongyi <sqrongyi@163.com>
2019-07-03 11:16:36 +0800

    Update smallest range II

---
+++ src/smallest-range-ii.cc
@@ -0,0 +1,21 @@
+// http://leetcode.com/problems/smallest-range-ii/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int smallestRangeII(vector<int> &A, int K) {
+    // 先排序，然后设想有一个点，这个点左边全是加，右边全是减K
+    // O(n)的推导出来
+    sort(A.begin(), A.end());
+    int left = A[0] + K;
+    int right = A[A.size() - 1] - K;
+    int ret = A[A.size() - 1] - A[0];
+    for (int i = 0; i < A.size() - 1; i++) {
+      int cur_max = max(A[i] + K, right);
+      int cur_min = min(left, A[i + 1] - K);
+      ret = min(ret, cur_max - cur_min);
+    }
+
+    return ret;
+  }
+};
