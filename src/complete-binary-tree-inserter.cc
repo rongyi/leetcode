6c9ef30d7a3249a4db58fc06cf9a217c4b210dab
rongyi <sqrongyi@163.com>
2019-07-06 10:29:33 +0800

    Update complete binary tree intserter

---
+++ src/complete-binary-tree-inserter.cc
@@ -0,0 +1,44 @@
+// http://leetcode.com/problems/complete-binary-tree-inserter/description/
+#include "xxx.h"
+
+class CBTInserter {
+public:
+  CBTInserter(TreeNode *root) {
+    root_ = root;
+    q_.push(root_);
+  }
+
+  int insert(int v) {
+    // 小蝌蚪找妈妈
+    TreeNode *mom = nullptr;
+    while (!mom) {
+      auto cur = q_.front();
+      if (!cur->left) {
+        mom = cur;
+        cur->left = new TreeNode(v);
+      } else if (!cur->right) {
+        mom = cur;
+        cur->right = new TreeNode(v);
+      } else {
+        q_.pop();
+        q_.push(cur->left);
+        q_.push(cur->right);
+      }
+    }
+
+    return mom->val;
+  }
+
+  TreeNode *get_root() { return root_; }
+
+private:
+  TreeNode *root_;
+  queue<TreeNode *> q_;
+};
+
+/**
+ * Your CBTInserter object will be instantiated and called as such:
+ * CBTInserter* obj = new CBTInserter(root);
+ * int param_1 = obj->insert(v);
+ * TreeNode* param_2 = obj->get_root();
+ */
