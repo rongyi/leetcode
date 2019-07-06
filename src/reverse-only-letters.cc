497443580fa146b6db3583eba5acc6a44c7ec0f1
rongyi <sqrongyi@163.com>
2019-07-06 08:26:37 +0800

    Update reverse only letters

---
+++ src/reverse-only-letters.cc
@@ -0,0 +1,29 @@
+// http://leetcode.com/problems/reverse-only-letters/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  string reverseOnlyLetters(string S) {
+    const int n = S.size();
+    int i = 0;
+    int j = n - 1;
+    while (i < j) {
+      while (i < j && !isalpha(S[i])) {
+        i++;
+      }
+      while (j > i && !isalpha(S[j])) {
+        j--;
+      }
+      swap(S[i], S[j]);
+      i++;
+      j--;
+    }
+    return S;
+  }
+};
+
+int main() {
+  Solution so;
+  auto ret = so.reverseOnlyLetters("?6C40E");
+  cout << ret << endl;
+}
