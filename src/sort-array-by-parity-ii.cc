d0a96a9eb0561991abe6d03d2b9a6a5f018f7de6
rongyi <sqrongyi@163.com>
2019-07-07 10:08:58 +0800

    Update sort array by parity

---
+++ src/sort-array-by-parity-ii.cc
@@ -0,0 +1,23 @@
+// http://leetcode.com/problems/sort-array-by-parity-ii/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<int> sortArrayByParityII(vector<int> &A) {
+    const int n = A.size();
+    int i = 0;
+    int j = n - 1;
+    while (i < n && j >= 0) {
+      for (; i < n && (A[i] % 2 == 0); i += 2)
+        ;
+      for (; j >= 0 && (A[j] % 2 == 1); j -= 2)
+        ;
+      if (i < n && j >= 0) {
+        swap(A[i], A[j]);
+        i += 2;
+        j -= 2;
+      }
+    }
+    return A;
+  }
+};
