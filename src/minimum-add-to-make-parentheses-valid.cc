b0a8b88f969b879f6d4ef0f498d8855c0b3b2ee0
rongyi <sqrongyi@163.com>
2019-07-07 09:53:43 +0800

    Update minimum add to make parentheses valid

---
+++ src/minimum-add-to-make-parentheses-valid.cc
@@ -0,0 +1,30 @@
+// http://leetcode.com/problems/minimum-add-to-make-parentheses-valid/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int minAddToMakeValid(string S) {
+    // 需要脑补左括号的个数
+    int left = 0;
+    // 需要脑补右括号的个数
+    int right = 0;
+    stack<char> stk;
+
+    for (auto c : S) {
+      if (c == '(') {
+        stk.push('(');
+      } else {
+        // 照道理遇到 )应该弹出来对应的 (
+        // 可万一没有呢，就脑补一个 (
+        if (stk.empty()) {
+          left++;
+        } else {
+          stk.pop();
+        }
+      }
+    }
+    // 栈里面剩下的( 都是没人匹配的，再脑补这么多的 )吧
+    right = stk.size();
+    return left + right;
+  }
+};
