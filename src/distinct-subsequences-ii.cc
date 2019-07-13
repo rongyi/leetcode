ad0be725759df1c6121bc9305e0cc539385ff752
rongyi <sqrongyi@163.com>
2019-07-13 09:19:52 +0800

    Update distinct subsequences

---
+++ src/distinct-subsequences-ii.cc
@@ -0,0 +1,30 @@
+// http://leetcode.com/problems/distinct-subsequences-ii/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int distinctSubseqII(string s) {
+    const int mod = pow(10, 9) + 7;
+    const int n = s.size();
+    // 包含自己
+    vector<int> dp(n, 1);
+    int ret = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < i; j++) {
+        if (s[j] != s[i]) {
+          dp[i] += dp[j];
+          dp[i] %= mod;
+        }
+      }
+
+      ret += dp[i];
+      ret %= mod;
+    }
+    return ret;
+  }
+};
+
+int main() {
+  Solution so;
+
+}
