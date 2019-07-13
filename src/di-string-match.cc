b3a9abfd3bdeb094469635f1180625f197f067d8
rongyi <sqrongyi@163.com>
2019-07-13 10:23:34 +0800

    Update di string match

---
+++ src/di-string-match.cc
@@ -0,0 +1,28 @@
+// http://leetcode.com/problems/di-string-match/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<int> diStringMatch(string S) {
+    const int n = S.size();
+    int left = 0;
+    int right = n;
+    vector<int> ret{};
+    int last = 0;
+    for (auto c : S) {
+      if (c == 'I') {
+        ret.push_back(left);
+        left++;
+        last = left;
+      } else {
+        ret.push_back(right);
+        right--;
+        last = right;
+      }
+    }
+    // now left+ 1 == right
+    ret.push_back(last);
+
+    return ret;
+  }
+};
