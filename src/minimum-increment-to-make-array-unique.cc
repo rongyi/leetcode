f285a938dc9b3d733fdc6cc6d8323ec081c74a5a
rongyi <sqrongyi@163.com>
2019-07-14 11:54:40 +0800

    Upate minimum increment to make array unique

---
+++ src/minimum-increment-to-make-array-unique.cc
@@ -0,0 +1,25 @@
+// http://leetcode.com/problems/minimum-increment-to-make-array-unique/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int minIncrementForUnique(vector<int> &A) {
+    sort(A.begin(), A.end());
+    int ret = 0;
+    // already sorted
+    for (int i = 1; i < A.size(); i++) {
+      if (A[i] <= A[i - 1]) {
+        ret += A[i - 1] + 1 - A[i];
+        A[i] = A[i - 1] + 1;
+      }
+    }
+    return ret;
+  }
+};
+
+int main() {
+  Solution so;
+  vector<int> input{7, 2, 7, 2, 1, 4, 3, 1, 4, 8};
+  auto ret = so.minIncrementForUnique(input);
+  cout << ret << endl;
+}
