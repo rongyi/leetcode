7b9baf9311a21f12fecab0bc6da5559ea2f84bf9
rongyi <sqrongyi@163.com>
2019-07-16 14:48:19 +0800

    Update leetcode

---
+++ src/largest-component-size-by-common-factor.cc
@@ -0,0 +1,9 @@
+// http://leetcode.com/problems/largest-component-size-by-common-factor/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int largestComponentSize(vector<int> &A) {
+
+  }
+};
