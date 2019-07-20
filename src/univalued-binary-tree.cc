68e3f341e0ad53e84a06ea7328ebf04f7687549d
rongyi <sqrongyi@163.com>
2019-07-20 12:08:48 +0800

    Update leetcode

---
+++ src/univalued-binary-tree.cc
@@ -0,0 +1,21 @@
+// http://leetcode.com/problems/univalued-binary-tree/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  bool isUnivalTree(TreeNode *root) {
+    // node range [1, 100]
+    const int value = root->val;
+    return check(root, value);
+  }
+  bool check(TreeNode *root, int value) {
+    // 空不是问题，就是不能相等
+    if (!root) {
+      return true;
+    }
+    if (root->val != value) {
+      return false;
+    }
+    return check(root->left, value) && check(root->right, value);
+  }
+};
