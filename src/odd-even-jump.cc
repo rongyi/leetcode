cdfba36c0aa5251e2734b31f510ec3e43f9aaabb
rongyi <sqrongyi@163.com>
2019-07-22 19:08:56 +0800

    Update oddEvenJumps

---
+++ src/odd-even-jump.cc
@@ -0,0 +1,12 @@
+// http://leetcode.com/problems/odd-even-jump/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  // 往前跳
+  // 1. 奇数次跳： 大于等于A[i]里面最小的
+  // 2. 偶数次跳： 小于等于A[i]里面最大的
+  int oddEvenJumps(vector<int> &A) {
+
+  }
+};
