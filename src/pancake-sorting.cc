61f1c762e3fcbe6d932a0dd3546ad98179b51bca
rongyi <sqrongyi@163.com>
2019-07-22 14:32:04 +0800

    Update leetcode

---
+++ src/pancake-sorting.cc
@@ -0,0 +1,22 @@
+// http://leetcode.com/problems/pancake-sorting/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<int> pancakeSort(vector<int> &A) {
+    vector<int> ret;
+    for (int i = 0; i < A.size(); i++) {
+
+      auto maxpos = max_element(A.begin(), A.end() - i);
+      ret.push_back(maxpos - A.begin() + 1);
+      ret.push_back(A.size() - i);
+
+      // 先把最大的翻最上面去
+      reverse(A.begin(), maxpos + 1);
+      // 这一次翻转，目的就是把当前最大的给它归位
+      reverse(A.begin(), A.end() - i);
+    }
+
+    return ret;
+  }
+};
