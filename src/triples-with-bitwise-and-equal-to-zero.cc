401871321dc373fa582986eb3855f986c7acf722
rongyi <sqrongyi@163.com>
2019-07-23 20:12:39 +0800

    Update leetcode

---
+++ src/triples-with-bitwise-and-equal-to-zero.cc
@@ -0,0 +1,23 @@
+// http://leetcode.com/problems/triples-with-bitwise-and-equal-to-zero/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int countTriplets(vector<int> &A) {
+    int tuples[1 << 16] = {};
+    for (auto a : A) {
+      for (auto b : A) {
+        ++tuples[a & b];
+      }
+    }
+    int ret = 0;
+    for (auto a : A) {
+      for (auto i = 0; i < (1 << 16); ++i) {
+        if ((i & a) == 0) {
+          ret += tuples[i];
+        }
+      }
+    }
+    return ret;
+  }
+};
