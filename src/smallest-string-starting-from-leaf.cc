c2eace084c879f508539ca114620075c05e145af
rongyi <sqrongyi@163.com>
2019-07-26 07:26:42 +0800

    Update leetcode

---
+++ src/smallest-string-starting-from-leaf.cc
@@ -0,0 +1,28 @@
+// http://leetcode.com/problems/smallest-string-starting-from-leaf/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  string smallestFromLeaf(TreeNode *root) {
+    vector<string> paths;
+    string cur{};
+    dfs(root, cur, paths);
+    sort(paths.begin(), paths.end());
+    return paths[0];
+  }
+  void dfs(TreeNode *root, string cur, vector<string> &paths) {
+    if (!root) {
+      return;
+    }
+
+    cur.push_back('a' + root->val);
+    if (!root->left && !root->right) {
+      reverse(cur.begin(), cur.end());
+      paths.push_back(cur);
+      return;
+    }
+
+    dfs(root->left, cur, paths);
+    dfs(root->right, cur, paths);
+  }
+};
