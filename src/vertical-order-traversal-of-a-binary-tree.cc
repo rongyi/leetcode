65d68418010988daa761616ba1d5197841353cfd
rongyi <sqrongyi@163.com>
2019-07-26 07:02:46 +0800

    Update leetcode

---
+++ src/vertical-order-traversal-of-a-binary-tree.cc
@@ -0,0 +1,29 @@
+// http://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<vector<int>> verticalTraversal(TreeNode *root) {
+    map<int, map<int, set<int>>> cache;
+    vector<vector<int>> ret;
+
+    recur(root, 0, 0, cache);
+
+    for (auto kv : cache) {
+      ret.push_back({});
+      for (auto kv2 : kv.second) {
+        ret.back().insert(end(ret.back()), begin(kv2.second), end(kv2.second));
+      }
+    }
+    return ret;
+  }
+  void recur(TreeNode *root, int x, int y,
+             map<int, map<int, set<int>>> &cache) {
+    if (!root) {
+      return;
+    }
+    cache[x][y].insert(root->val);
+    recur(root->left, x - 1, y + 1, cache);
+    recur(root->right, x + 1, y + 1, cache);
+  }
+};
