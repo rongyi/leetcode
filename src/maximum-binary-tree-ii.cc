c14beda328509950597d03f79829ad0a8545458b
rongyi <sqrongyi@163.com>
2019-08-01 10:58:19 +0800

    Update leetcode

---
+++ src/maximum-binary-tree-ii.cc
@@ -0,0 +1,15 @@
+// http://leetcode.com/problems/maximum-binary-tree-ii/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  TreeNode *insertIntoMaxTree(TreeNode *root, int val) {
+    if (!root || val > root->val) {
+      TreeNode *nr = new TreeNode(val);
+      nr->left = root;
+      return nr;
+    }
+    root->right = insertIntoMaxTree(root->right, val);
+    return root;
+  }
+};
