6459b3e6cd4accfa76526c94893c15a7a09b1a36
rongyi <sqrongyi@163.com>
2019-08-01 10:34:02 +0800

    Update leetcode

---
+++ src/find-the-town-judge.cc
@@ -0,0 +1,62 @@
+// http://leetcode.com/problems/find-the-town-judge/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int findJudge(int N, vector<vector<int>> &trust) {
+    unordered_set<int> all_nodes;
+    for (int i = 1; i <= N; i++) {
+      all_nodes.insert(i);
+    }
+    unordered_map<int, unordered_set<int>> graph;
+    unordered_set<int> trust_others;
+    for (auto &t : trust) {
+      graph[t[0]].insert(t[1]);
+      trust_others.insert(t[0]);
+    }
+    unordered_set<int> candi;
+    for (auto cur_node : all_nodes) {
+      if (graph.find(cur_node) != graph.end()) {
+        continue;
+      }
+      // this node trust no other
+      bool all_trust = true;
+      for (int i = 1; i <= N; i++) {
+        if (i == cur_node) {
+          continue;
+        }
+        if (graph.find(i) == graph.end() || graph[i].find(cur_node) == graph[i].end()) {
+          all_trust = false;
+          break;
+        }
+      }
+      if (all_trust) {
+        candi.insert(cur_node);
+      }
+    }
+    if (candi.size() != 1) {
+      return -1;
+    }
+    return *candi.begin();
+  }
+  void dfs(unordered_map<int, int> &graph, unordered_set<int> &candi, int cur_start, unordered_set<int> &visited) {
+    // fuck, there is a loop
+    if (visited.find(cur_start) != visited.end()) {
+      return;
+    }
+    // this node trust no one, so it is a judge candidate
+    if (graph.find(cur_start) == graph.end()) {
+      candi.insert(cur_start);
+      return;
+    }
+    visited.insert(cur_start);
+    dfs(graph, candi, graph[cur_start], visited);
+  }
+};
+
+int main() {
+  Solution so;
+  vector<vector<int>> input{{1, 2}, {2, 3}};
+  cout << so.findJudge(3, input) << endl;
+  return 0;
+}
---
+++ src/number-of-squareful-arrays.cc
@@ -0,0 +1,31 @@
+// http://leetcode.com/problems/number-of-squareful-arrays/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  // https://leetcode.com/problems/number-of-squareful-arrays/discuss/238612/4msC%2B%2B-Simple-Backtracking-like-Permutations-II
+  int numSquarefulPerms(vector<int> &A) {
+    sort(A.begin(), A.end());
+    int ret = 0;
+    dfs(A, 0, ret);
+    return ret;
+  }
+  void dfs(vector<int> A, int cur_idx, int &ret) {
+    if (cur_idx >= A.size()) {
+      ++ret;
+    }
+    for (int i = cur_idx; i < A.size(); i++) {
+      if (i > cur_idx && A[i] == A[cur_idx]) {
+        continue;
+      }
+      swap(A[i], A[cur_idx]);
+      if ((cur_idx == 0) || (cur_idx > 0 && isSquare(A[cur_idx] + A[cur_idx - 1]))) {
+        dfs(A, cur_idx + 1, ret);
+      }
+    }
+  }
+  bool isSquare(int v) {
+    int r = sqrt(v);
+    return r * r == v;
+  }
+};
