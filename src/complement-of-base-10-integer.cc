3261ce2cb69999682a5a20a47e501a0b2385f74a
rongyi <sqrongyi@163.com>
2019-08-06 16:03:16 +0800

    Update leetcode

---
+++ src/complement-of-base-10-integer.cc
@@ -0,0 +1,22 @@
+// http://leetcode.com/problems/complement-of-base-10-integer/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int bitwiseComplement(int N) {
+    if (N == 0) {
+      return 1;
+    }
+    int dc = 0;
+    int cp = N;
+    while (N) {
+      ++dc;
+      N >>= 1;
+    }
+    int ret = 0;
+    for (int i = 0; i < dc; i++) {
+      ret |= ((((cp >> i) & 1) ^ 1) << i);
+    }
+    return ret;
+  }
+};
