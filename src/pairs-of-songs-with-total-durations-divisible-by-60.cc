e18f48295d49f29ef9f3d9acefd335462cc41b55
rongyi <sqrongyi@163.com>
2019-08-06 16:41:14 +0800

    Update leetcode

---
+++ src/pairs-of-songs-with-total-durations-divisible-by-60.cc
@@ -0,0 +1,16 @@
+// http://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int numPairsDivisibleBy60(vector<int> &time) {
+    vector<int> count(60, 0);
+    int ret = 0;
+    for (auto t : time) {
+      ret += count[(60 - t % 60) % 60];
+      ++count[t % 60];
+    }
+
+    return ret;
+  }
+};
