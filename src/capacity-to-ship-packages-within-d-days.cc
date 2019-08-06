bcdcb8ec73445d074fe00aabccd4b948b2b8f9ba
rongyi <sqrongyi@163.com>
2019-08-06 17:04:09 +0800

    Update leetcode

--- src/capacity-to-ship-packages-within-d-days.cc
+++ src/capacity-to-ship-packages-within-d-days.cc
@@ -4,14 +4,18 @@
 class Solution {
 public:
   int shipWithinDays(vector<int> &weights, int D) {
+    // 口袋最大就是一次全吃，那就是装所有
     auto r = accumulate(weights.begin(), weights.end(), 0);
+    // 口袋最小那就是至少能装起来最大的那个
     auto l = max(r / D, *max_element(weights.begin(), weights.end()));
 
     while (l < r) {
       auto m = (l + r) / 2;
+      // 需要的口袋少了，那说明口袋敞口偏大，缩一缩
       if (countDays(weights, m) <= D) {
         r = m;
       } else {
+        // 否则口袋用多了，那就是口子小，放一放
         l = m + 1;
       }
     }
