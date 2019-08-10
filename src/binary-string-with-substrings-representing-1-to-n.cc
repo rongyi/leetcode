a5ebf2b4b0c532f00c9b5ea3bf4ea4c3842aa01e
rongyi <sqrongyi@163.com>
2019-08-10 10:34:24 +0800

    Update leetcode

---
+++ src/binary-string-with-substrings-representing-1-to-n.cc
@@ -0,0 +1,20 @@
+// http://leetcode.com/problems/binary-string-with-substrings-representing-1-to-n/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  // 喷的很惨
+  bool queryString(string S, int N) {
+    while (N > 0) {
+      auto s = bitset<32>(N--).to_string();
+      if (S.find(s.substr(s.find("1"))) == string::npos) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+int main() {
+  cout << bitset<32>(63).to_string() << endl;
+}
--- src/smallest-integer-divisible-by-k.cc
+++ src/smallest-integer-divisible-by-k.cc
@@ -3,6 +3,7 @@
 
 class Solution {
 public:
+  // 喷的很惨
   int smallestRepunitDivByK(int K) {
     for (int m = 1, N = 1 % K; N <= K; m = (m * 10 + 1) % K, ++N) {
       if (m == 0) {
