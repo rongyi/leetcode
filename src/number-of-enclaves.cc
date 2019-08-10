e69d1a539cf5efb2e27b6c5cd206688b8d674c50
rongyi <sqrongyi@163.com>
2019-08-10 21:17:01 +0800

    Update leetcode

---
+++ src/number-of-enclaves.cc
@@ -0,0 +1,42 @@
+// http://leetcode.com/problems/number-of-enclaves/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int numEnclaves(vector<vector<int>> &A) {
+    const int m = A.size();
+    const int n = A[0].size();
+    int ret = 0;
+    for (int i = 0; i < m; i++) {
+      dfs(A, i, 0);
+      dfs(A, i, n - 1);
+    }
+    for (int j = 0; j < n; j++) {
+      dfs(A, 0, j);
+      dfs(A, m - 1, j);
+    }
+
+    for (int i = 0; i < A.size(); i++) {
+      for (int j = 0; j < A[0].size(); j++) {
+        if (A[i][j] == 1) {
+          ++ret;
+        }
+      }
+    }
+
+    return ret;
+  }
+
+private:
+  void dfs(vector<vector<int>> &grid, int i, int j) {
+    if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() ||
+        grid[i][j] == 0) {
+      return;
+    }
+    grid[i][j] = 0;
+    dfs(grid, i + 1, j);
+    dfs(grid, i - 1, j);
+    dfs(grid, i, j - 1);
+    dfs(grid, i, j + 1);
+  }
+};
