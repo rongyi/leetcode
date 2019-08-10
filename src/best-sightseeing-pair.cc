c518649f4d32e9042ddf85d1c868b387abad9668
rongyi <sqrongyi@163.com>
2019-08-10 10:18:12 +0800

    Update leetcode

---
+++ src/best-sightseeing-pair.cc
@@ -0,0 +1,17 @@
+// http://leetcode.com/problems/best-sightseeing-pair/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int maxScoreSightseeingPair(vector<int> &A) {
+    int maxi = A[0] - 1;
+    int ret = 0;
+    for (int j = 1; j < A.size(); j++, --maxi) {
+      ret = max(ret, A[j] + maxi);
+      // 可以从j这里开始吗？
+      maxi = max(maxi, A[j]);
+    }
+
+    return ret;
+  }
+};
