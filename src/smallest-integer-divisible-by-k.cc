d87104a86fe038e7b24883d7f614d68d2a954e03
rongyi <sqrongyi@163.com>
2019-08-10 10:22:47 +0800

    Update leetcode

---
+++ src/smallest-integer-divisible-by-k.cc
@@ -0,0 +1,14 @@
+// http://leetcode.com/problems/smallest-integer-divisible-by-k/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int smallestRepunitDivByK(int K) {
+    for (int m = 1, N = 1 % K; N <= K; m = (m * 10 + 1) % K, ++N) {
+      if (m == 0) {
+        return N;
+      }
+    }
+    return -1;
+  }
+};
