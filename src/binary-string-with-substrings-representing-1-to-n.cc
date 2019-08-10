ae8ab65a1f53fc88948592ab880b5c92cf20a4e3
rongyi <sqrongyi@163.com>
2019-08-10 10:34:48 +0800

    Update leetcode

--- src/binary-string-with-substrings-representing-1-to-n.cc
+++ src/binary-string-with-substrings-representing-1-to-n.cc
@@ -6,6 +6,7 @@ public:
   // 喷的很惨
   bool queryString(string S, int N) {
     while (N > 0) {
+      // 这里需要记住一下
       auto s = bitset<32>(N--).to_string();
       if (S.find(s.substr(s.find("1"))) == string::npos) {
         return false;
