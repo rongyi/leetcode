5daf50c35deb1dc753a471442212ad57b53bebd8
rongyi <sqrongyi@163.com>
2019-08-10 20:34:01 +0800

    Update leetcode

---
+++ src/binary-prefix-divisible-by-5.cc
@@ -0,0 +1,28 @@
+// http://leetcode.com/problems/binary-prefix-divisible-by-5/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<bool> prefixesDivBy5(vector<int> &A) {
+    int prefix = 0;
+    vector<bool> ret;
+    for (int i = 0; i < A.size(); i++) {
+      prefix <<= 1;
+
+      prefix |= A[i];
+      if (prefix % 5 == 0) {
+        ret.push_back(true);
+      } else {
+        ret.push_back(false);
+      }
+      prefix %= 5;
+    }
+    return ret;
+  }
+};
+
+int main() {
+  Solution so;
+  vector<int> input{0, 1, 1, 1, 1, 1};
+  so.prefixesDivBy5(input);
+}
