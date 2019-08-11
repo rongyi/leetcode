5f5d9ce5ecea622f98163ed92d7e3e4a1bff1e09
rongyi <sqrongyi@163.com>
2019-08-11 21:14:18 +0800

    Update leetcode

---
+++ src/longest-arithmetic-sequence.cc
@@ -0,0 +1,22 @@
+// http://leetcode.com/problems/longest-arithmetic-sequence/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int longestArithSeqLength(vector<int> &A) {
+    // dp[diff][index]
+    unordered_map<int, unordered_map<int, int>> dp;
+    int ret = 2;
+    const int n = A.size();
+    for (int i = 0; i < n; ++i) {
+      for (int j = i + 1; j < n; ++j) {
+        int d = A[j] - A[i];
+        dp[d][j] = dp[d].count(i) ? dp[d][i] + 1 : 2;
+
+        ret = max(ret, dp[d][j]);
+      }
+    }
+
+    return ret;
+  }
+};
