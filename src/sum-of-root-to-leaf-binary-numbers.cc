a5a242a0b99a57e91d6c82dde3d7a45c1ef10b14
rongyi <sqrongyi@163.com>
2019-08-11 11:02:49 +0800

    Update leetcode

---
+++ src/sum-of-root-to-leaf-binary-numbers.cc
@@ -0,0 +1,26 @@
+// http://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int sumRootToLeaf(TreeNode *root) {
+    int ret = 0;
+    dfs(root, 0, ret);
+    return ret;
+  }
+private:
+  void dfs(TreeNode *root, int cur, int &sum) {
+    if (!root) {
+      return;
+    }
+    // a leaf
+    if (!root->left && !root->right) {
+      sum += (cur | root->val);
+      return;
+    }
+    cur |= root->val;
+
+    dfs(root->left, cur << 1, sum);
+    dfs(root->right, cur << 1, sum);
+  }
+};
