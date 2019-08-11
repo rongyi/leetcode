6e934f953200c3ceb48c5d37af5468e9b5dd66ac
rongyi <sqrongyi@163.com>
2019-08-11 11:44:16 +0800

    Update leetcode

--- src/camelcase-matching.cc
+++ src/camelcase-matching.cc
@@ -5,19 +5,21 @@ class Solution {
 public:
   vector<bool> camelMatch(vector<string> &qs, string pattern) {
     vector<bool> ret;
+
     for (int i = 0; i < qs.size(); i++) {
       auto &curq = qs[i];
-      int p = 0;
+      // 比较的方式就是逐个比较，遇到大写的不等就赶紧闪
       int j = 0;
-      for (; j < curq.size(); ++j) {
-        if (p < pattern.size() && curq[j] == pattern[p]) {
-          ++p;
-        } else if (!islower(curq[j])) {
+      int p = 0;
+      for (; j < curq.size(); j++) {
+        if (p < pattern.size() && pattern[p] == curq[j]) {
+          p++;
+        } else if (isupper(curq[j])) {
           break;
         }
-      }
+      } // inner for end
       ret.push_back(j == curq.size() && p == pattern.size());
-    }
+    } // outer for
     return ret;
   }
 };
