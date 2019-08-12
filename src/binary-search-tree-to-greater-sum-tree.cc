4be5dddde8057197f5ef44b13444722710f90dc2
rongyi <sqrongyi@163.com>
2019-08-13 07:31:16 +0800

    Update leetcode

---
+++ src/binary-search-tree-to-greater-sum-tree.cc
@@ -0,0 +1,31 @@
+// http://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  TreeNode *bstToGst(TreeNode *root) {
+    inorder(root, 0);
+    return root;
+  }
+
+  int inorder(TreeNode *root, int right_sum) {
+    if (!root) {
+      return right_sum;
+    }
+    auto sum = inorder(root->right, right_sum);
+    root->val += sum;
+
+    inorder(root->left, root->val);
+
+    // if (root->left) {
+    //   return root->left->val;
+    // }
+    // return root->val;
+    // 返回最左节点的那个值
+    TreeNode *p = root;
+    while (p->left) {
+      p = p->left;
+    }
+    return p->val;
+  }
+};
