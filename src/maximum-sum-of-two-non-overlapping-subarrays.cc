12b06a40b363506bddafaeee70a25d838620e214
rongyi <sqrongyi@163.com>
2019-08-12 11:05:26 +0800

    Update leetcode

--- src/maximum-sum-of-two-non-overlapping-subarrays.cc
+++ src/maximum-sum-of-two-non-overlapping-subarrays.cc
@@ -7,9 +7,12 @@ public:
     return max(maxNoOverlap(A, L, M, A.size()),
                maxNoOverlap(A, M, L, A.size()));
   }
+
 private:
   int maxNoOverlap(vector<int> &A, int L, int M, int len) {
+    // 值放在 1 ... len + 1
     vector<int> left(len + 1, 0);
+    // 值放在 0 ... len
     vector<int> right(len + 1, 0);
 
     int sum_left = 0;
@@ -17,20 +20,47 @@ private:
     for (int i = 0, j = len - 1; i < len; ++i, --j) {
       sum_left += A[i];
       sum_right += A[j];
+      // left fix size: L
+      if (i >= L) {
+        sum_left -= A[i - L];
+      }
+      // right fix size: M
+      if (j < len - M) {
+        sum_right -= A[j + M];
+      }
+
       left[i + 1] = max(left[i], sum_left);
       right[j] = max(right[j + 1], sum_right);
-
-      if (i + 1 >= L) {
-        sum_left -= A[i + 1 - L];
-      }
-      if (i + 1 >= M) {
-        sum_right -= A[j + M - 1];
-      }
     }
+
     int ret = 0;
     for (auto i = 0; i < len; ++i) {
+      // 这里有个问题： A[i]包含在哪个 sum里面？
+      // right里面，前面的定义时的描述说明了
       ret = max(ret, left[i] + right[i]);
     }
+
     return ret;
   }
 };
+
+int main() {
+  vector<int> input{1, 7, 3, 2, 8};
+  int sum = 0;
+  for (int i = 0; i < input.size(); ++i) {
+    sum += input[i];
+    if (i >= 2) {
+      sum -= input[i - 2];
+    }
+    cout << sum << endl;
+  }
+  cout << "xx" << endl;
+  sum = 0;
+  for (int j = input.size() - 1; j >= 0; --j) {
+    sum += input[j];
+    if (j < input.size() - 2) {
+      sum -= input[j + 2];
+    }
+    cout << sum << endl;
+  }
+}
