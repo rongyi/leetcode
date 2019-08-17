7a5af4f89c3449a10480e8496829920535384c4e
rongyi <sqrongyi@163.com>
2019-08-17 10:21:16 +0800

    Update leetcode

--- src/last-stone-weight-ii.cc
+++ src/last-stone-weight-ii.cc
@@ -4,6 +4,14 @@
 class Solution {
 public:
   int lastStoneWeightII(vector<int> &stones) {
+
+    // We add/subtract each stone to/from all possible sums achievable after
+    // processing previous stones. In the end, we return the smallest absolute
+    // value.
+
+    // To track possible sums, we need to use hash set to avoid duplicates (and
+    // TLE). We could also use a byte array, but the code seemed a bit more
+    // complicated to me.
     unordered_set<int> s1{0};
     for (auto cur_stone : stones) {
       unordered_set<int> s2{};
