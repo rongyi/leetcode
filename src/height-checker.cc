55f5b71efb41258bb03ed4ea1617e6fd0e603122
rongyi <sqrongyi@163.com>
2019-08-17 20:47:26 +0800

    Update leetcode

---
+++ src/height-checker.cc
@@ -0,0 +1,18 @@
+// http://leetcode.com/problems/height-checker/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int heightChecker(vector<int> &heights) {
+    int ret = 0;
+    vector<int> cp = heights;
+    sort(begin(cp), end(cp));
+    for (int i = 0; i < heights.size(); ++i) {
+      if (cp[i] != heights[i]) {
+        ++ret;
+      }
+    }
+
+    return ret;
+  }
+};
