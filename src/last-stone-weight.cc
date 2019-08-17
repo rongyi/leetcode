2355a7d41209f6a9f660d9ac8c1f6b5a6e4eaa35
rongyi <sqrongyi@163.com>
2019-08-17 08:41:50 +0800

    Update leetcode

---
+++ src/last-stone-weight.cc
@@ -0,0 +1,25 @@
+// http://leetcode.com/problems/last-stone-weight/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int lastStoneWeight(vector<int> &stones) {
+    priority_queue<int> pq;
+    for (auto stone: stones) {
+      pq.push(stone);
+    }
+
+    while (pq.size() > 1) {
+      auto l = pq.top();
+      pq.pop();
+      auto r = pq.top();
+      pq.pop();
+
+      if (l != r) {
+        pq.push(abs(l - r));
+      }
+    }
+
+    return pq.empty() ? 0 : pq.top();
+  }
+};
