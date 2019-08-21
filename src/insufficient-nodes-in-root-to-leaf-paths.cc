171d9e50b018ee4063ea432d18416aa11e6d4919
rongyi <sqrongyi@163.com>
2019-08-21 14:49:36 +0800

    Update leetcode

--- src/insufficient-nodes-in-root-to-leaf-paths.cc
+++ src/insufficient-nodes-in-root-to-leaf-paths.cc
@@ -1,18 +1,19 @@
 // http://leetcode.com/problems/insufficient-nodes-in-root-to-leaf-paths/description/
 #include "xxx.h"
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
+  // 喷的很厉害
   TreeNode *sufficientSubset(TreeNode *root, int limit) {
-
+    if (root->left == root->right) {
+      return root->val < limit ? nullptr : root;
+    }
+    if (root->left) {
+      root->left = sufficientSubset(root->left, limit - root->val);
+    }
+    if (root->right) {
+      root->right = sufficientSubset(root->right, limit - root->val);
+    }
+    return root->left == root->right ? nullptr : root;
   }
 };
