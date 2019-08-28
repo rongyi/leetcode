a17fe24a7a12ce0a82c591fb8b89f0f5b0cae2a1
rongyi <sqrongyi@163.com>
2019-08-28 19:33:03 +0800

    Update leetcode

---
+++ src/corporate-flight-bookings.cc
@@ -0,0 +1,20 @@
+// http://leetcode.com/problems/corporate-flight-bookings/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<int> corpFlightBookings(vector<vector<int>> &bookings, int n) {
+    vector<int> ret(n, 0);
+    for (auto &vec : bookings) {
+      ret[vec[0] - 1] += vec[2];
+      if (vec[1] < n) {
+        ret[vec[1]] -= vec[2];
+      }
+    }
+    for (int i = 1; i < n; ++i) {
+      ret[i] += ret[i - 1];
+    }
+
+    return ret;
+  }
+};
