8b0cef1d4ac16b6dc442b70f303613a69760be9c
rongyi <sqrongyi@163.com>
2019-10-10 08:58:52 +0800

    Update print in order

---
+++ src/print-in-order.cc
@@ -0,0 +1,39 @@
+// http://leetcode.com/problems/print-in-order/description/
+#include "xxx.h"
+#include <mutex>
+
+class Foo {
+public:
+  Foo() : count_{0} {}
+
+  void first(function<void()> printFirst) {
+
+    std::lock_guard<std::mutex> guard(m_);
+    ++count_;
+    // printFirst() outputs "first". Do not change or remove this line.
+    printFirst();
+  }
+
+  void second(function<void()> printSecond) {
+
+    while (count_ < 1) {
+    }
+    std::lock_guard<std::mutex> guard(m_);
+    ++count_;
+    // printSecond() outputs "second". Do not change or remove this line.
+    printSecond();
+  }
+
+  void third(function<void()> printThird) {
+
+    while (count_ < 2) {
+    }
+    std::lock_guard<std::mutex> guard(m_);
+    // printThird() outputs "third". Do not change or remove this line.
+    printThird();
+  }
+
+private:
+  mutable std::mutex m_;
+  int count_;
+};
