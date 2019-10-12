7f81d1e7099b670617d4f02fdc4984c20a467bcd
rongyi <sqrongyi@163.com>
2019-10-12 09:13:57 +0800

    Update worklog

---
+++ src/print-foobar-alternately.cc
@@ -0,0 +1,38 @@
+// http://leetcode.com/problems/print-foobar-alternately/description/
+#include "xxx.h"
+#include <mutex>
+
+class FooBar {
+
+public:
+  FooBar(int n) {
+    this->n = n;
+
+    m2_.lock();
+  }
+
+  void foo(function<void()> printFoo) {
+
+    for (int i = 0; i < n; i++) {
+      m1_.lock();
+      // printFoo() outputs "foo". Do not change or remove this line.
+      printFoo();
+      m2_.unlock();
+    }
+  }
+
+  void bar(function<void()> printBar) {
+
+    for (int i = 0; i < n; i++) {
+      m2_.lock();
+      // printBar() outputs "bar". Do not change or remove this line.
+      printBar();
+      m1_.unlock();
+    }
+  }
+
+private:
+  std::mutex m1_;
+  std::mutex m2_;
+  int n;
+};
---
+++ src/print-zero-even-odd.cc
@@ -0,0 +1,50 @@
+// http://leetcode.com/problems/print-zero-even-odd/description/
+#include "xxx.h"
+#include <mutex>
+
+// 多线程编程，运用锁的玩法
+class ZeroEvenOdd {
+
+public:
+  ZeroEvenOdd(int n) : cur_(0) {
+    this->n = n;
+    odd_.lock();
+    even_.lock();
+  }
+
+  // printNumber(x) outputs "x", where x is an integer.
+  void zero(function<void(int)> printNumber) {
+    for (int i = 0; i < n; ++i) {
+      zero_.lock();
+      printNumber(0);
+      if (++cur_ % 2) {
+        odd_.unlock();
+      } else {
+        even_.unlock();
+      }
+    }
+  }
+
+  void even(function<void(int)> printNumber) {
+    for (int i = 0; i < n / 2; ++i) {
+      even_.lock();
+      printNumber(cur_);
+      zero_.unlock();
+    }
+  }
+
+  void odd(function<void(int)> printNumber) {
+    for (int i = 0; i < (n + 1) / 2; ++i) {
+      odd_.lock();
+      printNumber(cur_);
+      zero_.unlock();
+    }
+  }
+
+private:
+  int n;
+  int cur_;
+  std::mutex zero_;
+  std::mutex even_;
+  std::mutex odd_;
+};
