2d6aa5ab38b021a5ea3e77e554d6525318143370
rongyi <sqrongyi@163.com>
2019-10-14 07:26:39 +0800

    Update leetcode

---
+++ src/number-of-equivalent-domino-pairs.cc
@@ -0,0 +1,20 @@
+// http://leetcode.com/problems/number-of-equivalent-domino-pairs/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int numEquivDominoPairs(vector<vector<int>> &dominoes) {
+    // 1 <= dominoes[i][j] <= 9
+    unordered_map<int, int> count;
+    int ret = 0;
+    for (auto &domino : dominoes) {
+      if (domino[0] <= domino[1]) {
+        ret += count[domino[0] * 10 + domino[1]]++;
+      } else {
+        ret += count[domino[1] * 10 + domino[0]]++;
+      }
+    }
+
+    return ret;
+  }
+};
