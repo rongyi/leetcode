2071565e239ec01679b8f61bc73a398c6a472a23
rongyi <sqrongyi@163.com>
2019-10-15 07:49:53 +0800

    Update leetcode

---
+++ src/maximum-of-absolute-value-expression.cc
@@ -0,0 +1,23 @@
+// http://leetcode.com/problems/maximum-of-absolute-value-expression/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int maxAbsValExpr(vector<int> &arr1, vector<int> &arr2) {
+    int ret = 0;
+    vector<vector<int>> sign = {{1, 1, 1},   {-1, 1, 1},  {1, -1, 1},
+                                {1, 1, -1},  {1, -1, -1}, {-1, -1, 1},
+                                {-1, 1, -1}, {-1, -1, -1}};
+    for (auto s : sign) {
+      int mini = numeric_limits<int>::max();
+      int maxi = numeric_limits<int>::min();
+      for (int i = 0; i < arr1.size(); ++i) {
+        mini = min(mini, s[0] * arr1[i] + s[1] * arr2[i] + s[2] * i);
+        maxi = max(maxi, s[0] * arr1[i] + s[1] * arr2[i] + s[2] * i);
+      }
+      ret = max(ret, maxi - mini);
+    }
+
+    return ret;
+  }
+};
