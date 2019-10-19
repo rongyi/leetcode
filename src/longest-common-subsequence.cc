221c86a3d0f6df23777ac982c00c00362f7d92a3
rongyi <sqrongyi@163.com>
2019-10-19 11:04:06 +0800

    Update lcs

---
+++ src/longest-common-subsequence.cc
@@ -0,0 +1,22 @@
+// http://leetcode.com/problems/longest-common-subsequence/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int longestCommonSubsequence(string text1, string text2) {
+    const int m = text1.size();
+    const int n = text2.size();
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+    for (int i = 1; i <= m; i++) {
+      for (int j = 1; j <= n; j++) {
+        if (text1[i - 1] == text2[j - 1]) {
+          dp[i][j] = dp[i - 1][j - 1] + 1;
+        } else {
+          dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+      }
+    }
+
+    return dp[m][n];
+  }
+};
