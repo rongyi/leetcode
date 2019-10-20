a993c8bd8b2f702bc2089879d0d50c17e252aaf4
rongyi <sqrongyi@163.com>
2019-10-20 09:12:50 +0800

    Update binary tree coloring

---
+++ src/binary-tree-coloring-game.cc
@@ -0,0 +1,29 @@
+// http://leetcode.com/problems/binary-tree-coloring-game/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  bool btreeGameWinningMove(TreeNode *root, int n, int x) {
+    countNode(root, x);
+    return lcount_ > n / 2 || rcount_ > n / 2 || lcount_ + rcount_ < n / 2;
+  }
+  int countNode(TreeNode *root, int x) {
+    if (!root) {
+      return 0;
+    }
+
+    auto l = countNode(root->left, x);
+    auto r = countNode(root->right, x);
+
+    if (root->val == x) {
+      lcount_ = l;
+      rcount_ = r;
+    }
+
+    return 1 + l + r;
+  }
+
+private:
+  int lcount_;
+  int rcount_;
+};
