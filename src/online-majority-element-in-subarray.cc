8adcd2bb5811ddbaa100d164d8885673755fc2b7
rongyi <sqrongyi@163.com>
2019-10-22 07:56:08 +0800

    Update leetcode

---
+++ src/online-majority-element-in-subarray.cc
@@ -0,0 +1,36 @@
+// http://leetcode.com/problems/online-majority-element-in-subarray/description/
+#include "xxx.h"
+
+class MajorityChecker {
+public:
+  MajorityChecker(vector<int> &arr) {
+    for (int i = 0; i < arr.size(); ++i) {
+      index_[arr[i]].push_back(i);
+    }
+  }
+
+  int query(int left, int right, int threshold) {
+    // NOTE!! without reference iteration will get
+    // MLE
+    for (auto &kv : index_) {
+      if (kv.second.size() < threshold) {
+        continue;
+      }
+      auto it1 = lower_bound(begin(kv.second), end(kv.second), left);
+      auto it2 = upper_bound(begin(kv.second), end(kv.second), right);
+      if (it2 - it1 >= threshold) {
+        return kv.first;
+      }
+    }
+    return -1;
+  }
+
+public:
+  unordered_map<int, vector<int>> index_;
+};
+
+/**
+ * Your MajorityChecker object will be instantiated and called as such:
+ * MajorityChecker* obj = new MajorityChecker(arr);
+ * int param_1 = obj->query(left,right,threshold);
+ */
