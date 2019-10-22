6b8687120f36d39d4ee2f158d0b19fac429f06f4
rongyi <sqrongyi@163.com>
2019-10-22 08:16:51 +0800

    Update leetcode

---
+++ src/find-words-that-can-be-formed-by-characters.cc
@@ -0,0 +1,32 @@
+// http://leetcode.com/problems/find-words-that-can-be-formed-by-characters/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int countCharacters(vector<string> &words, string chars) {
+    int ret = 0;
+    unordered_map<char, int> count;
+    for (auto &c : chars) {
+      ++count[c];
+    }
+    for (auto &w : words) {
+      unordered_map<char, int> cur;
+      for (auto &c : w) {
+        ++cur[c];
+      }
+      bool can{true};
+      for (auto &curkv : cur) {
+        if (count.find(curkv.first) == count.end() ||
+            curkv.second > count[curkv.first]) {
+          can = false;
+          break;
+        }
+      }
+      if (can) {
+        ret += w.size();
+      }
+    }
+
+    return ret;
+  }
+};
