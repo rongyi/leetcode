b8967a247b4b43367dbb08de42e5f450cb91d105
rongyi <sqrongyi@163.com>
2019-10-28 08:17:19 +0800

    Update leetcode

---
+++ src/last-substring-in-lexicographical-order.cc
@@ -0,0 +1,24 @@
+// http://leetcode.com/problems/last-substring-in-lexicographical-order/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  string lastSubstring(string s) {
+    int i = 0;
+    int sz = 0;
+    const int n = s.size();
+    for (int j = 1; j < n; ++j) {
+      for (sz = 0; j + sz < n; ++sz) {
+        if (s[i + sz] == s[j + sz]) {
+          continue;
+        }
+        i = s[j + sz] > s[i + sz] ? j : i;
+        break;
+      }
+      if (j + sz == n) {
+        break;
+      }
+    }
+    return s.substr(i);
+  }
+};
