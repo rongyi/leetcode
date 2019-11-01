ed3ed3dde88383b6277ea2c192cfcd5be4938191
rongyi <sqrongyi@163.com>
2019-11-01 19:09:23 +0800

    Update leetcode

---
+++ src/compare-strings-by-frequency-of-the-smallest-character.cc
@@ -0,0 +1,36 @@
+// http://leetcode.com/problems/compare-strings-by-frequency-of-the-smallest-character/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  // 又被喷的很惨
+  vector<int> numSmallerByFrequency(vector<string> &queries,
+                                    vector<string> &words) {
+    vector<int> qcount;
+    vector<int> wcount;
+    for (auto &q : queries) {
+      qcount.push_back(count(q));
+    }
+    for (auto &w : words) {
+      wcount.push_back(count(w));
+    }
+    sort(wcount.begin(), wcount.end());
+    vector<int> ret;
+    for (int i = 0; i < qcount.size(); ++i) {
+      auto u = upper_bound(wcount.begin(), wcount.end(), qcount[i]);
+      ret.push_back(wcount.end() - u);
+    }
+
+    return ret;
+  }
+  int count(string &s) {
+    auto c = *min_element(s.begin(), s.end());
+    return count_if(s.begin(), s.end(), [&c](char cur) { return cur == c; });
+  }
+};
+
+int main() {
+  Solution so;
+  string input{"dcce"};
+  cout << so.count(input) << endl;
+}
