d9e6affec0be837deac17895e0810993c5c8bac5
rongyi <sqrongyi@163.com>
2019-11-01 19:57:04 +0800

    Update leetcode

---
+++ src/remove-zero-sum-consecutive-nodes-from-linked-list.cc
@@ -0,0 +1,39 @@
+// http://leetcode.com/problems/remove-zero-sum-consecutive-nodes-from-linked-list/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  ListNode *removeZeroSumSublists(ListNode *head) {
+    ListNode dummy(0);
+    dummy.next = head;
+
+    unordered_map<int, ListNode *> same;
+    same[0] = &dummy;
+    int acc = 0;
+    while (head) {
+      acc += head->val;
+      if (same.find(acc) != same.end()) {
+        auto *prev = same[acc];
+        auto *start = prev;
+
+        int tmp = acc;
+        while (prev != head) {
+          prev = prev->next;
+          tmp += prev->val;
+          // the special case of consecutive zero
+          // is also considered
+          if (prev != head) {
+            same.erase(tmp);
+          }
+        }
+        start->next = head->next;
+      } else {
+        same[acc] = head;
+      }
+
+      head = head->next;
+    }
+
+    return dummy.next;
+  }
+};
