386139e3399be9568a2764cb94a0ea263ed4ef72
rongyi <sqrongyi@163.com>
2019-11-09 17:11:08 +0800

    Update leetcode

--- src/sort-items-by-groups-respecting-dependencies.cc
+++ src/sort-items-by-groups-respecting-dependencies.cc
@@ -3,28 +3,33 @@
 
 class Solution {
 public:
-  using graph = unordered_map<int, vector<int>>;
   vector<int> sortItems(int n, int m, vector<int> &group,
-                        vector<vector<int>> &beforeItems) {
-    // group之间的依赖
-    graph inter;
-    vector<graph> inner(m);
+                        vector<vector<int>> &before_items) {
+    // group之间的依赖, key是group number，value也是group number序列
+    unordered_map<int, vector<int>> inter;
+    // 每一个group内部之间元素的依赖，map中的key是node编号，value是node编号序列
+    vector<unordered_map<int, vector<int>>> inner_of_group(m);
     // 没有group的那些也给他分一个独立的group
     for (int i = 0; i < group.size(); ++i) {
       if (group[i] == -1) {
         group[i] = m++;
-        inner.resize(m);
+        inner_of_group.resize(m);
       }
-      inter[group[i]];
-      inner[group[i]][i];
+      // 先有group[i],依赖谁咱先不说
+      inter[group[i]] = {};
+      // 在group[i]中有i这个node，依赖谁咱先不说
+      inner_of_group[group[i]][i] = {};
     }
-    // group内部的依赖
 
     // 第i个节点前面必须要存在bi节点
+    // 以树表示就是 bi --> i 有一条边
+    // dfs recursive visit will first visit i
+    // then bi, the order is recorded in group_order
+    // and inner_order
     for (int i = 0; i < n; ++i) {
-      for (auto const &bi : beforeItems[i]) {
+      for (auto const &bi : before_items[i]) {
         if (group[i] == group[bi]) {
-          inner[group[bi]][bi].push_back(i);
+          inner_of_group[group[bi]][bi].push_back(i);
         } else {
           inter[group[bi]].push_back(group[i]);
         }
@@ -39,7 +44,7 @@ public:
 
     vector<list<int>> inner_order(m);
     for (int i = 0; i < m; ++i) {
-      if (!topo_sort(inner[i], inner_order[i])) {
+      if (!topo_sort(inner_of_group[i], inner_order[i])) {
         return {};
       }
     }
@@ -53,7 +58,7 @@ public:
   }
 
 private:
-  bool topo_sort(graph &g, list<int> &topo) {
+  bool topo_sort(unordered_map<int, vector<int>> &g, list<int> &topo) {
     unordered_map<int, int> visited;
     for (const auto &kv : g) {
       if (!dfs(kv.first, g, topo, visited)) {
@@ -62,7 +67,8 @@ private:
     }
     return true;
   }
-  bool dfs(int i, graph &g, list<int> &topo, unordered_map<int, int> &visited) {
+  bool dfs(int i, unordered_map<int, vector<int>> &g, list<int> &topo,
+           unordered_map<int, int> &visited) {
     if (visited[i] == -1) {
       return false;
     }
@@ -74,9 +80,19 @@ private:
         }
       }
       visited[i] = 1;
+      // 依赖的节点都在后面，所以，后面的节点应该是先
+      // 这里有一张图片简单明了
+      // https://leetcode.com/problems/sort-items-by-groups-respecting-dependencies/discuss/402945/C%2B%2B-with-picture-generic-topological-sort
       topo.push_front(i);
     }
 
     return true;
   }
 };
+
+int main() {
+  Solution so;
+  vector<int> group{-1, -1, 1, 0, 0, 1, 0, -1};
+  vector<vector<int>> bi{{}, {6}, {5}, {6}, {3, 6}, {}, {}, {}};
+  so.sortItems(8, 2, group, bi);
+}
