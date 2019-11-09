3d2eaf8d33ea371502bf6a1b7d9abfbbbcf9b0a2
rongyi <sqrongyi@163.com>
2019-11-09 10:06:49 +0800

    Update leetcode

--- src/smallest-string-with-swaps.cc
+++ src/smallest-string-with-swaps.cc
@@ -3,8 +3,11 @@
 
 class Solution {
 public:
+  // https://leetcode.com/problems/smallest-string-with-swaps/discuss/388257/C%2B%2B-with-picture-union-find
   string smallestStringWithSwaps(string s, vector<vector<int>> &pairs) {
+    // disjoint set
     vector<int> ds(s.size(), -1);
+    // group find set together
     vector<vector<int>> group(s.size());
 
     for (auto &p : pairs) {
@@ -19,13 +22,14 @@ public:
       group[find(ds, i)].push_back(i);
     }
     for (auto cur_group : group) {
-      string g{};
+      ostringstream ss;
       for (auto id : cur_group) {
-        g += s[id];
+        ss << s[id];
       }
-      sort(g.begin(), g.end());
+      string tmp = ss.str();
+      sort(tmp.begin(), tmp.end());
       for (auto i = 0; i < cur_group.size(); ++i) {
-        s[cur_group[i]] = g[i];
+        s[cur_group[i]] = tmp[i];
       }
     }
     return s;
@@ -37,7 +41,7 @@ private:
     if (ds[i] < 0) {
       return i;
     }
-    // cache
+    // cache, every element will know its group
     ds[i] = find(ds, ds[i]);
     return ds[i];
   }
