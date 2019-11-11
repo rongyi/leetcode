f825331fea843b9b3d9ac5cf5c7b61af89506328
rongyi <sqrongyi@163.com>
2019-11-12 07:52:08 +0800

    Update leetcode

---
+++ src/get-equal-substrings-within-budget.cc
@@ -0,0 +1,20 @@
+// http://leetcode.com/problems/get-equal-substrings-within-budget/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int equalSubstring(string s, string t, int maxCost) {
+    const int n = s.size();
+    int i = 0;
+    int j = 0;
+    while (i < n) {
+      maxCost -= abs(s[i] - t[i]);
+      i++;
+      if (maxCost < 0) {
+        maxCost += abs(s[j] - t[j]);
+        j++;
+      }
+    }
+    return i - j;
+  }
+};
---
+++ src/remove-all-adjacent-duplicates-in-string-ii.cc
@@ -0,0 +1,38 @@
+// http://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  string removeDuplicates(string s, int k) {
+    vector<char> stk;
+    for (auto &c : s) {
+      if (stk.empty() || stk.size() < k - 1) {
+        stk.push_back(c);
+      } else {
+        bool same = true;
+        int i = 0;
+        int j = stk.size() - 1;
+        while (i < k - 1) {
+          if (stk[j] != c) {
+            same = false;
+            break;
+          }
+          j--;
+          i++;
+        }
+        if (same) {
+          for (int i = 0; i < k - 1; ++i) {
+            stk.pop_back();
+          }
+        } else {
+          stk.push_back(c);
+        }
+      }
+    }
+    string ret;
+    for (auto &c : stk) {
+      ret.push_back(c);
+    }
+    return ret;
+  }
+};
