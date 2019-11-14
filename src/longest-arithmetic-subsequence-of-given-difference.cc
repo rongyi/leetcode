8966fa49e2488013624facfbe636297117ad18d8
rongyi <sqrongyi@163.com>
2019-11-14 08:03:00 +0800

    Update leetcode

--- src/longest-arithmetic-subsequence-of-given-difference.cc
+++ src/longest-arithmetic-subsequence-of-given-difference.cc
@@ -7,6 +7,7 @@ public:
     unordered_map<int, int> count;
     int ret = 0;
     for (int i = 0; i < arr.size(); ++i) {
+      // 前面有就串起来
       count[arr[i]] = 1 + count[arr[i] - difference];
       ret = max(ret, count[arr[i]]);
     }
