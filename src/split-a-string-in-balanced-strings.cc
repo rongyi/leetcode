cebf3bf68448e962fca3d490bc348481c1209875
rongyi <sqrongyi@163.com>
2019-11-18 08:16:16 +0800

    Update leetcode

---
+++ src/split-a-string-in-balanced-strings.cc
@@ -0,0 +1,24 @@
+// http://leetcode.com/problems/split-a-string-in-balanced-strings/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int balancedStringSplit(string s) {
+    int count[2] = {};
+    int ret = 0;
+    for (auto &c : s) {
+      if (c == 'L') {
+        ++count[0];
+      } else {
+        ++count[1];
+      }
+      if (count[0] == count[1]) {
+        ++ret;
+        count[0] = 0;
+        count[1] = 0;
+      }
+    }
+
+    return ret;
+  }
+};
