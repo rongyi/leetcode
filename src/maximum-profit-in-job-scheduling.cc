2c058c5ead9e87823d2b5df707bb95236286c093
rongyi <sqrongyi@163.com>
2019-11-23 19:02:48 +0800

    Update leetcode

--- src/maximum-profit-in-job-scheduling.cc
+++ src/maximum-profit-in-job-scheduling.cc
@@ -10,6 +10,7 @@ public:
     map<int, int> times;
     unordered_map<int, vector<pair<int, int>>> jobs;
     for (auto i = 0; i < startTime.size(); ++i) {
+      // 按startTime排序
       times[startTime[i]] = 0;
       jobs[startTime[i]].push_back({endTime[i], profit[i]});
     }
@@ -17,8 +18,10 @@ public:
     int ret = 0;
     for (auto it = times.rbegin(); it != times.rend(); ++it) {
       for (auto job : jobs[it->first]) {
+        // 出彩之处在于这里，意思就是能续上，lower_bound本身就是>=
+        // 也即不搭界，时间没有重叠 那么不搭界的话当然可以续上，这样一直加上来
         auto it = times.lower_bound(job.first);
-        ret = max(ret, (it == end(times) ? 0 : it->second) + job.second);
+        ret = max(ret, (it == times.end() ? 0 : it->second) + job.second);
       }
       it->second = ret;
     }
@@ -26,3 +29,11 @@ public:
     return ret;
   }
 };
+
+int main() {
+  Solution so;
+  vector<int> start{1, 2, 3, 4, 6};
+  vector<int> end{3, 5, 10, 6, 9};
+  vector<int> profit{20, 20, 100, 70, 60};
+  so.jobScheduling(start, end, profit);
+}
