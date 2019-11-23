90b8da31cd09da6fcad561e43b7dcfc5dc7e6cef
rongyi <sqrongyi@163.com>
2019-11-23 09:11:30 +0800

    Update leetcode

---
+++ src/the-dining-philosophers.cc
@@ -0,0 +1,30 @@
+// http://leetcode.com/problems/the-dining-philosophers/description/
+#include "xxx.h"
+
+class DiningPhilosophers {
+public:
+  DiningPhilosophers() {}
+
+  void wantsToEat(int philosopher, function<void()> pickLeftFork,
+                  function<void()> pickRightFork, function<void()> eat,
+                  function<void()> putLeftFork, function<void()> putRightFork) {
+    // clock wise order
+    int left = philosopher;
+    // so the right fork is index - 1;
+    int right = (philosopher + 4 /* -1 + 5 */) % 5;
+    bool even = philosopher % 2 == 0;
+
+    int first = even ? left : right;
+    int second = left + right - first;
+
+    unique_lock<mutex> lock1(m_[first]);
+    unique_lock<mutex> lock2(m_[second]);
+
+    pickLeftFork();
+    pickRightFork();
+    eat();
+    putLeftFork();
+    putRightFork();
+  }
+  mutex m_[5];
+};
