bd65278edeec049ba2c9c5ab88e08489b7aa3c04
rongyi <sqrongyi@163.com>
2019-11-23 10:50:40 +0800

    Update leetcode

--- src/remove-sub-folders-from-the-filesystem.cc
+++ src/remove-sub-folders-from-the-filesystem.cc
@@ -4,16 +4,20 @@
 class Solution {
 public:
   vector<string> removeSubfolders(vector<string> &folder) {
+    // 其实就是为了防止短的文件夹在后面的情况出现
+    // sort完以后，短的都在前面了
     sort(folder.begin(), folder.end());
     auto last = folder[0];
     vector<string> ret;
     ret.push_back(last);
+
     for (int i = 1; i < folder.size(); ++i) {
       auto cur = folder[i];
       if (cur.size() > last.size() && cur[last.size()] == '/' &&
           cur.substr(0, last.size()) == last) {
         continue;
       }
+
       last = cur;
       ret.push_back(cur);
     }
