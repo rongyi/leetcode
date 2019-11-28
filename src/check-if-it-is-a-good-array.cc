4d480d0706d0c93cd92db07676417b3c3b87c5b0
rongyi <sqrongyi@163.com>
2019-11-28 08:01:33 +0800

    Update leetcode

---
+++ src/check-if-it-is-a-good-array.cc
@@ -0,0 +1,15 @@
+// http://leetcode.com/problems/check-if-it-is-a-good-array/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  bool isGoodArray(vector<int> &nums) {
+    auto gcd = nums[0];
+
+    for (auto n : nums) {
+      gcd = __gcd(gcd, n);
+    }
+
+    return gcd == 1;
+  }
+};
