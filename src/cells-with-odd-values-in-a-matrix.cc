29b5ab13b795614f2c21faefc9fb547ab7095ff2
rongyi <sqrongyi@163.com>
2019-11-28 08:22:21 +0800

    Update leetcode

---
+++ src/cells-with-odd-values-in-a-matrix.cc
@@ -0,0 +1,24 @@
+// http://leetcode.com/problems/cells-with-odd-values-in-a-matrix/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int oddCells(int m, int n, vector<vector<int>> &indices) {
+    vector<int> row(m, 0);
+    vector<int> col(n, 0);
+    for (auto &p : indices) {
+      ++row[p[0]];
+      ++col[p[1]];
+    }
+    int ret = 0;
+
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if ((row[i] + col[j]) & 1) {
+          ++ret;
+        }
+      }
+    }
+    return ret;
+  }
+};
