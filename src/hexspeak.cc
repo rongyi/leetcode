9429b0204d49cfc566873ab3bf36bf3566d78c04
rongyi <sqrongyi@163.com>
2019-12-01 11:37:45 +0800

    Update leetcode

---
+++ src/hexspeak.cc
@@ -0,0 +1,27 @@
+// http://leetcode.com/problems/hexspeak/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  string toHexspeak(string num) {
+    stringstream ss;
+    ss << hex << stoll(num);
+    string hex = ss.str();
+    unordered_set<char> valid{'A', 'B', 'C', 'D', 'E', 'F', 'I', 'O'};
+
+    for (auto &c : hex) {
+      c = toupper(c);
+      if (c == '1') {
+        c = 'I';
+      }
+      if (c == '0') {
+        c = 'O';
+      }
+      if (valid.find(c) == valid.end()) {
+        return "ERROR";
+      }
+    }
+    return hex;
+  }
+};
+int main() { Solution so; }
