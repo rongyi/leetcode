7f6ce9941e5a2f3d8ecbdd4f88b821407d22b1c0
rongyi <sqrongyi@163.com>
2019-12-01 10:24:14 +0800

    Update leetcode

--- src/minimum-time-visiting-all-points.cc
+++ src/minimum-time-visiting-all-points.cc
@@ -7,6 +7,7 @@ public:
     const int n = points.size();
     int ret = 0;
     for (int i = 1; i < n; ++i) {
+      //真是不可思议，竟然就是这个答案
       ret += max(abs(points[i][0] - points[i - 1][0]),
                  abs(points[i][1] - points[i - 1][1]));
     }
