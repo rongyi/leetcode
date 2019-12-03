8edc36688f0fb774e3b72738f15279257eb3c0fd
rongyi <sqrongyi@163.com>
2019-12-03 08:44:41 +0800

    Update leetcode

---
+++ src/number-of-burgers-with-no-waste-of-ingredients.cc
@@ -0,0 +1,21 @@
+// http://leetcode.com/problems/number-of-burgers-with-no-waste-of-ingredients/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<int> numOfBurgers(int tomatoSlices, int cheeseSlices) {
+    // tomatoSlices will always be even number
+    // cheeseSlices will never > tomatoSlices / 2
+    if (tomatoSlices % 2 || cheeseSlices > tomatoSlices / 2) {
+      return {};
+    }
+    // 2x + 4y = tomatoSlices
+    // x + y = cheeseSlices
+    auto x = 2 * cheeseSlices - tomatoSlices / 2;
+    auto y = (tomatoSlices / 2) - cheeseSlices;
+    if (x < 0 || y < 0) {
+      return {};
+    }
+    return {y, x};
+  }
+};
