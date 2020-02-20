fa7f7cf7e098c2243af5713a90126ccda1763295
rongyi <sqrongyi@163.com>
2020-02-21 07:04:02 +0800

    Update leetcode

---
+++ src/minimum-falling-path-sum-ii.cc
@@ -0,0 +1,28 @@
+// http://leetcode.com/problems/minimum-falling-path-sum-ii/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int minFallingPathSum(vector<vector<int>> &arr) {
+    const int n = arr.size();
+    for (int r = n - 2; r >= 0; r--) {
+      for (int j = 0; j < n; j++) {
+        int m = numeric_limits<int>::max();
+        for (int k = 0; k < j; k++) {
+          m = min(m, arr[r + 1][k]);
+        }
+        for (int k = j + 1; k < n; k++) {
+          m = min(m, arr[r + 1][k]);
+        }
+        arr[r][j] += m;
+      }
+    }
+    int ret = numeric_limits<int>::max();
+
+    for (int j = 0; j < n; j++) {
+      ret = min(ret, arr[0][j]);
+    }
+
+    return ret;
+  }
+};
