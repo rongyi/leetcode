0b8cc5175f4b952a29583fd66be898d1f9e1bc40
rongyi <sqrongyi@163.com>
2020-02-20 20:44:28 +0800

    Update leetcode

---
+++ src/element-appearing-more-than-25-in-sorted-array.cc
@@ -0,0 +1,18 @@
+// http://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int findSpecialInteger(vector<int> &arr) {
+    const int total = arr.size();
+    unordered_map<int, int> count;
+    for (auto &num : arr) {
+      ++count[num];
+      if (count[num] * 1.0 / total > 0.25) {
+        return num;
+      }
+    }
+    // make compiler shutup
+    return -1;
+  }
+};
