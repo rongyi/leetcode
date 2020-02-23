ccf627b4e290146bc3318a8a7c9f9ce2913254f0
rongyi <sqrongyi@163.com>
2020-02-23 10:09:20 +0800

    Update leetcode

---
+++ src/replace-elements-with-greatest-element-on-right-side.cc
@@ -0,0 +1,19 @@
+// http://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<int> replaceElements(vector<int> &arr) {
+    const int n = arr.size();
+    int cur_max = arr[n - 1];
+
+    arr[n - 1] = -1;
+    for (int i = n - 2; i >= 0; i--) {
+      auto tmp = arr[i];
+      arr[i] = cur_max;
+      cur_max = max(cur_max, tmp);
+    }
+
+    return arr;
+  }
+};
