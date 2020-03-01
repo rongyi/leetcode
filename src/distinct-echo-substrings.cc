5870fdeddbc2ed7426d7e0eb0beb545b85d8e680
rongyi <sqrongyi@163.com>
2020-03-01 09:32:48 +0800

    Update leetcode

---
+++ src/distinct-echo-substrings.cc
@@ -0,0 +1,31 @@
+// http://leetcode.com/problems/distinct-echo-substrings/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  // substring can be split into two identical part
+  int distinctEchoSubstrings(string text) {
+    unordered_set<string> ret;
+    // TLS version
+    // for (int i = 0; i < text.size(); i++) {
+    //   for (int len = 1; i + len + len <= text.size(); ++len) {
+    //     const auto a = text.substr(i, len);
+    //     const auto b = text.substr(i + len, len);
+    //     if (a == b) {
+    //       ret.insert(a);
+    //     }
+    //   }
+    // }
+
+    // split point
+    for (auto split = 1; split < text.size() - 1; ++split) {
+      for (auto sz = 1; split - sz >= 0 && split + sz - 1 < text.size(); ++sz) {
+        if (text.compare(split - sz, sz, text, split, sz) == 0) {
+          ret.insert(text.substr(split, sz));
+        }
+      }
+    }
+
+    return ret.size();
+  }
+};
