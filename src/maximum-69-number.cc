f086134670ad056612c7a43fe61dfe7c45062e55
rongyi <sqrongyi@163.com>
2020-03-03 09:15:52 +0800

    Update worklog

--- src/maximum-69-number.cc
+++ src/maximum-69-number.cc
@@ -5,6 +5,9 @@ class Solution {
 public:
   int maximum69Number(int num) {
     auto s = to_string(num);
+
+    // find the first '6' from left to right and change
+    // it to '9'
     int six_index = -1;
     for (int i = 0; i < s.size(); i++) {
       if (s[i] == '6') {
