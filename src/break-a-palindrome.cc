4cbc15f9553f0deaeeeda0fc7d8d3e4d11141d7f
rongyi <sqrongyi@163.com>
2020-03-07 10:28:25 +0800

    Update break parlindrom

---
+++ src/break-a-palindrome.cc
@@ -0,0 +1,32 @@
+// http://leetcode.com/problems/break-a-palindrome/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  string breakPalindrome(string ps) {
+    const int n = ps.size();
+    // one char is palindrome forever
+    if (n == 1) {
+      return "";
+    }
+    bool is_odd = (n & 1);
+    int i = 0;
+
+    for (; i < ps.size(); i++) {
+      if (ps[i] != 'a') {
+        // ignore center not 'a'
+        if (is_odd && i == n / 2) {
+          continue;
+        }
+        ps[i] = 'a';
+        break;
+      }
+    }
+
+    // means every char is 'a', change the last one to b
+    if (i == ps.size()) {
+      ps[ps.size() - 1] = 'b';
+    }
+    return ps;
+  }
+};
