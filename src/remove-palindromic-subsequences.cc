a586b02f744ab92b38bb6affd7ebc36e0dc36eb1
rongyi <sqrongyi@163.com>
2020-03-08 12:55:19 +0800

    Update leetcode

---
+++ src/remove-palindromic-subsequences.cc
@@ -0,0 +1,19 @@
+// http://leetcode.com/problems/remove-palindromic-subsequences/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  // 喷的特别惨
+  int removePalindromeSub(string s) {
+    if (s.empty()) {
+      return 0;
+    }
+    if (s == string(s.rbegin(), s.rend())) {
+      return 1;
+    }
+    // string containes only two letters 'a' and 'b'
+    // we can first remote 'a' and then remote 'b'
+    // to let the string empty
+    return 2;
+  }
+};
