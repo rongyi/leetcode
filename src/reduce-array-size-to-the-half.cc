ad260b8142b172d436fb7596ba8a88a3c2016f3d
rongyi <sqrongyi@163.com>
2020-06-07 11:39:37 +0800

    Update leetcode

---
+++ src/reduce-array-size-to-the-half.cc
@@ -0,0 +1,29 @@
+// http://leetcode.com/problems/reduce-array-size-to-the-half/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int minSetSize(vector<int> &arr) {
+    const int n = arr.size();
+    unordered_map<int, int> count;
+    set<pair<int, int>> s;
+    for (auto &num : arr) {
+      count[num]++;
+    }
+    for (auto kv : count) {
+      s.insert({kv.second, kv.first});
+    }
+    int left = n;
+    int ret = 0;
+    for (auto it = s.rbegin(); it != s.rend(); it++) {
+      left -= it->first;
+      ret++;
+
+      if (left <= n / 2) {
+        return ret;
+      }
+    }
+
+    return ret;
+  }
+};
