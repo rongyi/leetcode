36e1335874b411d10f5d60ad24e9aa72c73a60e1
rongyi <sqrongyi@163.com>
2020-06-09 07:01:22 +0800

    Update leetcode

---
+++ src/number-of-steps-to-reduce-a-number-to-zero.cc
@@ -0,0 +1,19 @@
+// http://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int numberOfSteps(int num) {
+    int ret = 0;
+    while (num) {
+      ++ret;
+      if (num & 1) {
+        num--;
+      } else {
+        num >>= 1;
+      }
+    }
+
+    return ret;
+  }
+};
