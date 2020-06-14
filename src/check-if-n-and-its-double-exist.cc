0207a89a8a12e84ab054edbc8891d2ced587f75a
rongyi <sqrongyi@163.com>
2020-06-14 11:37:33 +0800

    Update leetcode

---
+++ src/check-if-n-and-its-double-exist.cc
@@ -0,0 +1,17 @@
+// http://leetcode.com/problems/check-if-n-and-its-double-exist/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  bool checkIfExist(vector<int> &arr) {
+    unordered_set<double> uniq;
+    for (int i = 0; i < arr.size(); i++) {
+      if (uniq.find(arr[i] / 2.0) != uniq.end() ||
+          uniq.find(arr[i] * 2.0) != uniq.end()) {
+        return true;
+      }
+      uniq.insert(arr[i]);
+    }
+    return false;
+  }
+};
