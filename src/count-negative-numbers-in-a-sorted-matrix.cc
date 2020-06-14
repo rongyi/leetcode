f2a0aba1ac9c3153c0ab356b3638da3e70a86dbc
rongyi <sqrongyi@163.com>
2020-06-15 07:07:45 +0800

    Add countNegatives

---
+++ src/count-negative-numbers-in-a-sorted-matrix.cc
@@ -0,0 +1,23 @@
+// http://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int countNegatives(vector<vector<int>> &grid) {
+    const int m = grid.size();
+    const int n = grid[0].size();
+    int ret = 0;
+    int i = m - 1;
+    int j = 0;
+    while (i >= 0 && j < n) {
+      if (grid[i][j] < 0) {
+        --i;
+        ret += n - j;
+      } else {
+        ++j;
+      }
+    }
+
+    return ret;
+  }
+};
