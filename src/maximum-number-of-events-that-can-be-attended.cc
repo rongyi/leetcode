7c92829e93f8b8ac946eb51b3d8805e5c6031688
rongyi <sqrongyi@163.com>
2020-06-15 08:21:24 +0800

    Update maxEvents

--- src/maximum-number-of-events-that-can-be-attended.cc
+++ src/maximum-number-of-events-that-can-be-attended.cc
@@ -11,22 +11,18 @@ public:
     int d = 0;
     const int n = events.size();
 
-    // d标记的今天
-    // pq里存的是结束的时间
     while (pq.size() > 0 || i < n) {
       if (pq.size() == 0) {
+        // 下一轮的第一个元素
         d = events[i][0];
       }
-      // 只要开始时间不超过今天，那就由机会去参加
       while (i < n && events[i][0] <= d) {
         pq.push(events[i++][1]);
       }
-      // 参加的那个是结束时间最早的那个
       pq.pop();
-      ++ret;
-      ++d;
-      // 如果结束时间已经比d小那么明天就没有机会去参加了
-      while (pq.size() > 0 && pq.top() < d) {
+      d++;
+      ret++;
+      while (!pq.empty() && pq.top() < d) {
         pq.pop();
       }
     }
