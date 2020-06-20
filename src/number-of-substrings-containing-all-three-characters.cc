a6d8eed113a8ca0614b2cd3b2b00e05115de221b
rongyi <sqrongyi@163.com>
2020-06-20 11:07:05 +0800

    Update leetcode

---
+++ src/number-of-substrings-containing-all-three-characters.cc
@@ -0,0 +1,24 @@
+// http://leetcode.com/problems/number-of-substrings-containing-all-three-characters/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int numberOfSubstrings(string s) {
+    int count[3] = {0, 0, 0};
+    int i = 0;
+    int ret = 0;
+    // aaaaaabc
+    // i      j
+    //       i
+    // count += i
+    for (int j = 0; j < s.size(); j++) {
+      ++count[s[j] - 'a'];
+      while (count[0] && count[1] && count[2]) {
+        --count[s[i++] - 'a'];
+      }
+      ret += i;
+    }
+
+    return ret;
+  }
+};
