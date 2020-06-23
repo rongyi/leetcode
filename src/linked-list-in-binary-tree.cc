afb4ceb98eddb167bb1f0948148f5cbb416d65d9
rongyi <sqrongyi@163.com>
2020-06-23 08:57:50 +0800

    Update leetcode

--- src/linked-list-in-binary-tree.cc
+++ src/linked-list-in-binary-tree.cc
@@ -11,6 +11,9 @@ public:
     return check(head, root) || isSubPath(head, root->left) ||
            isSubPath(head, root->right);
   }
+
+private:
+  // 分清楚递归的层次，从一个节点开始之后的比较是严格比较，一旦比较失败那么追溯到上面就是那个顶点开始不在上面
   bool check(ListNode *head, TreeNode *root) {
     if (!head) {
       return true;
