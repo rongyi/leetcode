3ab88fbd38a9202ca6615954d41ae7a8cd9943a2
rongyi <sqrongyi@163.com>
2020-06-23 08:22:32 +0800

    Update leetcode

---
+++ src/rank-teams-by-votes.cc
@@ -0,0 +1,33 @@
+// http://leetcode.com/problems/rank-teams-by-votes/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  string rankTeams(vector<string> &votes) {
+    string ret{votes[0]};
+    const int n = votes[0].size();
+    // 每一个字符对应每一列位置上的统计次数
+    vector<vector<int>> rank(26, vector<int>(n, 0));
+    for (auto &vote : votes) {
+      for (int j = 0; j < n; j++) {
+        rank[vote[j] - 'A'][j]++;
+      }
+    }
+
+    sort(ret.begin(), ret.end(), [&](auto &l, auto &r) -> bool {
+      // 如果这一列相等就比较下一列，这是vector的默认比较函数
+      return rank[l - 'A'] > rank[r - 'A'] ||
+             (rank[l - 'A'] == rank[r - 'A'] && l < r);
+    });
+
+    return ret;
+  }
+};
+
+int main() {
+  Solution so;
+  vector<string> input{"ABC", "ACB", "ABC", "ACB", "ACB"};
+
+  auto ret = so.rankTeams(input);
+  cout << ret << endl;
+}
