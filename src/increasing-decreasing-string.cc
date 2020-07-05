b176105bc9a87c29d4f2367f3a8db82ee1bfc6e5
rongyi <sqrongyi@163.com>
2020-07-05 08:37:42 +0800

    Update leetcode

---
+++ src/increasing-decreasing-string.cc
@@ -0,0 +1,33 @@
+// http://leetcode.com/problems/increasing-decreasing-string/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  string sortString(string s) {
+    int n = s.size();
+    string ret;
+    int count[26] = {};
+    for (auto &c : s) {
+      ++count[c - 'a'];
+    }
+
+    while (n) {
+      for (int i = 0; i < 26; i++) {
+        if (count[i]) {
+          ret.push_back(i + 'a');
+          --count[i];
+          --n;
+        }
+      }
+      for (int i = 25; i >= 0; i--) {
+        if (count[i]) {
+          ret.push_back(i + 'a');
+          --count[i];
+          --n;
+        }
+      }
+    }
+
+    return ret;
+  }
+};
