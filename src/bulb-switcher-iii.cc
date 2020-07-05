9301a4173cd6c27d544000b0add7c51deef18220
rongyi <sqrongyi@163.com>
2020-07-05 10:58:05 +0800

    Update leetcode

---
+++ src/bulb-switcher-iii.cc
@@ -0,0 +1,22 @@
+// http://leetcode.com/problems/bulb-switcher-iii/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int numTimesAllBlue(vector<int> &light) {
+    vector<int> prefix_sum(light.size() + 1, 0);
+    for (int i = 0; i < light.size(); ++i) {
+      prefix_sum[i + 1] = prefix_sum[i] + (i + 1);
+    }
+    int cur_sum = 0;
+    int ret = 0;
+
+    for (int i = 0; i < light.size(); ++i) {
+      cur_sum += light[i];
+      if (cur_sum == prefix_sum[i + 1]) {
+        ++ret;
+      }
+    }
+    return ret;
+  }
+};
