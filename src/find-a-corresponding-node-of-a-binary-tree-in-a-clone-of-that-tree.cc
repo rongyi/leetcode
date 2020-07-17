d2b3e086e6580c03391cabfed641781d08ccf615
rongyi <sqrongyi@163.com>
2020-07-18 07:13:22 +0800

    Update leetcode

---
+++ src/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cc
@@ -0,0 +1,30 @@
+// http://leetcode.com/problems/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  TreeNode *getTargetCopy(TreeNode *original, TreeNode *cloned,
+                          TreeNode *target) {
+    mirror_ = nullptr;
+
+    recur(original, cloned, target);
+
+    return mirror_;
+  }
+
+private:
+  void recur(TreeNode *o, TreeNode *c, TreeNode *target) {
+    if (!o) {
+      return;
+    }
+    if (target->val == o->val) {
+      mirror_ = c;
+      return;
+    }
+    recur(o->left, c->left, target);
+    recur(o->right, c->right, target);
+  }
+
+private:
+  TreeNode *mirror_;
+};
