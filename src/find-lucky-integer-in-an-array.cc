b76d2f251fb74e17b26574caccaf8228573d4f8e
rongyi <sqrongyi@163.com>
2020-07-26 07:44:05 +0800

    Add find lucky number

---
+++ src/find-lucky-integer-in-an-array.cc
@@ -0,0 +1,18 @@
+// http://leetcode.com/problems/find-lucky-integer-in-an-array/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int findLucky(vector<int> &arr) {
+    map<int, int> count;
+    for (auto &num : arr) {
+      ++count[num];
+    }
+    for (auto it = count.rbegin(); it != count.rend(); ++it) {
+      if (it->first == it->second) {
+        return it->first;
+      }
+    }
+    return -1;
+  }
+};
