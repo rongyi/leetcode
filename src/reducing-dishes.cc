250411a631102487c1a9e389a003382afbb9c1de
rongyi <sqrongyi@163.com>
2020-08-01 06:58:22 +0800

    Update leetcode

---
+++ src/reducing-dishes.cc
@@ -0,0 +1,19 @@
+// http://leetcode.com/problems/reducing-dishes/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int maxSatisfaction(vector<int> &satisfaction) {
+    sort(satisfaction.begin(), satisfaction.end());
+    int sum = 0;
+    int result = 0;
+
+    for (int i = satisfaction.size() - 1;
+         i >= 0 && satisfaction[i] + result > 0; --i) {
+      result += satisfaction[i];
+      sum += result;
+    }
+
+    return sum;
+  }
+};
