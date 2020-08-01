fe18245eb78268701d96bc6c6a5ea4c791bcd899
rongyi <sqrongyi@163.com>
2020-08-01 08:47:33 +0800

    Add numSteps

---
+++ src/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cc
@@ -0,0 +1,35 @@
+// http://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int numSteps(string s) {
+    int steps = 0;
+    while (s != "1") {
+      if (s.back() == '0') {
+        s.pop_back();
+      } else {
+        int carry = 1;
+        for (int i = s.size() - 1; i >= 0; i--) {
+          if (carry == 0) {
+            break;
+          }
+          auto sum = (s[i] - '0') + carry;
+          carry = sum / 2;
+          s[i] = sum % 2 + '0';
+        }
+        if (carry) {
+          s.insert(s.begin(), '1');
+        }
+      }
+
+      steps++;
+    }
+
+    return steps;
+  }
+};
+int main() {
+  Solution so;
+  so.numSteps("hello");
+}
