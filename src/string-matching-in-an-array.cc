dc055c5f802e06ac2cb345f18bef3ad21e23029d
rongyi <sqrongyi@163.com>
2020-08-06 08:35:44 +0800

    Add stringMatching

---
+++ src/string-matching-in-an-array.cc
@@ -0,0 +1,22 @@
+// http://leetcode.com/problems/string-matching-in-an-array/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<string> stringMatching(vector<string> &words) {
+    sort(words.begin(), words.end(),
+         [](auto &l, auto &r) -> bool { return l.size() < r.size(); });
+    vector<string> ret;
+    for (int i = 0; i < words.size(); ++i) {
+      auto &cur = words[i];
+      for (int j = i + 1; j < words.size(); ++j) {
+        if (words[j].find(cur) != string::npos) {
+          ret.push_back(cur);
+          break; // the inner loop
+        }
+      }
+    }
+
+    return ret;
+  }
+};
