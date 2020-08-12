3c99bfd928cdd45f7f8fc88073d5c74a0574b5f2
rongyi <sqrongyi@163.com>
2020-08-12 08:45:05 +0800

    Add kth lexicographical string

---
+++ src/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cc
@@ -0,0 +1,45 @@
+// http://leetcode.com/problems/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  string getHappyString(int n, int k) {
+    vector<string> ret;
+    queue<string> q;
+    q.push("a");
+    q.push("b");
+    q.push("c");
+    // what a wonderful loop
+    while (!q.empty()) {
+      auto x = q.front();
+      q.pop();
+      if (x.size() == n) {
+        ret.push_back(x);
+      }
+      string s1{};
+      string s2{};
+
+      if (x.back() == 'a') {
+        s1 = x + "b";
+        s2 = x + "c";
+      } else if (x.back() == 'b') {
+        s1 = x + "a";
+        s2 = x + "c";
+      } else {
+        s1 = x + "a";
+        s2 = x + "b";
+      }
+      if (s1.length() <= n) {
+        q.push(s1);
+      }
+      if (s2.length() <= n) {
+        q.push(s2);
+      }
+    }
+    if (k - 1 >= ret.size()) {
+      return "";
+    }
+
+    return ret[k - 1];
+  }
+};
