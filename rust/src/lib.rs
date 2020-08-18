b198860901bf64ae611985e2971e76ba0e6d4f2c
rongyi <sqrongyi@163.com>
2020-08-18 10:48:04 +0800

    Update leetcode

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -23,6 +23,33 @@ pub fn max_score(card_points: Vec<i32>, k: i32) -> i32 {
     ret
 }
 
+use std::collections::HashMap;
+pub fn find_diagonal_order(nums: Vec<Vec<i32>>) -> Vec<i32> {
+    let mut max_sum: i32 = 0;
+    let mut group: HashMap<i32, Vec<i32>> = HashMap::new();
+
+    let mut i: i32 = nums.len() as i32 - 1;
+    while i >= 0 {
+        for j in 0..nums[i as usize].len() as i32 {
+            group
+                .entry(i + j)
+                .or_insert(Vec::new())
+                .push(nums[i as usize][j as usize]);
+            max_sum = std::cmp::max(max_sum, i + j);
+        }
+        i -= 1;
+    }
+
+    let mut ret = Vec::new();
+    for i in  0..=max_sum {
+        for v in &group[i] {
+            ret.push(v);
+        }
+    }
+
+    ret
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
