176cf1464fe4c9d136957dbb81b20a27d88fcc96
rongyi <sqrongyi@163.com>
2020-08-22 16:07:46 +0800

    Update leetcode

---
+++ cc/check-if-a-string-can-break-another-string.cc
@@ -0,0 +1,36 @@
+// http://leetcode.com/problems/check-if-a-string-can-break-another-string/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  bool checkIfCanBreak(string s1, string s2) {
+    sort(s1.begin(), s1.end());
+    sort(s2.begin(), s2.end());
+    // 完全错开了
+    if (s1.front() >= s2.back() || s1.back() <= s2.front()) {
+      return true;
+    }
+    // 有交集，那要排次序
+    // relation unkown
+    int relation = 0;
+    for (int i = 0; i < s1.size(); ++i) {
+      if (relation == 0) {
+        if (s1[i] < s2[i]) {
+          relation = -1;
+        } else if (s1[i] > s2[i]) {
+          relation = 1;
+        }
+      } else {
+        // 确认已有的关系，不符合那就不行了，有cross的
+        if (relation == -1 && s1[i] > s2[i]) {
+          return false;
+        }
+        if (relation == 1 && s1[i] < s2[i]) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+};
