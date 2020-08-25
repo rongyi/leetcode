4689164eaf15c6cfc139b145c1a50fc858f51fed
rongyi <sqrongyi@163.com>
2020-08-25 09:11:57 +0800

    Update longestSubarray

--- cc/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cc
+++ cc/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cc
@@ -4,9 +4,11 @@
 class Solution {
 public:
   int longestSubarray(vector<int> &nums, int limit) {
+    // how to get max/min of a range? ==> multiset
     multiset<int> sorted;
     int j = 0;
     int ret = 1;
+
     for (int i = 0; i < nums.size(); ++i) {
       sorted.insert(nums[i]);
       while (*sorted.rbegin() - *sorted.begin() > limit) {
