53abc0dd5de501fca9a4869ff2ed68f18f30a11c
rongyi <sqrongyi@163.com>
2020-08-27 08:49:17 +0800

    Update count triplets

---
+++ cc/count-triplets-that-can-form-two-arrays-of-equal-xor.cc
@@ -0,0 +1,22 @@
+// http://leetcode.com/problems/count-triplets-that-can-form-two-arrays-of-equal-xor/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int countTriplets(vector<int> &arr) {
+    int ret = 0;
+    vector<int> prefix(arr.size() + 1, 0);
+    for (int i = 0; i < arr.size(); ++i) {
+      prefix[i + 1] = prefix[i] ^ arr[i];
+    }
+    for (int i = 0; i < prefix.size(); ++i) {
+      for (int j = i + 1; j < prefix.size(); ++j) {
+        if (prefix[i] == prefix[j]) {
+          ret += j - i - 1;
+        }
+      }
+    }
+
+    return ret;
+  }
+};
