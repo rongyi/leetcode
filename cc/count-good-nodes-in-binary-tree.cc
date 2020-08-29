f6a66529fb37d35429edf708c8fc3e3aebca27c7
rongyi <sqrongyi@163.com>
2020-08-29 11:10:28 +0800

    Update leetcode

---
+++ cc/count-good-nodes-in-binary-tree.cc
@@ -0,0 +1,26 @@
+// http://leetcode.com/problems/count-good-nodes-in-binary-tree/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int goodNodes(TreeNode *root) {
+    int ret = 0;
+
+    recur(root, numeric_limits<int>::min(), ret);
+
+    return ret;
+  }
+
+private:
+  void recur(TreeNode *root, int path_max, int &good_nums) {
+    if (!root) {
+      return;
+    }
+    if (path_max <= root->val) {
+      good_nums++;
+    }
+
+    recur(root->left, max(path_max, root->val), good_nums);
+    recur(root->right, max(path_max, root->val), good_nums);
+  }
+};
