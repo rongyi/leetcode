1e420e8ac258db6d72b8265bc964f1e34f415c0b
rongyi <sqrongyi@163.com>
2020-08-29 10:38:31 +0800

    Update simplified fractions

---
+++ cc/simplified-fractions.cc
@@ -0,0 +1,28 @@
+// http://leetcode.com/problems/simplified-fractions/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<string> simplifiedFractions(int n) {
+    vector<string> ret;
+    // i > 1
+    auto minform = [](int a, int b) -> bool {
+      for (int i = 2; i <= a; i++) {
+        if (a % i == 0 && b % i == 0) {
+          return false;
+        }
+      }
+      return true;
+    };
+
+    for (int i = 2; i <= n; ++i) {
+      for (int j = 1; j < i; ++j) {
+        if (j == 1 || minform(j, i)) {
+          ret.push_back(to_string(j) + "/" + to_string(i));
+        }
+      }
+    }
+
+    return ret;
+  }
+};
