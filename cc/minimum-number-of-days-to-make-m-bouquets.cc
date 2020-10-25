49547c91e167143fc2671b609b0b4ec34282e535
rongyi <rongyi@onchain.com>
2020-10-25 08:32:36 +0800

    Update leetcode
    add https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/

---
+++ cc/minimum-number-of-days-to-make-m-bouquets.cc
@@ -0,0 +1,40 @@
+// http://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int minDays(vector<int> &bloomDay, int m, int k) {
+    if (m * k > bloomDay.size()) {
+      return -1;
+    }
+
+    auto lo = 1;
+    int hi = pow(10, 9);
+    while (lo < hi) {
+      auto mid = (lo + hi) / 2;
+      if (canHavest(bloomDay, mid, m, k)) {
+        hi = mid;
+      } else {
+        lo = mid + 1;
+      }
+    }
+    return lo;
+  }
+
+private:
+  bool canHavest(vector<int> &bday, int curday, int m, int k) {
+    int count = 0;
+    for (auto &d : bday) {
+      if (d <= curday) {
+        count++;
+        if (count >= k) {
+          count = 0;
+          --m;
+        }
+      } else {
+        count = 0;
+      }
+    }
+    return m <= 0;
+  }
+};
