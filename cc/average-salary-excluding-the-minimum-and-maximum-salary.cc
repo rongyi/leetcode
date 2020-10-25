09747e2b1654b4417db4687531e93fca6df24020
rongyi <rongyi@onchain.com>
2020-10-26 07:03:49 +0800

    Update leetcode

---
+++ cc/average-salary-excluding-the-minimum-and-maximum-salary.cc
@@ -0,0 +1,20 @@
+// http://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  double average(vector<int> &salary) {
+    int min_slry = numeric_limits<int>::max();
+    int max_slry = numeric_limits<int>::min();
+    double sum = 0;
+    for (auto &s : salary) {
+      sum += s;
+      min_slry = min(s, min_slry);
+      max_slry = max(s, max_slry);
+    }
+    sum -= min_slry;
+    sum -= max_slry;
+
+    return sum / (salary.size() - 2);
+  }
+};
