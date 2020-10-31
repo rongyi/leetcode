50eeac5abbb057a0e92eca51e7b5a6cbacb0b8dc
rongyi <rongyi@onchain.com>
2020-10-31 08:27:41 +0800

    Update leetcode

---
+++ cc/path-crossing.cc
@@ -0,0 +1,35 @@
+// http://leetcode.com/problems/path-crossing/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  bool isPathCrossing(string path) {
+    set<pair<int, int>> cache;
+    int x = 0;
+    int y = 0;
+    cache.insert({x, y});
+    // 这是经过原点
+    for (auto &p : path) {
+      switch (p) {
+      case 'N':
+        y += 1;
+        break;
+      case 'S':
+        y -= 1;
+        break;
+      case 'W':
+        x -= 1;
+        break;
+      case 'E':
+        x += 1;
+        break;
+      }
+
+      if (cache.count({x, y})) {
+        return true;
+      }
+      cache.insert({x, y});
+    }
+    return false;
+  }
+};
