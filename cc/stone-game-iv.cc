d90109e6fbd07ef537130c75ecdcf3abdd78b7d8
rongyi <rongyi@onchain.com>
2020-11-08 08:38:06 +0800

    Update leetcode

---
+++ cc/stone-game-iv.cc
@@ -0,0 +1,21 @@
+// http://leetcode.com/problems/stone-game-iv/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  bool winnerSquareGame(int n) {
+    vector<bool> dp(n + 1, false);
+    // 枚举所有可能，然后看这一步走完对方怎么样？
+    for (int i = 1; i <= n; ++i) {
+      for (int j = 1; j * j <= i; ++j) {
+        // 对方输了，那么我赢
+        if (!dp[i - j * j]) {
+          dp[i] = true;
+          break;
+        }
+      }
+    }
+
+    return dp[n];
+  }
+};
