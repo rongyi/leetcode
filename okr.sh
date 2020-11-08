97b28a4cd1470b28c2e97359c9532a18b7b4acd1
rongyi <rongyi@onchain.com>
2020-11-08 11:17:15 +0800

    Update leetcode

--- okr.sh
+++ okr.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # deduplicate
+# some times also print the cc files
+git log --since=4.days --stat |grep '\.cc' |grep -v 'scratch' |awk -F'|' '{print $1}'  |sed -e 's/^  *//' -e 's/  *$//' |sort |uniq
+
 git log --since=4.days --stat |grep '\.cc' |grep -v 'scratch' |awk -F'|' '{print $1}'  |sed -e 's/^  *//' -e 's/  *$//' |sort |uniq  |wc -l
