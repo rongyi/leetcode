59ddbc814f00a1989f668752cb6905bb39a6b40f
rongyi <rongyi@onchain.com>
2020-11-08 11:13:38 +0800

    Update leetcode

---
+++ cc/water-bottles.cc
@@ -0,0 +1,21 @@
+// http://leetcode.com/problems/water-bottles/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int numWaterBottles(int numBottles, int numExchange) {
+    // drink all
+    int ret = numBottles;
+
+    // we have numBottles empty bottles
+    while (numBottles >= numExchange) {
+      int ex_bottle = numBottles / numExchange;
+      // we drink it
+      ret += numBottles / numExchange;
+      numBottles = numBottles / numExchange + numBottles % numExchange;
+      // drink the ex bottle and we have more empty bottles
+    }
+
+    return ret;
+  }
+};
