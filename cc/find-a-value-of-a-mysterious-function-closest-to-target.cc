defe65997b9ea226aa170428add796330fe92819
rongyi <rongyi@onchain.com>
2020-11-13 20:27:06 +0800

    Update leetcode

---
+++ cc/find-a-value-of-a-mysterious-function-closest-to-target.cc
@@ -0,0 +1,29 @@
+// http://leetcode.com/problems/find-a-value-of-a-mysterious-function-closest-to-target/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int closestToTarget(vector<int> &arr, int target) {
+    int ret = numeric_limits<int>::max();
+    const int n = arr.size();
+    // and_nums[i] i开始位置的与的uniq的那些数字
+    set<int> and_nums[n];
+    and_nums[n - 1].insert(arr[n - 1]);
+
+    for (int i = n - 2; i >= 0; --i) {
+      // 首先是当前的这个值
+      and_nums[i].insert(arr[i]);
+      // 其次是跟之前的and操作的值
+      for (auto &next_num : and_nums[i + 1]) {
+        and_nums[i].insert(arr[i] & next_num);
+      }
+    }
+    for (int i = 0; i < n; ++i) {
+      for (auto &num : and_nums[i]) {
+        ret = min(ret, abs(num - target));
+      }
+    }
+
+    return ret;
+  }
+};
