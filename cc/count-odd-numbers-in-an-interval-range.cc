9113d42634c86bb71f2ef455c5f316ece38c1eb8
rongyi <rongyi@onchain.com>
2020-11-15 10:06:12 +0800

    Update leetcode

---
+++ cc/count-odd-numbers-in-an-interval-range.cc
@@ -0,0 +1,12 @@
+// http://leetcode.com/problems/count-odd-numbers-in-an-interval-range/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int countOdds(int low, int high) {
+    if (low & 1) {
+      return (high - low + 2) / 2; // (high - low + 1) + 1
+    }
+    return (high - low + 1) / 2;
+  }
+};
