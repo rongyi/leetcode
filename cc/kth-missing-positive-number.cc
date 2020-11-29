7056997ff300a1392022489458ff1b875a843a11
rongyi <rongyi@onchain.com>
2020-11-29 18:31:35 +0800

    Add kth missing positive number

---
+++ cc/kth-missing-positive-number.cc
@@ -0,0 +1,17 @@
+// http://leetcode.com/problems/kth-missing-positive-number/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int findKthPositive(vector<int> &arr, int k) {
+    for (int n = 1, i = 0; n <= 1000; ++n) {
+      if (i < arr.size() && arr[i] == n) {
+        ++i;
+      } else if (--k == 0) {
+        return n;
+      }
+    }
+
+    return 1000 + k;
+  }
+};
