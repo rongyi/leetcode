d0db70b7e6c9092c21f59b4003e04e6621c2ec1a
rongyi <rongyi@onchain.com>
2020-12-12 11:46:35 +0800

    Update leetcode
    Add maximum non overlapping

---
+++ cc/maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.cc
@@ -0,0 +1,32 @@
+// http://leetcode.com/problems/maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int maxNonOverlapping(vector<int> &nums, int target) {
+    unordered_map<int, int> sum_idx;
+    sum_idx[0] = -1;
+    int prev_right = -1;
+    int cur_sum = 0;
+    int ret = 0;
+    for (int i = 0; i < nums.size(); ++i) {
+      cur_sum += nums[i];
+      if (sum_idx.count(cur_sum - target)) {
+        int next_left = sum_idx[cur_sum - target] + 1;
+        if (next_left > prev_right) {
+          ++ret;
+          prev_right = i;
+        }
+      }
+
+      sum_idx[cur_sum] = i;
+    }
+    return ret;
+  }
+};
+
+int main() {
+  Solution so;
+  vector<int> input{-2, 6, 6, 3, 5, 4, 1, 2, 8};
+  so.maxNonOverlapping(input, 10);
+}
