a67b1e1753bc37b57135ecbd866c9b2b2eb05be1
rongyi <rongyi@onchain.com>
2020-12-20 10:41:52 +0800

    Update leetcode

---
+++ cc/maximum-number-of-coins-you-can-get.cc
@@ -0,0 +1,15 @@
+// http://leetcode.com/problems/maximum-number-of-coins-you-can-get/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int maxCoins(vector<int> &piles) {
+    const int n = piles.size();
+    sort(piles.begin(), piles.end(), std::greater<int>());
+    int ret = 0;
+    for (int i = 0; i < n / 3; ++i) {
+      ret += piles[i * 2 + 1];
+    }
+    return ret;
+  }
+};
