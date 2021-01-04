cf5ea8a64f422a1175ef57413e698c8a87a9f254
rongyi <rongyi@onchain.com>
2021-01-05 07:17:36 +0800

    Update leetcode

---
+++ cc/matrix-diagonal-sum.cc
@@ -0,0 +1,19 @@
+// http://leetcode.com/problems/matrix-diagonal-sum/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int diagonalSum(vector<vector<int>> &mat) {
+    int ret = 0;
+    const int n = mat.size();
+    for (int i = 0, j = n - 1; i < n; ++i, --j) {
+      ret += mat[i][i];
+      ret += mat[i][j];
+    }
+    if (n & 1) {
+      ret -= mat[n / 2][n / 2];
+    }
+
+    return ret;
+  }
+};
