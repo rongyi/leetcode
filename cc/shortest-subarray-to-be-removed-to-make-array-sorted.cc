d8a545b2fc9962af7aadf8801c2a970539771e0e
rongyi <rongyi@onchain.com>
2021-01-07 08:17:32 +0800

    Update leetcode

---
+++ cc/shortest-subarray-to-be-removed-to-make-array-sorted.cc
@@ -0,0 +1,24 @@
+// http://leetcode.com/problems/shortest-subarray-to-be-removed-to-make-array-sorted/description/
+#include "xxx.h"
+class Solution {
+public:
+  // 是连续的，所以看中间那块能有多短
+  int findLengthOfShortestSubarray(vector<int> &arr) {
+    const int n = arr.size();
+    int right = n - 1;
+    for (; right > 0 && arr[right - 1] <= arr[right]; --right)
+      ;
+    auto ret = right;
+    for (int left = 0;
+         // 注意右边界right
+         left < right && (left == 0 || arr[left - 1] <= arr[left]); ++left) {
+      // 不符合非递减条件
+      while (right < n && arr[right] < arr[left]) {
+        ++right;
+      }
+      ret = min(ret, right - left - 1);
+    }
+
+    return ret;
+  }
+};
