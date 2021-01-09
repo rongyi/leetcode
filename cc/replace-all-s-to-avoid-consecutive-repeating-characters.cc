390ca0d56567c2e600aefba5bd2335a29539f753
rongyi <rongyi@onchain.com>
2021-01-09 10:32:47 +0800

    Update leetcode

---
+++ cc/replace-all-s-to-avoid-consecutive-repeating-characters.cc
@@ -0,0 +1,23 @@
+// http://leetcode.com/problems/replace-all-s-to-avoid-consecutive-repeating-characters/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  string modifyString(string s) {
+    char c = 'a';
+    const int n = s.size();
+    auto validchar = [&](int i) -> char {
+      for (; (i + 1 < n && c == s[i + 1]) || (i - 1 >= 0 && c == s[i - 1]);
+           c = (c - 'a' + 1) % 26 + 'a')
+        ;
+      return c;
+    };
+    for (int i = 0; i < s.size(); ++i) {
+      if (s[i] == '?') {
+        s[i] = validchar(i);
+      }
+    }
+
+    return s;
+  }
+};
