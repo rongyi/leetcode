cc6ef854f62a449f288e0791386c3c50cb865d72
rongyi <rongyi@onchain.com>
2021-01-10 11:17:44 +0800

    Update leetcode

---
+++ cc/count-unhappy-friends.cc
@@ -0,0 +1,37 @@
+// http://leetcode.com/problems/count-unhappy-friends/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  // 喷的很惨
+  int unhappyFriends(int n, vector<vector<int>> &preferences,
+                     vector<vector<int>> &pairs) {
+    vector<unordered_map<int, int>> prefs(n);
+    for (int i = 0; i < n; ++i) {
+      int psize = preferences[i].size();
+      for (int j = 0; j < psize; ++j) {
+        prefs[i][preferences[i][j]] = j;
+      }
+    }
+    vector<int> distance(n);
+    for (auto &p : pairs) {
+      distance[p[0]] = prefs[p[0]][p[1]];
+      distance[p[1]] = prefs[p[1]][p[0]];
+    }
+    int ret = 0;
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < distance[i]; ++j) {
+        // i这个小朋友其实更喜欢x，看看x是不是也更喜欢i，如果是就不开心了
+        int x = preferences[i][j];
+        for (int k = 0; k < distance[x]; ++k) {
+          if (i == preferences[x][k]) {
+            ret++;
+            j = k = n;
+          }
+        }
+      }
+    }
+
+    return ret;
+  }
+};
---
+++ cc/min-cost-to-connect-all-points.cc
@@ -0,0 +1,49 @@
+// http://leetcode.com/problems/min-cost-to-connect-all-points/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int minCostConnectPoints(vector<vector<int>> &points) {
+    int n = points.size();
+    int ret = 0;
+    vector<int> ds(n, -1);
+    vector<array<int, 3>> arr;
+    for (auto i = 0; i < n; ++i) {
+      for (auto j = i + 1; j < n; ++j) {
+        auto md =
+            abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
+        arr.push_back({md, i, j});
+      }
+    }
+    make_heap(arr.begin(), arr.end(), greater<array<int, 3>>());
+    while (!arr.empty()) {
+      pop_heap(arr.begin(), arr.end(), greater<array<int, 3>>());
+      auto last = arr.back();
+      auto dist = last[0];
+      auto i = last[1];
+      auto j = last[2];
+      arr.pop_back();
+
+      i = find(ds, i);
+      j = find(ds, j);
+      if (i != j) {
+        ret += dist;
+        // 连起来
+        ds[i] += ds[j];
+        ds[j] = i;
+        // 如果这里不理解请参考:
+        // remove-max-number-of-edges-to-keep-graph-fully-traversable.cc:28
+        if (ds[i] == -n) {
+          break;
+        }
+      }
+    }
+
+    return ret;
+  }
+
+private:
+  int find(vector<int> &ds, int i) {
+    return ds[i] < 0 ? i : ds[i] = find(ds, ds[i]);
+  }
+};
