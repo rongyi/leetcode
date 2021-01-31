408c2a444c8501bb527b3f1312712c1bbfe29b34
rongyi <rongyi@onchain.com>
2021-01-31 11:32:15 +0800

    Update leetcode

---
+++ cc/maximum-profit-of-operating-a-centennial-wheel.cc
@@ -0,0 +1,26 @@
+// http://leetcode.com/problems/maximum-profit-of-operating-a-centennial-wheel/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int minOperationsMaxProfit(vector<int> &customers, int boardingCost,
+                             int runningCost) {
+    int wait = 0;
+    int prof = 0;
+    int max_prof = 0;
+    int max_i = -1;
+    for (int i = 0; i < customers.size() || wait > 0; ++i) {
+      if (i < customers.size()) {
+        wait += customers[i];
+      }
+      prof += min(4, wait) * boardingCost - runningCost;
+      wait -= min(4, wait);
+      if (max_prof < prof) {
+        max_prof = prof;
+        max_i = i + 1;
+      }
+    }
+
+    return max_i;
+  }
+};
