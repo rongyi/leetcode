6e5e720ee634ba5150fc0db0f2f80d429c0503df
rongyi <rongyi@onchain.com>
2021-03-06 11:11:29 +0800

    Update leetcode

---
+++ cc/minimum-one-bit-operations-to-make-integers-zero.cc
@@ -0,0 +1,17 @@
+// http://leetcode.com/problems/minimum-one-bit-operations-to-make-integers-zero/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  // https://leetcode.com/problems/minimum-one-bit-operations-to-make-integers-zero/discuss/877741/C%2B%2B-solution-with-explanation
+  int minimumOneBitOperations(int n) {
+    if (n <= 1) {
+      return n;
+    }
+    int bit = 0;
+    while ((1 << bit) <= n) {
+      ++bit;
+    }
+    return ((1 << bit) - 1) - minimumOneBitOperations(n - (1 << (bit - 1)));
+  }
+};
