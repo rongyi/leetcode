96db9cb2747ba679b76c5a01a68433ed3a0bea02
rongyi <rongyi@onchain.com>
2021-03-06 11:17:31 +0800

    Update leetcode

---
+++ cc/maximum-nesting-depth-of-the-parentheses.cc
@@ -0,0 +1,21 @@
+// http://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int maxDepth(string s) {
+    vector<char> stk;
+    int ret = 0;
+
+    for (auto c : s) {
+      if (c == '(') {
+        stk.push_back('(');
+        ret = max(ret, int(stk.size()));
+      } else if (c == ')') {
+        stk.pop_back();
+      }
+    }
+
+    return ret;
+  }
+};
