009b20728e21fd8789c6f252e53b533cf0a38a70
rongyi <rongyi@onchain.com>
2021-03-21 08:26:10 +0800

    Add arithmetic subarrays

---
+++ cc/arithmetic-subarrays.cc
@@ -0,0 +1,35 @@
+// http://leetcode.com/problems/arithmetic-subarrays/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<bool> checkArithmeticSubarrays(vector<int> &nums, vector<int> &l,
+                                        vector<int> &r) {
+    // 暴力可以过
+    vector<bool> ret;
+    for (int i = 0; i < l.size(); ++i) {
+      vector<int> cur;
+      copy(nums.begin() + l[i], next(nums.begin() + r[i]), back_inserter(cur));
+      if (is_arithmetic(cur)) {
+        ret.push_back(true);
+      } else {
+        ret.push_back(false);
+      }
+    }
+    return ret;
+  }
+
+private:
+  bool is_arithmetic(vector<int> &cur) {
+    sort(cur.begin(), cur.end());
+
+    auto delta = cur[1] - cur[0];
+
+    for (int i = 1; i < cur.size(); ++i) {
+      if (cur[i] - cur[i - 1] != delta) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
--- cc/slowest-key.cc
+++ cc/slowest-key.cc
@@ -6,6 +6,7 @@ public:
   char slowestKey(vector<int> &releaseTimes, string keysPressed) {
     int max_interval = releaseTimes[0];
     char max_key = keysPressed[0];
+
     for (int i = 1; i < keysPressed.size(); ++i) {
       auto cur = releaseTimes[i] - releaseTimes[i - 1];
       if (cur > max_interval) {
