c1421b023f69851cff049a9b5c26ea4bea285e22
rongyi <rongyi@onchain.com>
2021-05-13 08:34:39 +0800

    Update leetcode

---
+++ cc/check-if-two-string-arrays-are-equivalent.cc
@@ -0,0 +1,16 @@
+// http://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  bool arrayStringsAreEqual(vector<string> &word1, vector<string> &word2) {
+    auto flat = [](vector<string> &lst) -> string {
+      ostringstream ss;
+      for (auto &s : lst) {
+        ss << s;
+      }
+      return ss.str();
+    };
+    return flat(word1) == flat(word2);
+  }
+};
