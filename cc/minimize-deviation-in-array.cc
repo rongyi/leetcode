2600a559b0669680ba6b73313b882b90e1847197
rongyi <rongyi@onchain.com>
2021-05-23 09:01:44 +0800

    Update leetcode

---
+++ cc/minimize-deviation-in-array.cc
@@ -0,0 +1,30 @@
+// http://leetcode.com/problems/minimize-deviation-in-array/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int minimumDeviation(vector<int> &nums) {
+    int ret = numeric_limits<int>::max();
+    int cur_min = numeric_limits<int>::max();
+
+    // 1. 偶数不会升
+    // 2. double odd once
+    priority_queue<int> pq;
+    for (auto &n : nums) {
+      if (n & 1) {
+        n *= 2;
+      }
+      pq.push(n);
+      cur_min = min(cur_min, n);
+    }
+
+    while (pq.top() % 2 == 0) {
+      ret = min(ret, pq.top() - cur_min);
+      cur_min = min(cur_min, pq.top() / 2);
+      pq.push(pq.top() / 2);
+      pq.pop();
+    }
+
+    return min(ret, pq.top() - cur_min);
+  }
+};
