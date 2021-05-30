f439b09d5a60088d9d1f205f3c82d6b3b28299c1
rongyi <rongyi@onchain.com>
2021-05-30 08:40:54 +0800

    Update leetcode

---
+++ cc/partitioning-into-minimum-number-of-deci-binary-numbers.cc
@@ -0,0 +1,17 @@
+// http://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int minPartitions(string n) {
+    char ret = '0';
+    for (auto &c : n) {
+      ret = max(ret, c);
+      // shortcut
+      if (ret == '9') {
+        return 9;
+      }
+    }
+    return ret - '0';
+  }
+};
