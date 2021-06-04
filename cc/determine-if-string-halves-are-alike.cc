d44a2175aaca6af1ac597c6e2b2fb6d97408f2c4
rongyi <rongyi@onchain.com>
2021-06-04 21:25:32 +0800

    Update leetcode
    commit at Dongshan hotel suzhou

---
+++ cc/determine-if-string-halves-are-alike.cc
@@ -0,0 +1,23 @@
+// http://leetcode.com/problems/determine-if-string-halves-are-alike/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  bool halvesAreAlike(string s) {
+    const int n = s.size() / 2;
+    // r not included
+    auto count = [](string &s, int l, int r) {
+      int ret = 0;
+      for (int i = l; i < r; ++i) {
+        if (s[i] == 'a' || s[i] == 'o' || s[i] == 'e' || s[i] == 'i' ||
+            s[i] == 'u' || s[i] == 'A' || s[i] == 'O' || s[i] == 'E' ||
+            s[i] == 'I' || s[i] == 'U') {
+          ret++;
+        }
+      }
+      return ret;
+    };
+
+    return count(s, 0, n) == count(s, n, 2 * n);
+  }
+};
---
+++ cc/maximum-number-of-eaten-apples.cc
@@ -0,0 +1,32 @@
+// http://leetcode.com/problems/maximum-number-of-eaten-apples/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int eatenApples(vector<int> &apples, vector<int> &days) {
+    using p = pair<int, int>;
+    priority_queue<p, vector<p>, greater<p>> pq;
+    const int n = apples.size();
+    int ret = 0;
+    int i = 0;
+    while (i < n || !pq.empty()) {
+      if (i < n && apples[i] > 0) {
+        pq.push({i + days[i] - 1, apples[i]});
+      }
+      while (pq.size() && pq.top().first < i) {
+        pq.pop();
+      }
+      if (pq.size()) {
+        auto cur = pq.top();
+        pq.pop();
+        if (cur.second > 1) {
+          pq.push({cur.first, cur.second - 1});
+        }
+        ++ret;
+      }
+      ++i;
+    }
+
+    return ret;
+  }
+};
