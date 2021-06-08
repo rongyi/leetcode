77ccc0fb85ff4802c36b5f631bbadc759abbd493
rongyi <rongyi@onchain.com>
2021-06-08 09:17:24 +0800

    Update worklog

---
+++ cc/where-will-the-ball-fall.cc
@@ -0,0 +1,45 @@
+// http://leetcode.com/problems/where-will-the-ball-fall/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<int> findBall(vector<vector<int>> &grid) {
+    const int m = grid.size();
+    const int n = grid[0].size();
+    vector<int> ret;
+    for (int b = 0; b < n; ++b) {
+      int i = 0;
+      int j = b;
+      for (; i < m; ++i) {
+        // right direction
+        if (grid[i][j] == 1) {
+          if (j == n - 1) {
+            break;
+          }
+          // vshape?
+          if (grid[i][j + 1] != grid[i][j]) {
+            break;
+          }
+          j++;
+        } else {
+          // left direction case
+          if (j == 0) {
+            break;
+          }
+          // vshape?
+          if (grid[i][j - 1] != grid[i][j]) {
+            break;
+          }
+          j--;
+        }
+      }
+      if (i == m) {
+        ret.push_back(j);
+      } else {
+        ret.push_back(-1);
+      }
+    }
+
+    return ret;
+  }
+};
