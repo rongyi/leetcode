cefbaa5ede044898dae5d17efa8b520a65ecce36
rongyi <rongyi@onchain.com>
2021-07-01 08:45:44 +0800

    Update leetcode

--- cc/building-boxes.cc
+++ cc/building-boxes.cc
@@ -22,12 +22,16 @@ public:
       return layer_sum;
     }
     // 高了，回滚最后一层
+    // 总数减去最下一层
     cur_total_sum -= layer_sum;
+    // 最下一层减去对角线的个数
     layer_sum -= layer;
     layer = 0;
-    // 从某个方向再1
+    // 从某个方向再逐渐放
     // 1..2 ..3这样的放
     // 地板上多少层就多多少个
+    // 看这里的图
+    // https://leetcode.com/problems/building-boxes/discuss/1032016/C%2B%2B-Python-3-variables-solution-with-drawing-explanation
     while (cur_total_sum < n) {
       ++layer;
       cur_total_sum += layer;
---
+++ cc/maximum-number-of-balls-in-a-box.cc
@@ -0,0 +1,24 @@
+// http://leetcode.com/problems/maximum-number-of-balls-in-a-box/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int countBalls(int lowLimit, int highLimit) {
+    map<int, int> count;
+    int ret = 0;
+    auto ds = [](int n) {
+      int ret = 0;
+      while (n) {
+        ret += n % 10;
+        n /= 10;
+      }
+      return ret;
+    };
+    for (int i = lowLimit; i <= highLimit; ++i) {
+      auto cur = ds(i);
+      count[cur]++;
+      ret = max(ret, count[cur]);
+    }
+    return ret;
+  }
+};
