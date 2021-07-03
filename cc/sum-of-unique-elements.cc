1ce4fd5d0e44e6fc5dd3c22ef8a25cc6248fecb5
rongyi <rongyi@onchain.com>
2021-07-03 19:46:57 +0800

    Update leetcode

---
+++ cc/sum-of-unique-elements.cc
@@ -0,0 +1,20 @@
+// http://leetcode.com/problems/sum-of-unique-elements/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int sumOfUnique(vector<int> &nums) {
+    map<int, int> count;
+    for (auto &n : nums) {
+      count[n]++;
+    }
+    int ret = 0;
+    for (auto kv : count) {
+      if (kv.second == 1) {
+        ret += kv.first;
+      }
+    }
+
+    return ret;
+  }
+};
