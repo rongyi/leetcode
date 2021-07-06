52a515a30bb23d7ecdb54fb4507f81210dc40458
rongyi <rongyi@onchain.com>
2021-07-06 08:16:10 +0800

    Update leetcode

---
+++ cc/minimum-changes-to-make-alternating-binary-string.cc
@@ -0,0 +1,33 @@
+// http://leetcode.com/problems/minimum-changes-to-make-alternating-binary-string/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int minOperations(string s) {
+    int ret = s.size();
+    // keep first unchanged, and flip in orders
+    int count = 0;
+    int expect = 1 - (s[0] - '0');
+    for (int i = 1; i < s.size(); ++i) {
+      int cur = s[i] - '0';
+      if (cur != expect) {
+        count++;
+      }
+      expect ^= 1;
+    }
+    ret = min(ret, count);
+    count = 1;
+    // flip first and change indorders
+    expect = s[0] - '0';
+    for (int i = 1; i < s.size(); ++i) {
+      int cur = s[i] - '0';
+      if (cur != expect) {
+        count++;
+      }
+      expect ^= 1;
+    }
+
+    // select the min
+    return min(ret, count);
+  }
+};
