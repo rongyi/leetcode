6adcd851d5326f446e5b2820fa6bcf7fb11725e2
rongyi <rongyi@onchain.com>
2021-07-08 07:14:30 +0800

    Update leetcode

---
+++ cc/count-number-of-homogenous-substrings.cc
@@ -0,0 +1,24 @@
+// http://leetcode.com/problems/count-number-of-homogenous-substrings/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int countHomogenous(string s) {
+    const int mod = 1e9 + 7;
+    const int n = s.size();
+    long long ret = 0;
+    for (int i = 0; i < n;) {
+      int j = i;
+      while (j < n && s[j] == s[i]) {
+        j++;
+      }
+      long long len = j - i;
+      long long cur_count = len * (len + 1) / 2 % mod;
+      ret += cur_count;
+      ret %= mod;
+      i = j;
+    }
+
+    return ret;
+  }
+};
