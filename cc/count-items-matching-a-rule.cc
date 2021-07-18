1904ac1a59e371cb78ca09c346dc503cc93c66dd
rongyi <rongyi@onchain.com>
2021-07-18 08:16:11 +0800

    Update leetcode

---
+++ cc/count-items-matching-a-rule.cc
@@ -0,0 +1,25 @@
+// http://leetcode.com/problems/count-items-matching-a-rule/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int countMatches(vector<vector<string>> &items, string ruleKey,
+                   string ruleValue) {
+    int idx = -1;
+    if (ruleKey == "type") {
+      idx = 0;
+    } else if (ruleKey == "color") {
+      idx = 1;
+    } else {
+      idx = 2;
+    }
+    int count = 0;
+    for (auto &item : items) {
+      if (item[idx] == ruleValue) {
+        ++count;
+      }
+    }
+
+    return count;
+  }
+};
