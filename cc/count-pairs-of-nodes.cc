778cc0c53958823bf1c70cab5ba123dda5c34b76
rongyi <rongyi@onchain.com>
2021-07-22 08:04:41 +0800

    Update leetcode

---
+++ cc/count-pairs-of-nodes.cc
@@ -0,0 +1,46 @@
+// http://leetcode.com/problems/count-pairs-of-nodes/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<int> countPairs(int n, vector<vector<int>> &edges,
+                         vector<int> &queries) {
+    vector<int> sorted_count(n + 1, 0);
+    vector<int> count(n + 1, 0);
+    // 两个点之间的共同边统计
+    vector<unordered_map<int, int>> shared_count(n + 1);
+    for (auto &e : edges) {
+      ++count[e[0]];
+      ++count[e[1]];
+      sorted_count[e[0]] = count[e[0]];
+      sorted_count[e[1]] = count[e[1]];
+
+      ++shared_count[min(e[0], e[1])][max(e[0], e[1])];
+    }
+    sort(sorted_count.begin(), sorted_count.end());
+
+    vector<int> ret;
+    for (auto q : queries) {
+      ret.push_back(0);
+      for (int i = 1, j = n; i < j;) {
+        // 先统计 degree(a) + degree(b) > q
+        if (sorted_count[i] + sorted_count[j] > q) {
+          ret.back() += (j - i);
+          --j;
+        } else {
+          ++i;
+        }
+      }
+      for (int i = 1; i <= n; ++i) {
+        for (auto [j, sh_cnt] : shared_count[i]) {
+          // 再减去degree(a) + degree(b) - edgeof(a, b) <= q
+          if (count[i] + count[j] > q && count[i] + count[j] - sh_cnt <= q) {
+            --ret.back();
+          }
+        }
+      }
+    }
+
+    return ret;
+  }
+};
---
+++ cc/sum-of-beauty-of-all-substrings.cc
@@ -0,0 +1,30 @@
+// http://leetcode.com/problems/sum-of-beauty-of-all-substrings/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  // 没想到是暴力的解法
+  int beautySum(string s) {
+    int ret = 0;
+    const int n = s.size();
+    for (int i = 0; i < n; ++i) {
+      vector<int> count(26, 0);
+      int max_freq = 0;
+      int min_freq = 0;
+      for (int j = i; j < n; ++j) {
+        int cur_idx = s[j] - 'a';
+        max_freq = max(max_freq, ++count[cur_idx]);
+        if (min_freq >= count[cur_idx] - 1) {
+          min_freq = count[cur_idx];
+          for (int k = 0; k < 26; ++k) {
+            min_freq = min(min_freq, count[k] == 0 ? numeric_limits<int>::max()
+                                                   : count[k]);
+          }
+        }
+        ret += (max_freq - min_freq);
+      }
+    }
+
+    return ret;
+  }
+};
