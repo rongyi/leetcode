93b504ddcee2d74ba6056cb3e7f54b02996cbf4c
rongyi <rongyi@onchain.com>
2021-07-25 08:37:40 +0800

    Update leetcode

---
+++ cc/maximum-score-of-a-good-subarray.cc
@@ -0,0 +1,25 @@
+// http://leetcode.com/problems/maximum-score-of-a-good-subarray/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int maximumScore(vector<int> &nums, int k) {
+    int ret = nums[k];
+    int min_range = nums[k];
+    int i = k;
+    int j = k;
+    const int n = nums.size();
+
+    // 总是尽可能的往大的那边靠
+    while (i > 0 || j < n - 1) {
+      if ((i > 0 ? nums[i - 1] : 0) < (j < n - 1 ? nums[j + 1] : 0)) {
+        min_range = min(min_range, nums[++j]);
+      } else {
+        min_range = min(min_range, nums[--i]);
+      }
+      ret = max(ret, min_range * (j - i + 1));
+    }
+
+    return ret;
+  }
+};
