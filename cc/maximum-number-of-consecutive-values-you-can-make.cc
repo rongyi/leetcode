497968d8f0e1f60ced9bf9e4fda4b5a884c47506
rongyi <rongyi@onchain.com>
2021-08-07 14:56:35 +0800

    Update leetcode

---
+++ cc/maximum-number-of-consecutive-values-you-can-make.cc
@@ -0,0 +1,16 @@
+// http://leetcode.com/problems/maximum-number-of-consecutive-values-you-can-make/description/
+#include_next "xxx.h"
+
+class Solution {
+public:
+  int getMaximumConsecutive(vector<int> &coins) {
+    sort(coins.begin(), coins.end());
+    // 出现gap的原因即是当前到达的最大值 < 下一个数字
+    int current_reach = 0;
+    for (int i = 0; i < coins.size() && coins[i] <= current_reach + 1; ++i) {
+      current_reach += coins[i];
+    }
+
+    return current_reach + 1;
+  }
+};
