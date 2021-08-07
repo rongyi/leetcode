5e91b7d8ce51682f9b9e2923b3fac78ca59e9f4f
rongyi <rongyi@onchain.com>
2021-08-07 15:30:20 +0800

    Update worklog

---
+++ cc/maximum-ascending-subarray-sum.cc
@@ -0,0 +1,22 @@
+// http://leetcode.com/problems/maximum-ascending-subarray-sum/description/
+#include_next "xxx.h"
+
+class Solution {
+public:
+  int maxAscendingSum(vector<int> &nums) {
+    int max_sum = nums[0];
+    int cur_max = nums[0];
+
+    for (int i = 1; i < nums.size(); ++i) {
+      if (nums[i] > nums[i - 1]) {
+        cur_max += nums[i];
+      } else {
+        cur_max = nums[i];
+      }
+
+      max_sum = max(max_sum, cur_max);
+    }
+
+    return max_sum;
+  }
+};
