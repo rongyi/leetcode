8c61296d84856b5023dcf2480696813695d33c40
rongyi <rongyi@onchain.com>
2021-08-21 09:31:40 +0800

    Update leetcode

---
+++ cc/minimum-absolute-sum-difference.cc
@@ -0,0 +1,35 @@
+// http://leetcode.com/problems/minimum-absolute-sum-difference/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  // 考察的lower_bound
+  int minAbsoluteSumDiff(vector<int> &nums1, vector<int> &nums2) {
+    set<int> s(nums1.begin(), nums1.end());
+
+    long ret = 0;
+    long gain = 0;
+    for (int i = 0; i < nums1.size(); ++i) {
+      long cur_diff = abs(nums1[i] - nums2[i]);
+      ret += cur_diff;
+      if (gain < cur_diff) {
+        auto it = s.lower_bound(nums2[i]);
+        if (it != s.end()) {
+          gain = max(gain, cur_diff - abs(*it - nums2[i]));
+        }
+        if (it != s.begin()) {
+          gain = max(gain, cur_diff - abs(*prev(it) - nums2[i]));
+        }
+      }
+    }
+    return (ret - gain) % (int)(1e9 + 7);
+  }
+};
+
+int main() {
+  vector<int> input{1, 2, 2, 3, 3, 4};
+  auto i = lower_bound(input.begin(), input.end(), -1);
+  auto j = upper_bound(input.begin(), input.end(), 2);
+  cout << *i << endl;
+  cout << *j << endl;
+}
---
+++ cc/number-of-different-subsequences-gcds.cc
@@ -0,0 +1,44 @@
+// http://leetcode.com/problems/number-of-different-subsequences-gcds/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int countDifferentSubsequenceGCDs(vector<int> &nums) {
+    int ret = 0;
+    // 一个数列的gcd不可能大于数列中的最大值
+    int max_num = numeric_limits<int>::min();
+    const int n = nums.size();
+    int b[200001] = {0};
+
+    for (int i = 0; i < n; ++i) {
+      max_num = max(max_num, nums[i]);
+      b[nums[i]] = 1;
+    }
+
+    // 一个数列的gcd随着数组的变多将会递减
+    for (int g = 1; g <= max_num; ++g) {
+      int cur_gcd = 0;
+
+      for (int j = g; j <= max_num; j += g) {
+        if (b[j] == 1) {
+          // e.g.
+          // g 为4
+          // 出现的numer为 8 16 24 36 ==> gcd 为 4
+          cur_gcd = __gcd(cur_gcd, j);
+        }
+
+        // 最终会降到这里，那么g这个value可以形成一个subsequence
+        if (cur_gcd == g) {
+          ret++;
+          break;
+        }
+      }
+    }
+
+    return ret;
+  }
+};
+
+int main() {
+  cout << __gcd(0, 10) << endl;
+}
