2e4ead4f69ef4e9b1ad21c35e5d974ec73019156
rongyi <rongyi@onchain.com>
2021-08-21 09:35:40 +0800

    Update leetcode

---
+++ cc/sign-of-the-product-of-an-array.cc
@@ -0,0 +1,23 @@
+// http://leetcode.com/problems/sign-of-the-product-of-an-array/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int arraySign(vector<int> &nums) {
+    // < 0 count
+    int ltz = 0;
+    for (auto num : nums) {
+      if (num == 0) {
+        return 0;
+      }
+      if (num < 0) {
+        ltz++;
+      }
+    }
+
+    if (ltz & 1) {
+      return -1;
+    }
+    return 1;
+  }
+};
