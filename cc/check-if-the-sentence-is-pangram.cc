4ddf22d82e1f292615ab2d8791fc96823f1e9c5b
rongyi <rongyi@onchain.com>
2021-08-29 08:54:16 +0800

    Update leetcode

---
+++ cc/check-if-the-sentence-is-pangram.cc
@@ -0,0 +1,20 @@
+// http://leetcode.com/problems/check-if-the-sentence-is-pangram/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  bool checkIfPangram(string sentence) {
+    vector<int> count(26, 0);
+
+    for (auto c : sentence) {
+      count[c - 'a']++;
+    }
+    for (auto cur : count) {
+      if (cur == 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+};
