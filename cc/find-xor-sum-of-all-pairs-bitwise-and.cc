dacc371570c0bed6a44f4b544f8e03d8129ea6a5
rongyi <rongyi@onchain.com>
2021-09-04 15:51:24 +0800

    Update leetcode

--- cc/find-xor-sum-of-all-pairs-bitwise-and.cc
+++ cc/find-xor-sum-of-all-pairs-bitwise-and.cc
@@ -5,6 +5,8 @@ class Solution {
 public:
   int getXORSum(vector<int> &arr1, vector<int> &arr2) {
     int sum1 = 0;
+    //(x1 & b1) ^ (x2 & b1) ^ ... (xn & b1) ==>
+    // (x1 ^ x2 ^ ... ^ xn) & b1
     for (auto num : arr1) {
       sum1 ^= num;
     }
