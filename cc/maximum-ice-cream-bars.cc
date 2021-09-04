59ba336a3382a92aaf79ae4fc30f96f52f3442c1
rongyi <rongyi@onchain.com>
2021-09-04 09:36:52 +0800

    Update leetcode

---
+++ cc/maximum-ice-cream-bars.cc
@@ -0,0 +1,22 @@
+// http://leetcode.com/problems/maximum-ice-cream-bars/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int maxIceCream(vector<int> &costs, int coins) {
+    // pick low price first
+    int count = 0;
+    priority_queue<int, vector<int>, greater<int>> pq(costs.begin(),
+                                                      costs.end());
+    while (!pq.empty()) {
+      auto cur_price = pq.top();
+      pq.pop();
+      if (cur_price > coins) {
+        break;
+      }
+      count++;
+      coins -= cur_price;
+    }
+    return count;
+  }
+};
