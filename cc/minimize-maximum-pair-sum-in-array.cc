f644d69f97cafcc28981a21699c5ead1dc0accf3
rongyi <rongyi@onchain.com>
2021-09-26 19:20:59 +0800

    Update leetcode

---
+++ cc/minimize-maximum-pair-sum-in-array.cc
@@ -0,0 +1,17 @@
+// http://leetcode.com/problems/minimize-maximum-pair-sum-in-array/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int minPairSum(vector<int> &nums) {
+    sort(nums.begin(), nums.end());
+    int sz = nums.size();
+    int ret = 0;
+
+    for (int i = 0; i < sz / 2; ++i) {
+      ret = max(ret, nums[i] + nums[sz - i - 1]);
+    }
+
+    return ret;
+  }
+};
