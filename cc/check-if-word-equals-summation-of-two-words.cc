26cb60d75112fe5b567ebea15128832151c2bdc8
rongyi <rongyi@onchain.com>
2021-09-30 07:06:10 +0800

    Update leetcode

---
+++ cc/check-if-word-equals-summation-of-two-words.cc
@@ -0,0 +1,25 @@
+// http://leetcode.com/problems/check-if-word-equals-summation-of-two-words/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  bool isSumEqual(string firstWord, string secondWord, string targetWord) {
+    auto tonum = [](string &s) -> int {
+      string ns;
+      for (auto c : s) {
+        ns.push_back('0' + (c - 'a'));
+      }
+      int i = 0;
+      int sz = ns.size();
+      while (i < sz && ns[i] == '0') {
+        i++;
+      }
+      if (i == sz) {
+        return 0;
+      }
+      return stoi(ns.substr(i));
+    };
+
+    return (tonum(firstWord) + tonum(secondWord)) == tonum(targetWord);
+  }
+};
---
+++ cc/minimum-xor-sum-of-two-arrays.cc
@@ -0,0 +1,37 @@
+// http://leetcode.com/problems/minimum-xor-sum-of-two-arrays/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  Solution() { memset(dp_, numeric_limits<int>::max(), sizeof(dp_)); }
+  int minimumXORSum(vector<int> &nums1, vector<int> &nums2,
+                    int i = 0 /* current nums1 index*/, int mask = 0) {
+    if (i >= nums1.size()) {
+      return 0;
+    }
+
+    if (dp_[mask] == numeric_limits<int>::max()) {
+      for (int j = 0; j < nums2.size(); ++j) {
+        // not yet used
+        if ((mask & (1 << j)) == 0) {
+          dp_[mask] = min(dp_[mask], (nums1[i] ^ nums2[j]) +
+                                         minimumXORSum(nums1, nums2, i + 1,
+                                                       mask | (1 << j)));
+        }
+      }
+    }
+
+    return dp_[mask];
+  }
+
+private:
+  int dp_[16384] = {[0 ... 16383] = INT_MAX};
+};
+
+int main() {
+  int nums[3] = {[0 ... 1] = 99};
+  for (auto n : nums) {
+    cout << n << endl;
+  }
+  cout << sizeof(nums) << endl;
+}
