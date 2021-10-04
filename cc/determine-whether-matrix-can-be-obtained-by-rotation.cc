00fd93af11f05c3aa852c7a61b8b3cb9ad02038d
rongyi <rongyi@onchain.com>
2021-10-05 07:48:44 +0800

    Update leetcode

---
+++ cc/determine-whether-matrix-can-be-obtained-by-rotation.cc
@@ -0,0 +1,66 @@
+// http://leetcode.com/problems/determine-whether-matrix-can-be-obtained-by-rotation/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  bool findRotation(vector<vector<int>> &mat, vector<vector<int>> &target) {
+    if (equalMatrix(mat, target)) {
+      return true;
+    }
+
+    // not the transform position
+    // clock wise 90
+    int n = mat.size();
+    vector<vector<int>> tmp(n, vector<int>(n, 0));
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
+        tmp[j][n - i - 1] = mat[i][j];
+      }
+    }
+
+    if (equalMatrix(tmp, target)) {
+      return true;
+    }
+
+    // clock wise 180
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; ++j) {
+        tmp[n - i - 1][n - j - 1] = mat[i][j];
+      }
+    }
+    if (equalMatrix(tmp, target)) {
+      return true;
+    }
+    // clock wise 270
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
+        tmp[n - j - 1][i] = mat[i][j];
+      }
+    }
+    if (equalMatrix(tmp, target)) {
+      return true;
+    }
+
+    return false;
+  }
+
+private:
+  bool equalMatrix(vector<vector<int>> &l, vector<vector<int>> &r) {
+    int n = l.size();
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if (l[i][j] != r[i][j]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+};
+
+int main() {
+  Solution so;
+  vector<vector<int>> input{{0, 1}, {1, 1}};
+  vector<vector<int>> target{{1, 0}, {0, 1}};
+  so.findRotation(input, target);
+}
---
+++ cc/egg-drop-with-2-eggs-and-n-floors.cc
@@ -0,0 +1,34 @@
+// http://leetcode.com/problems/egg-drop-with-2-eggs-and-n-floors/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int twoEggDrop(int n) {
+    vector<int> bests(n + 1);
+    // dp[i][j] ==> building have j floors and drop first egg at i level
+
+    bests[0] = 0;
+    bests[1] = 1;
+    if (n >= 2) {
+      bests[2] = 2;
+    }
+
+    for (int total_floor = 3; total_floor <= n; ++total_floor) {
+      int curBest = n;
+      for (int first_drop = 1; first_drop <= total_floor; ++first_drop) {
+        // egg breaks at current drop
+        int breaks = 1 + first_drop - 1;
+        // egg does not break at current drop
+        int survive = 1 + bests[total_floor - first_drop];
+        // either break or not break, it is a drop that should be counted
+        // so we choose the max one as valid drop count
+
+        // https://leetcode.com/problems/egg-drop-with-2-eggs-and-n-floors/discuss/1265931/That-Math-solution-is-too-fancy-to-come-up-with.-Let's-just-try-DP-with-step-by-step-explanations
+        curBest = min(curBest, max(breaks, survive));
+      }
+      bests[total_floor] = curBest;
+    }
+
+    return bests[n];
+  }
+};
