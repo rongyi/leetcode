dcf1c171f25cd30c2ac440aa305427e77c46b796
rongyi <rongyi@onchain.com>
2021-10-05 09:21:26 +0800

    Update leetcode

--- cc/reduction-operations-to-make-the-array-elements-equal.cc
+++ cc/reduction-operations-to-make-the-array-elements-equal.cc
@@ -6,6 +6,11 @@ public:
   int reductionOperations(vector<int> &nums) {
     int ret = 0;
     int sz = nums.size();
+    // 2 1 4 4 5 7 7 7
+    // 7 7 7 --> 3
+    // 5 7 7 7 -> 4
+    // 4 4 5 7 7 7 -> 6
+    // ...
     sort(nums.begin(), nums.end());
     for (int i = sz - 1; i > 0; --i) {
       if (nums[i - 1] != nums[i]) {
