f7ab379d2f0a5f3b6c97b1b26c2a5ebfb0b59b7a
rongyi <rongyi@onchain.com>
2021-10-13 07:25:21 +0800

    Update leetcode

--- cc/check-if-all-the-integers-in-a-range-are-covered.cc
+++ cc/check-if-all-the-integers-in-a-range-are-covered.cc
@@ -2,25 +2,27 @@
 
 class Solution {
 public:
-    bool isCovered(vector<vector<int>>& ranges, int left, int right) {
-      sort(ranges.begin(), ranges.end());
-      bool is_point = (left == right);
-      for (auto &r: ranges) {
-        // totally contained in this range
-        if (r[0] <= left && r[1] >= right) {
-          return true;
-        }
-        // then we shrink the range
-        // will never match
-        if (left < r[0]) {
-          return false;
-        }
-        // shrink the range
-        if (left <= r[1]) {
-          left = r[1] + 1;
-        }
+  bool isCovered(vector<vector<int>> &ranges, int left, int right) {
+    sort(ranges.begin(), ranges.end());
 
+    for (auto &r : ranges) {
+      // totally contained in this range
+      if (r[0] <= left && r[1] >= right) {
+        return true;
       }
-      return left > right;
+      // then we shrink the range
+      // will never match
+      // the left part will never catch, cause the next range starting point
+      // is >= current
+      if (left < r[0]) {
+        return false;
+      }
+      // shrink the range
+      if (left <= r[1]) {
+        left = r[1] + 1;
+      }
     }
+
+    return left > right;
+  }
 };
