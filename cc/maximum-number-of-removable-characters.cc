85f8f63ee1f438b3e245845b9a6c151c54b06067
rongyi <rongyi@onchain.com>
2021-10-18 07:36:05 +0800

    Update leetcode

---
+++ cc/maximum-number-of-removable-characters.cc
@@ -0,0 +1,44 @@
+// http://leetcode.com/problems/maximum-number-of-removable-characters/description/
+#include "xxx.h"
+
+class Solution {
+public:
+  int maximumRemovals(string s, string p, vector<int> &removable) {
+    int l = 0;
+    int r = removable.size();
+    while (l <= r) {
+      auto mid = (l + r) / 2;
+      if (valid(s, p, removable, mid)) {
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
+    }
+    return r;
+  }
+
+private:
+  bool valid(string &s, string &p, vector<int> &rm, int k) {
+    memset(black_, 0, sizeof(black_));
+    for (int i = 0; i < k; ++i) {
+      // the remove index;
+      black_[rm[i]] = 1;
+    }
+    int m = s.size();
+    int n = p.size();
+    int j = 0;
+    for (int i = 0; i < m && j < n; ++i) {
+      if (black_[i] == 1) {
+        continue;
+      }
+      if (s[i] == p[j]) {
+        ++j;
+      }
+    }
+
+    return j == n;
+  }
+
+private:
+  int black_[100001];
+};
---
+++ cc/redistribute-characters-to-make-all-strings-equal.cc
@@ -0,0 +1,20 @@
+#include "xxx.h"
+
+class Solution {
+public:
+  bool makeEqual(vector<string> &words) {
+    int sz = words.size();
+    vector<int> count(26, 0);
+    for (auto &w : words) {
+      for (auto c : w) {
+        count[c - 'a']++;
+      }
+    }
+    for (int i = 0; i < 26; ++i) {
+      if (count[i] % sz) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
