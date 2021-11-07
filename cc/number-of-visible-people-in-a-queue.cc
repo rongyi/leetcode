4b58c11f0602b429deb9b425287f3d68e5086f21
rongyi <rongyi@onchain.com>
2021-11-08 07:20:11 +0800

    Update leetcode

---
+++ cc/number-of-visible-people-in-a-queue.cc
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/number-of-visible-people-in-a-queue/
+#include "xxx.h"
+
+class Solution {
+public:
+  vector<int> canSeePersonsCount(vector<int> &heights) {
+    int sz = heights.size();
+    stack<int> st;
+    vector<int> ret(sz, 0);
+    for (int i = sz - 1; i >= 0; --i) {
+      // all numbers are uniq
+      while (!st.empty() && heights[i] > st.top()) {
+        ret[i]++;
+        st.pop();
+      }
+      // can see one more which is taller than me
+      if (!st.empty()) {
+        ret[i]++;
+      }
+      st.push(heights[i]);
+    }
+    return ret;
+  }
+};
