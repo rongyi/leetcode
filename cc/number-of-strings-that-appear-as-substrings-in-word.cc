cc41acb4ec71422b058853c57028c6ec89713dc2
rongyi <rongyi@onchain.com>
2021-11-30 08:22:27 +0800

    Update leetcode

---
+++ cc/number-of-strings-that-appear-as-substrings-in-word.cc
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/number-of-strings-that-appear-as-substrings-in-word/
+#include "xxx.h"
+
+class Solution {
+public:
+  int numOfStrings(vector<string> &patterns, string word) {
+    int count = 0;
+    for (auto &p : patterns) {
+      if (word.find(p) != string::npos) {
+        count++;
+      }
+    }
+
+    return count;
+  }
+};
