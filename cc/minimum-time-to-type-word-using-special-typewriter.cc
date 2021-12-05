ad7547b6b87d3fa367f2d6f8cee1f0ea5695a584
rongyi <rongyi@onchain.com>
2021-12-05 09:49:20 +0800

    Update leetcode

---
+++ cc/minimum-time-to-type-word-using-special-typewriter.cc
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/minimum-time-to-type-word-using-special-typewriter/
+#include "xxx.h"
+
+class Solution {
+public:
+  int minTimeToType(string word) {
+    int prev = 'a';
+    int ret = word.size();
+    for (auto c : word) {
+      // anti clockwise
+      int aclk = (c - prev + 26) % 26;
+      // clockwise rotate
+      int clk = (26 - (c - prev) + 26) % 26;
+      ret += min(aclk, clk);
+      prev = c;
+    }
+
+    return ret;
+  }
+};
+
+int main() {
+  Solution so;
+  int ret = so.minTimeToType("bza");
+  cout << ret << endl;
+}
