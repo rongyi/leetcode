dd38b78e3c2d9a09452322a2f377960a0a1db318
rongyi <rongyi@onchain.com>
2021-12-21 08:55:17 +0800

    Update leetcode

---
+++ cc/count-number-of-pairs-with-absolute-difference-k.cc
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/count-number-of-pairs-with-absolute-difference-k/
+#include "xxx.h"
+
+class Solution {
+public:
+  int countKDifference(vector<int> &nums, int k) {
+    map<int, int> cnt;
+    for (auto &num : nums) {
+      ++cnt[num];
+    }
+    int ret = 0;
+    // remember to use reference
+    for (auto &kv : cnt) {
+      if (cnt.count(kv.first + k)) {
+        ret += (kv.second * cnt[kv.first + k]);
+      }
+      if (cnt.count(kv.first - k)) {
+        ret += (kv.second * cnt[kv.first - k]);
+      }
+      // don't use this value again
+      kv.second = 0;
+    }
+    return ret;
+  }
+};
