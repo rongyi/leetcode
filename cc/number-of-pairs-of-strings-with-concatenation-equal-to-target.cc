f09a97846a06960e8ab48e161bd6195613770b89
rongyi <rongyi@onchain.com>
2021-12-26 10:46:56 +0800

    Update leetcode

---
+++ cc/number-of-pairs-of-strings-with-concatenation-equal-to-target.cc
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/number-of-pairs-of-strings-with-concatenation-equal-to-target/
+#include "xxx.h"
+
+class Solution {
+public:
+  int numOfPairs(vector<string> &nums, string target) {
+    int sz = nums.size();
+    int ret = 0;
+    for (int i = 0; i < sz; ++i) {
+      for (int j = 0; j < sz; ++j) {
+        if (i != j) {
+          if (nums[i].size() + nums[j].size() == target.size() &&
+              nums[i] + nums[j] == target) {
+            ret += 1;
+          }
+        }
+      }
+    }
+
+    return ret;
+  }
+};
