9a75d0713b30a9c53f56870b0c240badfe8dd242
rongyi <rongyi@onchain.com>
2022-01-11 09:00:36 +0800

    Update leetcode

---
+++ cc/count-number-of-maximum-bitwise-or-subsets.cc
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/count-number-of-maximum-bitwise-or-subsets/
+#include "xxx.h"
+
+class Solution {
+public:
+  int countMaxOrSubsets(vector<int> &nums) {
+    int maxor = 0;
+    for (auto num: nums) {
+      maxor |= num;
+    }
+    int sz = nums.size();
+    int total = 0;
+    // just using the mask, you think too much
+    for (int mask = 0; mask < (1 << sz); ++mask) {
+      int curor = 0;;
+      for (int i = 0; i < sz; ++i) {
+        if ((mask >> i) & 1) {
+          curor |= nums[i];
+        }
+      }
+      if (curor == maxor) {
+        total++;
+      }
+    }
+    return total;
+  }
+};
---
+++ cc/simple-bank-system.cc
@@ -0,0 +1,64 @@
+// https://leetcode.com/problems/simple-bank-system/
+#include "xxx.h"
+
+class Bank {
+public:
+  Bank(vector<long long> &balance) {
+    acts_ = balance;
+    sz_ = acts_.size();
+  }
+
+  bool transfer(int account1, int account2, long long money) {
+    if (!validAccount(account1) || !validAccount(account2)) {
+      return false;
+    }
+    // insuffcient
+    if (acts_[account1 - 1] < money) {
+      return false;
+    }
+    acts_[account1 - 1] -= money;
+    acts_[account2 - 1] += money;
+    return true;
+  }
+
+  bool deposit(int account, long long money) {
+    if (!validAccount(account)) {
+      return false;
+    }
+    acts_[account - 1] += money;
+    return true;
+  }
+
+  bool withdraw(int account, long long money) {
+    if (!validAccount(account)) {
+      return false;
+    }
+
+    if (acts_[account - 1] < money) {
+      return false;
+    }
+    acts_[account - 1] -= money;
+
+    return true;
+  }
+
+private:
+  bool validAccount(int index) {
+    if (index < 1 || index > sz_) {
+      return false;
+    }
+    return true;
+  }
+
+private:
+  vector<long long> acts_;
+  int sz_;
+};
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank* obj = new Bank(balance);
+ * bool param_1 = obj->transfer(account1,account2,money);
+ * bool param_2 = obj->deposit(account,money);
+ * bool param_3 = obj->withdraw(account,money);
+ */
