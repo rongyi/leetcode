0ce25e390c92fe185f22bfe64fc6f91b23c9a728
rongyi <rongyi@onchain.com>
2022-01-16 09:03:44 +0800

    Update leetcode

---
+++ cc/kth-distinct-string-in-an-array.cc
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/kth-distinct-string-in-an-array/
+#include "xxx.h"
+
+class Solution {
+public:
+  string kthDistinct(vector<string> &arr, int k) {
+    map<string, int> cnt;
+    for (auto &s: arr) {
+      cnt[s]++;
+    }
+    int idx = 0;
+    for (int i = 0; i < arr.size(); ++i) {
+      if (cnt[arr[i]] == 1) {
+        idx++;
+        if (idx == k) {
+          return arr[i];
+        }
+      }
+    }
+    return "";
+  }
+};
