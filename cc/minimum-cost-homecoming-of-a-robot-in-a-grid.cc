57d551c255731c119e63b1cf844cfc7978949336
rongyi <rongyi@onchain.com>
2022-02-14 19:00:46 +0800

    Update leetcode

---
+++ cc/minimum-cost-homecoming-of-a-robot-in-a-grid.cc
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/minimum-cost-homecoming-of-a-robot-in-a-grid/
+#include "xxx.hpp"
+
+class Solution {
+public:
+  int minCost(vector<int> &startPos, vector<int> &homePos,
+              vector<int> &rowCosts, vector<int> &colCosts) {
+    int ret = 0;
+    int dr = (startPos[0] < homePos[0]) ? 1 : -1;
+    int dc = (startPos[1] < homePos[1]) ? 1 : -1;
+    for (int r = startPos[0]; r != homePos[0]; r += dr) {
+      ret += rowCosts[r + dr];
+    }
+    for (int c = startPos[1]; c != homePos[1]; c += dc) {
+      ret += colCosts[c + dc];
+    }
+
+    return ret;
+  }
+};
