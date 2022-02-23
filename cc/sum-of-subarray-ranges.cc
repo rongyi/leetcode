064c64fc9d1d02a52e99a3743bb2e56dda7275e2
rongyi <rongyi@onchain.com>
2022-02-23 22:18:55 +0800

    Update leetcode

--- cc/sum-of-subarray-ranges.cc
+++ cc/sum-of-subarray-ranges.cc
@@ -3,7 +3,21 @@
 
 class Solution {
 public:
+  // O(n^2) can pass
   long long subArrayRanges(vector<int> &nums) {
+    int sz = nums.size();
+    long long sum = 0;
 
+    for (int i = 0; i < sz; ++i) {
+      int cur_max = nums[i];
+      int cur_min = nums[i];
+      for (int j = i + 1; j < sz; ++j) {
+        cur_max = max(cur_max, nums[j]);
+        cur_min = min(cur_min, nums[j]);
+        sum += (cur_max - cur_min);
+      }
+    }
+
+    return sum;
   }
 };
