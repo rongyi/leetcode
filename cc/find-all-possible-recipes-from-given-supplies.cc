1d4e8535e3ba9d64a8f85548c0cf075bf08a6965
rongyi <rongyi@onchain.com>
2022-03-03 07:10:45 +0800

    Update leetcode

--- cc/find-all-possible-recipes-from-given-supplies.cc
+++ cc/find-all-possible-recipes-from-given-supplies.cc
@@ -1,6 +1,50 @@
 // https://leetcode.com/problems/find-all-possible-recipes-from-given-supplies/
 #include "xxx.hpp"
 
+class Solution {
+public:
+  vector<string> findAllRecipes(vector<string> &recipes,
+                                vector<vector<string>> &ingredients,
+                                vector<string> &supplies) {
+    int sz = recipes.size();
+    unordered_set<string> spls(supplies.begin(), supplies.end());
+    vector<int> indegree(sz, 0);
+    unordered_map<string, vector<int>> neibs;
+    vector<string> ret;
+
+    for (int i = 0; i < sz; ++i) {
+      for (auto &grid : ingredients[i]) {
+        // needed ingredient not in supplies so we mark the needed relationship
+        if (spls.find(grid) == spls.end()) {
+          indegree[i]++;
+          // when grid is ready, see if we can "wake up" those recipes
+          neibs[grid].push_back(i);
+        }
+      }
+    }
+    queue<string> q;
+    for (int i = 0; i < sz; ++i) {
+      if (indegree[i] == 0) {
+        q.push(recipes[i]);
+      }
+    }
+    while (!q.empty()) {
+      auto cur = q.front();
+      q.pop();
+      ret.push_back(cur);
+      for (auto &wait_recipe : neibs[cur]) {
+        indegree[wait_recipe]--;
+        // wake up?
+        if (indegree[wait_recipe] == 0) {
+          q.push(recipes[wait_recipe]);
+        }
+      }
+    }
+
+    return ret;
+  }
+};
+
 class SolutionBruteForce {
 public:
   vector<string> findAllRecipes(vector<string> &recipes,
