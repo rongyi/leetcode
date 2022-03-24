e4b83fa51e3bbb1b31c6d6b646e0c285f4b53842
rongyi <rongyi@onchain.com>
2022-03-25 07:53:34 +0800

    Update leetcode

---
+++ cc/find-three-consecutive-integers-that-sum-to-a-given-number.cc
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/find-three-consecutive-integers-that-sum-to-a-given-number/
+#include "xxx.hpp"
+
+class Solution {
+public:
+  vector<long long> sumOfThree(long long num) {
+    if (num % 3 != 0) {
+      return {};
+    }
+    long long mid = num / 3;
+
+    return {mid - 1, mid, mid + 1};
+  }
+};
