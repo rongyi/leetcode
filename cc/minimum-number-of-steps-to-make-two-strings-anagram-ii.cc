d80b485f6f14c2081cf75dcea11c63c172d3a508
rongyi <rongyi@onchain.com>
2022-03-29 07:39:32 +0800

    Update leetcode
    Locked in a room for about 10 days, Shanghai 2022 is a tough year

--- cc/minimum-number-of-steps-to-make-two-strings-anagram-ii.cc
+++ cc/minimum-number-of-steps-to-make-two-strings-anagram-ii.cc
@@ -7,16 +7,19 @@ public:
     // just summary the diff
     vector<int> cnt1(26, 0);
     vector<int> cnt2(26, 0);
+
     for (auto c : s) {
       cnt1[c - 'a']++;
     }
     for (auto c : t) {
       cnt2[c - 'a']++;
     }
+
     int ret = 0;
     for (int i = 0; i < 26; ++i) {
       ret += abs(cnt1[i] - cnt2[i]);
     }
+
     return ret;
   }
 };
