3b9ca56614cc586121c813c2d2266d14f8bce169
rongyi <rongyi@onchain.com>
2022-04-30 14:41:12 +0800

    Add empty

---
+++ cc/encrypt-and-decrypt-strings.cc
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/encrypt-and-decrypt-strings/
+#include "xxx.hpp"
+
+class Encrypter {
+public:
+  Encrypter(vector<char> &keys, vector<string> &values,
+            vector<string> &dictionary) {}
+
+  string encrypt(string word1) {}
+
+  int decrypt(string word2) {}
+};
+
+/**
+ * Your Encrypter object will be instantiated and called as such:
+ * Encrypter* obj = new Encrypter(keys, values, dictionary);
+ * string param_1 = obj->encrypt(word1);
+ * int param_2 = obj->decrypt(word2);
+ */
