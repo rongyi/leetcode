dba2bdcf042fee1244ec50edfc3eb99901ef0279
rongyi <rongyi@onchain.com>
2022-09-18 10:59:56 +0800

    Update leetcode

--- cc/find-subarrays-with-equal-sum.cc
+++ cc/find-subarrays-with-equal-sum.cc
@@ -4,6 +4,23 @@
 class Solution {
 public:
   bool findSubarrays(vector<int> &nums) {
+    int sz = nums.size();
+    long long sum = 0;
+    map<long long, int> cnt;
 
+    for (int i = 0; i < sz; ++i) {
+      sum += nums[i];
+      if (i >= 2) {
+        sum -= nums[i - 2];
+      }
+      if (i >= 1) {
+        cnt[sum]++;
+        if (cnt[sum] == 2) {
+          return true;
+        }
+      }
+    }
+
+    return false;
   }
 };
