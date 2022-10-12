e243590049d4b8b2e38bdeaa589a2853d8543c62
rongyi <rongyi@onchain.com>
2022-10-12 20:45:28 +0800

    Update code

--- cc/maximum-number-of-robots-within-budget.cc
+++ cc/maximum-number-of-robots-within-budget.cc
@@ -4,7 +4,26 @@
 class Solution {
 public:
   int maximumRobots(vector<int> &chargeTimes, vector<int> &runningCosts,
-      long long budget) {
-
+                    long long budget) {
+    deque<int> q;
+    int i = 0;
+    int j = 0;
+    int sz = chargeTimes.size();
+    long long cost = 0;
+    for (; i < sz; ++i) {
+      cost += runningCosts[i];
+      while (!q.empty() && q.back() < chargeTimes[i]) {
+        q.pop_back();
+      }
+      q.push_back(chargeTimes[i]);
+      if (cost * (i - j + 1) + q.front() > budget) {
+        cost -= runningCosts[j];
+        if (q.front() == chargeTimes[j]) {
+          q.pop_front();
+        }
+        j++;
+      }
+    }
+    return sz - j;
   }
 };
