9f70a2e7dda4c1af2be610281a10bd45eaa98a3f
rongyi <rongyi@onchain.com>
2022-12-15 16:58:36 +0800

    Update leetcode

--- cc/minimum-fuel-cost-to-report-to-the-capital.cc
+++ cc/minimum-fuel-cost-to-report-to-the-capital.cc
@@ -21,6 +21,22 @@ public:
     }
     long long fuels = 0;
     for (int i = 1; i < sz; ++i) {
+      // wtf is this?
+      // childres[i] means this subtree(including root) number
+      // so for all nodes except node 0, we count how many cars we need
+      // for current subtree, those cars will cost 1 * cars fuels to uppper node
+      // and then for every upper node, we count how many cars we need(including
+      // the subtree car) and to move upper layer, also cost (1 * cars) fuels
+      // 0----1-----3
+      // |    \
+      // 5     4
+      //        \
+      //         2
+      // seats = 3
+      // childrens[2] = 1 -> one car
+      // ...
+      // childres[1] = 4 -> need two car, so from node 1 to capital 0 this step
+      // need 2 fuels
       fuels += (childrens[i] + seats - 1) / seats;
     }
 
