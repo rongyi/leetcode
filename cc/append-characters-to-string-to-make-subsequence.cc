772990c73103b3e0bb447f56d6a34207ce26d7a6
rongyi <rongyi@onchain.com>
2022-12-19 07:18:55 +0800

    Update leetcode

---
+++ cc/append-characters-to-string-to-make-subsequence.cc
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/append-characters-to-string-to-make-subsequence/
+#include "xxx.hpp"
+
+class Solution {
+public:
+  int appendCharacters(string s, string t) {
+    int sz1 = s.size();
+    int sz2 = t.size();
+    int j = 0;
+    // just go for the subsequence
+    for (int i = 0; i < sz1 && j < sz2; i++) {
+      if (s[i] == t[j]) {
+        j++;
+      }
+    }
+
+    // what left is we need to add
+    return sz2 - j;
+  }
+};
