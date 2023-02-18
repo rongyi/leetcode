915143fa22582410f6f6b5913097a62cb3088e4a
rongyi <rongyi@onchain.com>
2023-02-18 17:41:16 +0800

    Update leetcode

--- cc/unique-paths-ii.cc
+++ cc/unique-paths-ii.cc
@@ -3,33 +3,40 @@
 
 class Solution {
 public:
-  int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid) {
-    const int m = obstacleGrid.size();
-    const int n = obstacleGrid[0].size();
-
-    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-    if (obstacleGrid[0][0] == 0) {
-      dp[1][1] = 1;
+  int uniquePathsWithObstacles(vector<vector<int>> &obs) {
+    int m = obs.size();
+    int n = obs[0].size();
+    vector<vector<long long>> dp(m, vector<long long>(n, 0));
+    for (int i = 0; i < m; ++i) {
+      if (obs[i][0] == 0) {
+        dp[i][0] = 1;
+      } else {
+        break;
+      }
     }
-
-    for (int i = 1; i <= m; ++i) {
-      for (int j = 1; j <= n; ++j) {
-        if (dp[i - 1][j] && obstacleGrid[i - 1][j - 1] == 0) {
-          dp[i][j] += dp[i - 1][j];
+    for (int j = 0; j < n; ++j) {
+      if (obs[0][j] == 0) {
+        dp[0][j] = 1;
+      } else {
+        // can not go any futhre
+        break;
+      }
+    }
+    for (int i = 1; i < m; ++i) {
+      for (int j = 1; j < n; ++j) {
+        // ignore update obstacle
+        if (obs[i][j] == 1) {
+          continue;
         }
-        if (dp[i][j - 1] && obstacleGrid[i - 1][j - 1] == 0) {
+        if (obs[i][j - 1] == 0) {
           dp[i][j] += dp[i][j - 1];
+        }
+        if (obs[i - 1][j] == 0) {
+          dp[i][j] += dp[i - 1][j];
         }
       }
     }
-
-    return dp[m][n];
+    return dp[m - 1][n - 1];
   }
 };
 
-int main() {
-  Solution so;
-  vector<vector<int>> input{{0, 0}};
-  auto ret = so.uniquePathsWithObstacles(input);
-  cout << ret << endl;
-}
