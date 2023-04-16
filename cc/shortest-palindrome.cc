e7a258bc8c84adb13891273892b61bcdc5e28ced
rongyi <rongyi@onchain.com>
2023-04-16 20:49:36 +0800

    Update leetcode

--- cc/shortest-palindrome.cc
+++ cc/shortest-palindrome.cc
@@ -6,13 +6,16 @@ public:
   string shortestPalindrome(string s) {
     string rev_s = s;
     reverse(rev_s.begin(), rev_s.end());
+    // find palindrome start at index 0
     string l = s + "#" + rev_s;
 
     vector<int> p(l.size(), 0);
     for (int i = 1; i < l.size(); i++) {
       int j = p[i - 1];
-      while (j > 0 && l[i] != l[j])
+      // just fallback again and again
+      while (j > 0 && l[i] != l[j]) {
         j = p[j - 1];
+      }
       p[i] = (j += l[i] == l[j]);
     }
 
