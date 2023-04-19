635c375e1e0acc9e005b16bf966a1ca796702188
rongyi <rongyi@onchain.com>
2023-04-19 19:30:11 +0800

    Update leetcode

--- cc/the-skyline-problem.cc
+++ cc/the-skyline-problem.cc
@@ -1,63 +1,41 @@
 // http://leetcode.com/problems/the-skyline-problem/description/
 #include "xxx.hpp"
 
-// taken from
-// https://leetcode.com/problems/the-skyline-problem/discuss/154793/Short-documented-C%2B%2B-code-(16ms-runtime)
 class Solution {
 public:
-  vector<pair<int, int>> getSkyline(vector<vector<int>> &buildings) {
-    // the skyline can only change at the critical points which are either the
-    // beginning or the end of a building
-    set<int> critical;
-    for (auto cur_build : buildings) {
-      critical.insert(cur_build[0]);
-      critical.insert(cur_build[1]);
+  vector<vector<int>> getSkyline(vector<vector<int>> &buildings) {
+    // for staring, it is (xpos, -height) (-height to make sure higher
+    // score is before the lower height in sort)
+    vector<pair<int, int>> critical_lines;
+    for (auto &b : buildings) {
+      // e.g. {3, 4} {3, 10}
+      // the normal sort will be {3, 4}, {3, 10}
+      // but what we want is {3, 10} {3, 4}
+      // so we make second pos negative
+      critical_lines.push_back({b[0], -b[2]});
+      critical_lines.push_back({b[1], b[2]});
     }
-
-    vector<pair<int, int>> skyline;
-
-    int last_height = 0;
-
-    priority_queue<pair<int, int>> active;
-
-    auto cur_build = buildings.begin();
-
-    for (int crit : critical) {
-      // any building that started on or before the critical point could be
-      // active at the critical point
-      while (cur_build != buildings.end() && cur_build->at(0) <= crit) {
-        active.push({cur_build->at(2), cur_build->at(1)});
-        cur_build++;
+    sort(critical_lines.begin(), critical_lines.end());
+    // make sure the last line get height 0, when leaving
+    multiset<int> heights{0};
+    // point pos
+    vector<vector<int>> ret;
+    int prev_height = 0;
+    for (auto &line : critical_lines) {
+      if (line.second < 0) {
+        heights.insert(-line.second);
+      } else {
+        // this line is leaving
+        heights.erase(heights.find(line.second));
       }
-
-      // any building that ends or or before the critical point is not active at
-      // the critical point
-      while (!active.empty() && active.top().second <= crit)
-        active.pop();
-
-      // the height at the critical point is simply the tallest active building
-      // note: no active building implies we are at the right edge of a building
-      //       hence height is zero.
-      int height = 0;
-      if (!active.empty())
-        height = active.top().first;
-
-      // the skyline only changes when the height at a critical point changes
-      if (height != last_height) {
-        skyline.push_back({crit, height});
-        last_height = height;
+      // choose the highest building, all others are shadowed
+      int cur_height = *heights.rbegin();
+      if (cur_height != prev_height) {
+        ret.push_back({line.first, cur_height});
+        prev_height = cur_height;
       }
     }
 
-    return skyline;
+    return ret;
   }
 };
-
-int main() {
-  Solution so;
-  vector<vector<int>> input{{2, 9, 10}, {3, 7, 15}, {5, 12, 12}, {15, 20, 10}, {19, 24, 8}};
-  auto ret = so.getSkyline(input);
-  for (auto kv : ret) {
-    cout << kv.first << " . " << kv.second << endl;
-  }
-}
