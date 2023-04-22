26ecfd1164b8ee3a09c3ee18633e4905bebd0748
rongyi <rongyi@onchain.com>
2023-04-22 11:36:36 +0800

    Update leetcode

--- cc/majority-element-ii.cc
+++ cc/majority-element-ii.cc
@@ -4,53 +4,18 @@
 class Solution {
 public:
   vector<int> majorityElement(vector<int> &nums) {
-    vector<int> ret{};
-    if (nums.empty()) {
-      return ret;
-    }
-    int candi1 = 0;
-    int candi2 = 0;
-    int count1 = 0;
-    int count2 = 0;
-    for (auto i : nums) {
-      if (i == candi1) {
-        count1++;
-        continue;
+    int sz = nums.size();
+    int cnt = sz / 3;
+    sort(nums.begin(), nums.end());
+    vector<int> ret;
+    // make code more shorter
+    for (auto it = nums.begin(); it != nums.end();) {
+      auto up = upper_bound(nums.begin(), nums.end(), *it);
+      auto d = distance(it, up);
+      if (d > cnt) {
+        ret.push_back(*it);
       }
-      if (i == candi2) {
-        count2++;
-        continue;
-      }
-      if (count1 == 0) {
-        candi1 = i;
-        count1++;
-        continue;
-      }
-      if (count2 == 0) {
-        candi2 = i;
-        count2++;
-        continue;
-      }
-
-      count1--;
-      count2--;
-    }
-
-    // check again
-    count1 = 0;
-    count2 = 0;
-    for (auto i : nums) {
-      if (i == candi1) {
-        count1++;
-      } else if (i == candi2) {
-        count2++;
-      }
-    }
-    if (count1 > nums.size() / 3) {
-      ret.push_back(candi1);
-    }
-    if (count2 > nums.size() / 3) {
-      ret.push_back(candi2);
+      it = up;
     }
 
     return ret;
