aeb34d0fff4e4af82997fe163ee6b4cb2ea259c0
rongyi <rongyi@onchain.com>
2023-04-25 06:17:28 +0800

    Update leetcode

--- cc/product-of-array-except-self.cc
+++ cc/product-of-array-except-self.cc
@@ -4,28 +4,21 @@
 class Solution {
 public:
   vector<int> productExceptSelf(vector<int> &nums) {
-    // 构造两个数组，分别缓存从左边s开会到左边隔壁的成绩，
-    // 和从最后一个元素开始到隔壁右边的成绩
-    // 这样要求的就是这两个数组乘积
-    const int n = nums.size();
+    int sz = nums.size();
+    vector<int> ret(sz, 1);
+    int lacc = nums[0];
+    int racc = nums[sz - 1];
 
-    vector<long long> left(n, 0);
-    left[0] = 1;
-    for (int i = 1; i < n; i++) {
-      left[i] = left[i - 1] * nums[i - 1];
+    for (int i = 1; i < sz; i++) {
+      ret[i] *= lacc;
+      lacc *= nums[i];
     }
-
-    vector<long long> right(n, 0);
-    right[n - 1] = 1;
-    for (int i = n - 2; i >= 0; i--) {
-      right[i] = right[i + 1] * nums[i + 1];
-    }
-
-    vector<int> ret(n, 0);
-    for (int i = 0; i < n; i++) {
-      ret[i] = left[i] * right[i];
+    for (int i = sz - 2; i >= 0; i--) {
+      ret[i] *= racc;
+      racc *= nums[i];
     }
 
     return ret;
   }
 };
+
