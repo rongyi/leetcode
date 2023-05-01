c38e7fc18f86ce9580e59940d31be91fa028a36c
rongyi <rongyi@onchain.com>
2023-05-01 17:18:43 +0800

    Update leetcode

--- cc/find-median-from-data-stream.cc
+++ cc/find-median-from-data-stream.cc
@@ -3,46 +3,40 @@
 
 class MedianFinder {
 public:
-  /** initialize your data structure here. */
   MedianFinder() {}
 
   void addNum(int num) {
-    // max_heap 放小值，这样top出来的是一堆矮的里面挑高的
-    if (max_heap.empty() || num <= max_heap.top()) {
-      max_heap.push(num);
+    if (max_queue_.empty() || num <= max_queue_.top()) {
+      max_queue_.push(num);
     } else {
-      min_heap.push(num);
+      min_queue_.push(num);
     }
-    // max_heap 可以多一个，但不能太多
-    if (max_heap.size() > min_heap.size() + 1) {
-      min_heap.push(max_heap.top());
-      max_heap.pop();
-    } else if (min_heap.size() > max_heap.size()) {
-      max_heap.push(min_heap.top());
-      min_heap.pop();
+
+    if (max_queue_.size() > min_queue_.size() + 1) {
+      min_queue_.push(max_queue_.top());
+      max_queue_.pop();
+    } else if (min_queue_.size() > max_queue_.size()) {
+      max_queue_.push(min_queue_.top());
+      min_queue_.pop();
     }
   }
 
   double findMedian() {
-    if (min_heap.size() == max_heap.size()) {
-      return (min_heap.top() + max_heap.top()) / 2.0;
+    if (max_queue_.size() == min_queue_.size()) {
+      return (max_queue_.top() + min_queue_.top()) / 2.0;
     }
 
-    return max_heap.top();
+    return max_queue_.top();
   }
 
 private:
-  // 放的都是比较大的数,高的里面挑矮的
-  priority_queue<int, vector<int>, greater<int>> min_heap;
-  // 放的都是比较小的数，这样综合起来，矮的里面挑高的，
-  // 高的里面挑矮的，挑出来的就是中间的那些高度
-  // 否则如果大顶堆放大的数，小顶堆
-  priority_queue<int, vector<int>, less<int>> max_heap;
+  priority_queue<int> max_queue_;
+  priority_queue<int, vector<int>, greater<int>> min_queue_;
 };
 
 /**
  * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder obj = new MedianFinder();
- * obj.addNum(num);
- * double param_2 = obj.findMedian();
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
  */
