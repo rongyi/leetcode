028f25c6557889b5814599622eda4e5a6bf7077a
rongyi <rongyi@onchain.com>
2023-05-05 08:24:14 +0800

    Update leetcode

--- cc/longest-increasing-subsequence.cc
+++ cc/longest-increasing-subsequence.cc
@@ -4,24 +4,20 @@
 class Solution {
 public:
   int lengthOfLIS(vector<int> &nums) {
-    const int n = nums.size();
-    if (n == 0) {
-      return 0;
-    }
-    // dp[i]表示到节点i为止时的最长递增序列
-    // 那么要么这个序列为1， ==> 前面找不到比当前节点还小的
-    // 要么在前面找到一个值，这个值比当前值小，那么 此时有dp[i] == dp[j] + 1(假设前面找到的那个点为j)
-    // 如果在前面找到多个比当前值小呢？ 那就都算一遍找最大值咯，这样算出来就是最大啦
-    vector<int> dp(n, 1);
-
-    for (int k = 0; k < n; k++) {
-      for (int i = 0; i < k; i++) {
-        if (nums[i] < nums[k]) {
-          dp[k] = max(dp[k], dp[i] + 1);
+    int sz = nums.size();
+    vector<int> dp(sz, 1);
+    int ret = 1;
+    for (int j = 1; j < sz; j++) {
+      for (int i = 0; i < j; i++) {
+        // for every element, if we find one smaller before
+        // then we can construct one more lis
+        if (nums[j] > nums[i]) {
+          dp[j] = max(dp[j], 1 + dp[i]);
+          ret = max(ret, dp[j]);
         }
       }
     }
 
-    return *max_element(dp.begin(), dp.end());
+    return ret;
   }
 };
--- cc/remove-invalid-parentheses.cc
+++ cc/remove-invalid-parentheses.cc
@@ -5,49 +5,50 @@ class Solution {
 public:
   vector<string> removeInvalidParentheses(string s) {
     vector<string> ret;
-    remove(s, 0, 0, '(', ')', ret);
+    // edited string and last modified index
+    queue<pair<string, int>> q;
+    q.push({s, 0});
+
+    while (!q.empty()) {
+      auto [cur, idx] = q.front();
+      q.pop();
+      if (isValid(cur)) {
+        ret.push_back(cur);
+      } else if (ret.empty()) {
+        // dedup, we can use a set or
+        // make deletion in a uniq way
+        // 1. delete with order
+        // 2. same paren, only delete the first one e.g.
+        //    (((, we don't delete for the second and third '('
+        for (int i = idx; i < cur.size(); i++) {
+          if ((cur[i] == ')' || cur[i] == '(') &&
+              (i == idx || cur[i] != cur[i - 1])) {
+            q.push({cur.substr(0, i) + cur.substr(i + 1), i});
+          }
+        }
+      }
+    }
 
     return ret;
   }
+
 private:
-  void remove(string s, int last_valid, int last_remove, const char open, const char close, vector<string> &ret) {
-    int paren_count = 0;
-    for (int i = last_valid; i < s.size(); i++) {
-      if (s[i] == open) {
-        paren_count++;
+  bool isValid(string &s) {
+    int cnt = 0;
+    for (auto &c : s) {
+      if (c == '(') {
+        cnt++;
       }
-      if (s[i] == close) {
-        paren_count--;
-      }
-      if (paren_count < 0) {
-        for (int j = last_remove; j <= i; ++j) {
-          if (s[j] == close && (j == last_remove || s[j - 1] != close)) {
-            remove(s.substr(0, j) + s.substr(j + 1), i, j, open, close, ret);
-          }
+      if (c == ')') {
+        if (cnt > 0) {
+          cnt--;
+        } else {
+          return false;
         }
-        return;
       }
     }
 
-    // if we reach here, current s is valid without any fix.
-    // But each s should go through two pass, the second pass is for reversed string.
-    // so we need to check if this is the first pass, if yes, reverse s and check again
-    // if this is second pass, just save result
-    // how to check if it's first pass? use the order of open/close Parentheses as flag
-    if (open == '(') {
-      reverse(s.begin(), s.end());
-      remove(s, 0, 0, ')', '(', ret);
-    } else {
-      reverse(s.begin(), s.end());
-      ret.push_back(s);
-    }
+    return cnt == 0;
   }
 };
 
-int main() {
-  Solution so;
-  auto ret = so.removeInvalidParentheses("()())()");
-  for (auto s : ret) {
-    cout << s << endl;
-  }
-}
