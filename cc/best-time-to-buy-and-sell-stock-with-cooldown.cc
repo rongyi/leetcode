6e2330d438ef9af12e3f4da18be9a31e302aa717
rongyi <rongyi@onchain.com>
2023-05-10 20:00:43 +0800

    Update leetcode

--- cc/best-time-to-buy-and-sell-stock-with-cooldown.cc
+++ cc/best-time-to-buy-and-sell-stock-with-cooldown.cc
@@ -3,57 +3,27 @@
 
 class Solution {
 public:
-  // https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/discuss/75931/Easiest-JAVA-solution-with-explanations
-  // 1. Define States
-
-  // To represent the decision at index i:
-
-  // buy[i]: Max profit till index i. The series of transaction is ending with a buy.
-  // sell[i]: Max profit till index i. The series of transaction is ending with a sell.
-  // To clarify:
-
-  // Till index i, the buy / sell action must happen and must be the last action. It may not happen at index i. It may happen at i - 1, i - 2, ... 0.
-  // In the end n - 1, return sell[n - 1]. Apparently we cannot finally end up with a buy. In that case, we would rather take a rest at n - 1.
-  // For special case no transaction at all, classify it as sell[i], so that in the end, we can still return sell[n - 1]. Thanks @alex153 @kennethliaoke @anshu2.
-  // 2. Define Recursion
-
-  // buy[i]: To make a decision whether to buy at i, we either take a rest, by just using the old decision at i - 1, or sell at/before i - 2, then buy at i, We cannot sell at i - 1, then buy at i, because of cooldown.
-  // sell[i]: To make a decision whether to sell at i, we either take a rest, by just using the old decision at i - 1, or buy at/before i - 1, then sell at i.
-  // So we get the following formula:
-
-  // buy[i] = Math.max(buy[i - 1], sell[i - 2] - prices[i]);
-  // sell[i] = Math.max(sell[i - 1], buy[i - 1] + prices[i]);
-  // 3. Optimize to O(1) Space
-
-  // DP solution only depending on i - 1 and i - 2 can be optimized using O(1) space.
-
-  // Let b2, b1, b0 represent buy[i - 2], buy[i - 1], buy[i]
-  // Let s2, s1, s0 represent sell[i - 2], sell[i - 1], sell[i]
-  // Then arrays turn into Fibonacci like recursion:
-
-  // b0 = Math.max(b1, s2 - prices[i]);
-  // s0 = Math.max(s1, b1 + prices[i]);
-  // 4. Write Code in 5 Minutes
-
-  // First we define the initial states at i = 0:
-
-  // We can buy. The max profit at i = 0 ending with a buy is -prices[0].
-  // We cannot sell. The max profit at i = 0 ending with a sell is 0.
   int maxProfit(vector<int> &prices) {
-    if (prices.size() <= 1) {
+    int sz = prices.size();
+    if (sz <= 1) {
       return 0;
     }
-    int b0 = -prices[0];
-    int b1 = b0;
-    int s0 = 0, s1 = 0, s2 = 0;
+    // profit for last state is cooldown, buy or sell at index i
+    vector<int> cooldown(sz, 0);
+    vector<int> buy(sz, 0);
+    vector<int> sell(sz, 0);
 
-    for (int i = 1; i < prices.size(); i++) {
-      b0 = max(b1, s2 - prices[i]);
-      s0 = max(s1, b1 + prices[i]);
-      b1 = b0;
-      s2 = s1;
-      s1 = s0;
+    buy[0] = -prices[0];
+    sell[0] = numeric_limits<int>::min();
+
+    for (int i = 1; i < sz; i++) {
+      cooldown[i] = max(cooldown[i - 1], sell[i - 1]);
+      // buy one from cooldown
+      buy[i] = max(buy[i - 1], cooldown[i - 1] - prices[i]);
+      sell[i] = buy[i - 1] + prices[i];
     }
-    return s0;
+
+    return max(cooldown[sz - 1], sell[sz - 1]);
   }
 };
+
