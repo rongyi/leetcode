862be6bb62806f792312b9544279a2d203761322
rongyi <rongyi@onchain.com>
2023-05-14 10:29:04 +0800

    Update leetcode

--- cc/count-of-smaller-numbers-after-self.cc
+++ cc/count-of-smaller-numbers-after-self.cc
@@ -4,6 +4,55 @@
 class Solution {
 public:
   vector<int> countSmaller(vector<int> &nums) {
+    int sz = nums.size();
+
+    vector<int> index(sz, 0);
+    iota(index.begin(), index.end(), 0);
+
+    vector<int> ret(sz, 0);
+    mergesort(nums, 0, sz, index, ret);
+
+    return ret;
+  }
+
+private:
+  void mergesort(vector<int> &nums, int start, int end, vector<int> &index,
+                 vector<int> &ret) {
+    int len = end - start;
+    if (len <= 1) {
+      return;
+    }
+    int step = len / 2;
+    int mid = start + step;
+    mergesort(nums, start, mid, index, ret);
+    mergesort(nums, mid, end, index, ret);
+
+    vector<int> tmp_idx;
+    tmp_idx.reserve(len);
+    int idx1 = start;
+    int idx2 = mid;
+    int low_cnt = 0;
+
+    while (idx1 < mid || idx2 < end) {
+      if (idx2 == end ||
+          (idx1 < mid && nums[index[idx1]] <= nums[index[idx2]])) {
+        tmp_idx.push_back(index[idx1]);
+        ret[index[idx1]] += low_cnt;
+        ++idx1;
+      } else {
+        tmp_idx.push_back(index[idx2]);
+        ++low_cnt;
+        ++idx2;
+      }
+    }
+
+    move(tmp_idx.begin(), tmp_idx.end(), index.begin() + start);
+  }
+};
+
+class SolutionTLE {
+public:
+  vector<int> countSmaller(vector<int> &nums) {
     const int n = nums.size();
     if (n == 0) {
       return vector<int>{};
