fd2111193f4cd7fab09832dd18118fe4dccda431
rongyi <rongyi@onchain.com>
2024-02-29 21:14:46 +0800

    Update

--- cc/battleships-in-a-board.cc
+++ cc/battleships-in-a-board.cc
@@ -6,56 +6,17 @@ public:
   int countBattleships(vector<vector<char>> &board) {
     int ret = 0;
     const int m = board.size();
-    if (m == 0) {
-      return ret;
-    }
     const int n = board[0].size();
-    if (n == 0) {
-      return ret;
-    }
     for (int i = 0; i < m; ++i) {
       for (int j = 0; j < n; ++j) {
-        if (board[i][j] == 'X') {
+        if (board[i][j] == 'X' && ((i == 0 || board[i - 1][j] == '.') &&
+                                   (j == 0 || board[i][j - 1] == '.'))) {
           ++ret;
-          reset(board, i, j);
         }
       }
     }
 
     return ret;
-  }
-private:
-  // reset reset all column and row to dot
-  void reset(vector<vector<char>> &board, int x, int y) {
-    const int m = board.size();
-    const int n = board[0].size();
-    // reset should start from this point
-    int i = x;
-    // down
-    while (i < m && board[i][y] == 'X') {
-      board[i][y] = '.';
-      i++;
-    }
-    // up
-    i = x - 1;
-    while (i >= 0 && board[i][y] == 'X') {
-      board[i][y] = '.';
-      i--;
-    }
-
-    // right
-    // jump the x, y, it's already been set
-    int j = y + 1;
-    while (j < n && board[x][j] == 'X') {
-      board[x][j] = '.';
-      j++;
-    }
-    // left
-    j = y - 1;
-    while (j >= 0 && board[x][j] == 'X') {
-      board[x][j] = '.';
-      j--;
-    }
   }
 };
 
