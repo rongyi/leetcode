ad0c8dd70fbe42b08a56308c5825d02ad74f460e
rongyi <rongyi@onchain.com>
2024-03-09 09:32:44 +0800

    Make 466 pass again

--- cc/count-the-repetitions.cc
+++ cc/count-the-repetitions.cc
@@ -5,49 +5,29 @@ class Solution {
 public:
   // https://leetcode.com/problems/count-the-repetitions/discuss/119679/brutal-force-%2B-optimization-easy-understanding-c%2B%2B
   int getMaxRepetitions(string s1, int n1, string s2, int n2) {
-    // s2中找到一个不存在于s1中的字符，那么怎么也凑不起来
-    for (auto c : s2) {
-      if (s1.find(c) == string::npos) {
-        return 0;
-      }
-    }
+    int count1 = 0;
+    int count2 = 0;
+    int i = 0;
+    int j = 0;
+    const int len1 = s1.length();
+    const int len2 = s2.length();
 
-    const int s1len = s1.size();
-    const int s2len = s2.size();
-    int p1 = 0;
-    int p2 = 0;
-    int mark = 0;
-
-    while (p1 < s1len * n1) {
-      char c = s2[p2 % s2len];
-      // 找到相等的第一个字符
-      while (s1[p1 % s1len] != c && p1 < s1len * n1) {
-        p1++;
-      }
-      // 然后一起进步，检测下一个字符
-      p2++;
-      p1++;
-
-      // 完整匹配一轮s2
-      if (p2 % s2.length() == 0) {
-        // 第一次匹配OK的时候记录下此时p1中的位置
-        if (p2 == s2len) {
-          mark = p1;
-        } else if (p1 % s1len == mark % s1len) {
-          // p1又走到相同的相对位置了，说明此时产生了重复的规律
-          // p1 - mark是这段重复规律的长度
-          // s1.length() * n1是总长， 减去已经走掉的 p1
-          int round = (s1len * n1 - p1) / (p1 - mark);
-          //  直接飞过去
-          p1 += round * (p1 - mark);
-          // 生成mark那一轮，也即第一轮p1走到了mark,p2走到s2长度，所以要减掉
-          p2 += round * (p2 - s2len);
+    while (count1 < n1) {
+      if (s1[i] == s2[j]) {
+        j += 1;
+        if (j == len2) {
+          j = 0;
+          count2 += 1;
         }
       }
+      i += 1;
+      if (i == len1) {
+        i = 0;
+        count1 += 1;
+      }
     }
 
-    // 如果s2在S1中重复了R次，那么S2(s2重复n2次构成)则肯定重复了 R / n2 次。
-    return p2 / s2len / n2;
+    return count2 / n2;
   }
 };
 
