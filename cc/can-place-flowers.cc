c7188fb72982d5b491275af4e7c0275091a15258
rongyi <rongyi@onchain.com>
2024-03-30 11:12:38 +0800

    make 605 more concise

--- cc/can-place-flowers.cc
+++ cc/can-place-flowers.cc
@@ -4,39 +4,17 @@
 class Solution {
 public:
   bool canPlaceFlowers(vector<int> &flowerbed, int target) {
-    int plant = 0;
-    const int n = flowerbed.size();
-    int i = 0;
-    while (i < n) {
+    int cnt = 0;
+    for (int i = 0; i < flowerbed.size(); i++) {
       if (flowerbed[i] == 0) {
-        int j = i;
-        while (j < n && flowerbed[j] == 0) {
-          j++;
+        if ((i == 0 || flowerbed[i - 1] == 0) &&
+            (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0)) {
+          cnt += 1;
+          flowerbed[i] = 1;
         }
-        // flowerbed has no plant at all!
-        if (i == 0 && j == n) {
-          plant = (n + 1) / 2;
-          break;
-        }
-        // we have start partial plant
-        // 00001 or 10000 start or end
-        // 如上，我们有四个空位放花去掉与1相邻的地方不能放，我们有三个空位 j - i
-        // - 1 而三个不带限制条件所能放的最多的花的方法是 f(n) = (n + 1) / 2
-        // 代入(j - i - 1 + 1) / 2 ==> (j - i) / 2
-        if (i == 0 || j == n) {
-          plant += max(0, (j - i) / 2);
-        } else { // 分析同上,空位有 j - i - 2个
-          plant += max(0, (j - i - 1) / 2);
-        }
-
-        // we have middle partial plant
-        // we have end section partial plant
-        i = j;
-      } else {
-        i++;
       }
     }
-    return plant >= target;
+    return cnt >= target;
   }
 };
 
