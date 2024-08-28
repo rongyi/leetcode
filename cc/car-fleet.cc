3ab90d2c3aaa314d8dee60951f60e085e029799a
rongyi <sqrongyi@163.com>
2024-08-28 08:04:10 +0800

    Update cc

--- cc/car-fleet.cc
+++ cc/car-fleet.cc
@@ -6,11 +6,13 @@ public:
   int carFleet(int target, vector<int> &position, vector<int> &speed) {
     map<int, double> durations;
     for (int i = 0; i < position.size(); i++) {
+      // put smaller distance point in front
       durations[-position[i]] = (double)(target - position[i]) / speed[i];
     }
     int ret = 0;
     double cur = 0;
     for (auto kv : durations) {
+      // If a car takes longer to reach the target than the car in front of it, it forms a new fleet.
       if (kv.second > cur) {
         cur = kv.second;
         ret++;
--- cc/peak-index-in-a-mountain-array.cc
+++ cc/peak-index-in-a-mountain-array.cc
@@ -3,22 +3,20 @@
 #include "xxx.hpp"
 class Solution {
 public:
-  int peakIndexInMountainArray(vector<int> &A) {
-    const int n = A.size();
-    vector<int> lmax(n, -1);
-    vector<int> rmax(n, -1);
-    for (int i = 1; i < n; ++i) {
-      lmax[i] = max(lmax[i -1], A[i - 1]);
-    }
+  int peakIndexInMountainArray(vector<int> &arr) {
+    int left = 0;
+    int right = arr.size() - 1;
 
-    for (int i = n - 2; i >= 0; --i) {
-      rmax[i] = max(rmax[i + 1], A[i + 1]);
-    }
-    for (int i = 0; i < n; ++i) {
-      if (A[i] > lmax[i] && A[i] > rmax[i]) {
-        return i;
+    while (left < right) {
+      int mid = left + (right - left) / 2;
+
+      if (arr[mid] < arr[mid + 1]) {
+        left = mid + 1;
+      } else {
+        right = mid;
       }
     }
-    return -1;
+
+    return left;
   }
 };
