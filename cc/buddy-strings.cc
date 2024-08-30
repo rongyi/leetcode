71245158d67214732e49035b49a42c5551375714
rongyi <sqrongyi@163.com>
2024-08-30 14:58:07 +0800

    make code pass again

--- cc/buddy-strings.cc
+++ cc/buddy-strings.cc
@@ -1,58 +1,31 @@
 // https://leetcode.com/problems/buddy-strings/description/
 #include "xxx.hpp"
+
 class Solution {
 public:
-  bool buddyStrings(string A, string B) {
-    const int m = A.size();
-    const int n = B.size();
-    if (m != n) {
-      return false;
-    }
-    if (n < 2) {
+  bool buddyStrings(string s, string goal) {
+    if (s.size() != goal.size()) {
       return false;
     }
 
-    int first = -1;
-    int second = -1;
-    for (int i = 0; i < n; ++i) {
-      if (A[i] != B[i]) {
-        if (first == -1) {
-          first = i;
-        } else {
-          if (second == -1) {
-            second = i;
-            continue;
-          }
-          return false;
+    if (s == goal) {
+      unordered_set<int> seen;
+      for (auto c : s) {
+        if (!seen.insert(c).second) {
+          return true;
         }
       }
+      return false;
     }
-    // equal case, when equal, there must exist an even element
-    // so we can switch the even char
-    if (first == -1 || second == -1) {
-      // test even case
-      return has_even(A);
-    }
-
-    swap(A[first], A[second]);
-
-    return A == B;
-  }
-
-private:
-  bool has_even(const string &s) {
-    const int n = s.size();
-    unordered_map<char, int> cnt;
-    for (int i = 0; i < n; ++i) {
-      ++cnt[s[i]];
-    }
-    for (auto cur : cnt) {
-      if ((cur.second & 1) == 0) {
-        return true;
+    vector<pair<char, char>> diff;
+    for (int i = 0; i < s.length(); i++) {
+      if (s[i] != goal[i]) {
+        diff.push_back({s[i], goal[i]});
       }
     }
 
-    return false;
+    return diff.size() == 2 && diff[0].first == diff[1].second &&
+           diff[0].second == diff[1].first;
   }
 };
 
