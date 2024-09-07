c36d64b7d3520f11ab9bd1f9d823c6477242804a
rongyi <sqrongyi@163.com>
2024-09-08 07:38:17 +0800

    Update cc

--- cc/find-and-replace-pattern.cc
+++ cc/find-and-replace-pattern.cc
@@ -1,38 +1,37 @@
 // http://leetcode.com/problems/find-and-replace-pattern/description/
 #include "xxx.hpp"
+#include <unordered_map>
 
 class Solution {
 public:
   // order matters
   vector<string> findAndReplacePattern(vector<string> &words, string pattern) {
+    auto expect = normalize(pattern);
     vector<string> ret;
+
     for (auto &s : words) {
-      // 要找不到都找不到，找到还必须和之前配对的相等
-      unordered_map<char, char> permu;
-      unordered_set<char> valueset;
-      int i = 0;
-      for (i = 0; i < s.size(); i++) {
-        // 没找到
-        if (permu.find(s[i]) == permu.end()) {
-          if (valueset.find(pattern[i]) == valueset.end()) {
-            // 都没找到，建立关系
-            permu[s[i]] = pattern[i];
-            valueset.insert(pattern[i]);
-          } else {
-            // value有对应关系了，而key s[i]没有
-            break;
-          }
-        } else {
-          // 找到了，跟之前关系不一样也不行
-          if (pattern[i] != permu[s[i]]) {
-            break;
-          }
-        }
-      }
-      if (i == s.size()) {
+      auto cur = normalize(s);
+      if (cur == expect) {
         ret.push_back(s);
       }
     }
+
+    return ret;
+  }
+  vector<int> normalize(string &s) {
+    vector<int> ret(s.length(), 0);
+    unordered_map<char, int> to_index;
+
+    for (auto &c : s) {
+      auto sz = to_index.size();
+      if (to_index.contains(c)) {
+        ret.push_back(to_index[c]);
+      } else {
+        ret.push_back(sz);
+        to_index[c] = sz;
+      }
+    }
+
     return ret;
   }
 };
