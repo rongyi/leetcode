12b0a2226be4a5a68c29b32d80f53795f5122b26
rongyi <sqrongyi@163.com>
2024-09-12 07:07:10 +0800

    Update

--- cc/fruit-into-baskets.cc
+++ cc/fruit-into-baskets.cc
@@ -1,35 +1,25 @@
 // http://leetcode.com/problems/fruit-into-baskets/description/
 #include "xxx.hpp"
+#include <unordered_map>
 
 class Solution {
 public:
   int totalFruit(vector<int> &tree) {
+    // sliding window prombels
+    unordered_map<int, int> cnt;
+    int i = 0;
     int ret = 0;
-    int cur_max = 0;
-    int lastFruit = -1;
-    int secondLastFruit = -1;
-    int lastFruitCount = 0;
-
-    for (auto fruit : tree) {
-      if (fruit == lastFruit || fruit == secondLastFruit) {
-        cur_max++;
-      } else {
-        cur_max = lastFruitCount + 1;
+    for (int j = 0; j < tree.size(); j++) {
+      cnt[tree[j]] += 1;
+      while (cnt.size() > 2) {
+        cnt[tree[i]] -= 1;
+        if (cnt[tree[i]] == 0) {
+          cnt.erase(tree[i]);
+        }
+        i += 1;
       }
-
-      if (fruit == lastFruit) {
-        lastFruitCount++;
-      } else {
-        lastFruitCount = 1;
-      }
-
-      if (fruit != lastFruit) {
-        secondLastFruit = lastFruit;
-        lastFruit = fruit;
-      }
-      ret = max(ret, cur_max);
+      ret = max(ret, j - i + 1);
     }
-
     return ret;
   }
 };
